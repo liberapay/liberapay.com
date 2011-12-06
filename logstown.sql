4efded3b32af3bb50d8a07ec3fa6cfcfffd10992
Chad Whitacre <chad@zetaweb.com>
2011-12-06 00:09:20 -0500

    Add dowski.

--- logstown.sql
+++ logstown.sql
@@ -11,6 +11,8 @@ CREATE TABLE users (
 
 INSERT INTO users (email, hash) 
     VALUES ('chad@zetaweb.com', 'cabd1aba5b11a4eef45d4015c003510e6a7c682c');
+INSERT INTO users (email, hash) 
+    VALUES ('christian@dowski.com', '657faae1aef3c3e7e806f8354a3e3f5b6839a76f');
 
 --DROP ROLE logstown;
 --CREATE ROLE logstown;
