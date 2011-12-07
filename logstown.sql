95455321c6511b6e3b943bfd2c33c61114b475a0
Chad Whitacre <chad@zetaweb.com>
2011-12-07 17:50:44 -0500

    Implement a common template for all pages.

--- logstown.sql
+++ logstown.sql
@@ -1,29 +1,17 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
-    email                   varchar(255)    NOT NULL UNIQUE,
+    email                   varchar(64)     PRIMARY KEY,
     hash                    char(40)        NOT NULL,
-    id                      varchar(255)    PRIMARY KEY,
     payment_method_token    text            DEFAULT NULL,
     session_token           char(36)        DEFAULT NULL,
     session_expires         timestamp       DEFAULT 'now'
 );
 
-INSERT INTO users (id, email, hash) 
-    VALUES ( 'chad@zetaweb.com'
-           , 'chad@zetaweb.com'
-           , 'cabd1aba5b11a4eef45d4015c003510e6a7c682c'
-            );
-INSERT INTO users (id, email, hash) 
-    VALUES ( 'christian@dowski.com'
-           , 'christian@dowski.com'
-           , '657faae1aef3c3e7e806f8354a3e3f5b6839a76f'
-            );
-
-DROP TABLE IF EXISTS ledger;
+DROP TABLE IF EXISTS transactions;
 -- Max amount is $999,999,999,999,999.99.
-CREATE TABLE ledger (
+CREATE TABLE transactions (
     id      serial          PRIMARY KEY,
-    user_id varchar(255)    REFERENCES users (id),
+    email   varchar(64)     REFERENCES users (email),
     amount  numeric(15,2)   NOT NULL,
     ts      timestamp       NOT NULL DEFAULT 'now'
 );
--- www/.aspen/logstown/authentication.py
+++ www/.aspen/logstown/authentication.py
@@ -16,23 +16,26 @@ salt = "cheese and crackers" # XXX
 def hash(password):
     return sha1(password + salt).hexdigest()
 
-SESSION = ("UPDATE users SET session_token=%s "
-           "WHERE id=%s AND hash=%s RETURNING *")
-def authenticate(id, password):
+SESSION_NEW = ("UPDATE users SET session_token=%s "
+               "WHERE email=%s AND hash=%s RETURNING *")
+def authenticate(email, password):
     from logstown import db
     token = str(uuid.uuid4())
     hashed = hash(password)
-    rec = db.fetchone(SESSION, (token, id, hashed))
+    rec = db.fetchone(SESSION_NEW, (token, email, hashed))
     if rec is not None:
         del rec['hash'] # safety
         return rec
     return {}
 
-TOKEN = ("SELECT email, session_token, session_expires, payment_method_token "
-         "FROM users WHERE session_token=%s")
+SESSION_LOAD = """\
+    SELECT email, session_token, session_expires, payment_method_token 
+      FROM users
+     WHERE session_token=%s
+"""
 def load_session(token):
     from logstown import db
-    rec = db.fetchone(TOKEN, (token,))
+    rec = db.fetchone(SESSION_LOAD, (token,))
     if rec is not None:
         assert rec['session_token'] == token # sanity
         assert 'hash' not in rec # safety
