497f3071ebe59eaebc8eb220fb8c0b4cebf7b30f
Chad Whitacre <chad@zetaweb.com>
2012-03-25 18:00:41 -0400

    Stripping way down.

--- sql/0000.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS transactions;
-
--- sql/0002.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE users
-  ADD COLUMN created            timestamp       NOT NULL DEFAULT 'now'
-, ADD COLUMN is_admin           boolean         NOT NULL DEFAULT FALSE
- ;
-UPDATE users SET is_admin=true WHERE email='chad@zetaweb.com';
