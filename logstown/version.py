b59e07c9ee11460cb3399a001b02d9cfa104cd9f
Chad Whitacre <chad@zetaweb.com>
2012-06-01 12:09:01 -0400

    Resetting version in logstown/version.py.

--- logstown/version.py
+++ logstown/version.py
@@ -1,4 +1,4 @@
 # This is isolated so that we can safely automatically mutate it during
 # release. We don't want to accidentally commit changes to __init__.py
 
-__version__ = "0.0.4"
+__version__ = "~~VERSION~~"
