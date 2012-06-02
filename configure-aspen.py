c2ecb57888a5fba0165fb65c8f0dc8f7285e76bd
Chad Whitacre <chad@zetaweb.com>
2012-06-01 22:54:38 -0400

    Tighten up sign in/out to stay on same page

--- configure-aspen.py
+++ configure-aspen.py
@@ -17,8 +17,16 @@ website.hooks.inbound_early.register(gittip.canonize)
 website.hooks.inbound_early.register(gittip.authentication.inbound) 
 website.hooks.outbound_late.register(gittip.authentication.outbound) 
 
+def github_oauth_url(then=""):
+    url = "https://github.com/login/oauth/authorize?client_id=%s" 
+    url %= website.github_client_id
+    if url:
+        url += "&redirect_uri=%s?then=%s" % (website.github_callback, then)
+    return url
+
 def add_stuff(request):
     request.context['__version__'] = gittip.__version__
     request.context['username'] = None 
+    request.context['github_oauth_url'] = github_oauth_url
 
 website.hooks.inbound_early.register(add_stuff) 
