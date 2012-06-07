be7d630362dcfd3a34ae275e7b3d3681029c849e
Chad Whitacre <chad@zetaweb.com>
2012-06-07 17:52:48 -0400

    Add UI for making a statement (#33)

---
+++ gittip/utils.py
@@ -0,0 +1,11 @@
+from aspen.utils import typecheck
+from aspen._tornado.escape import linkify
+
+
+def wrap(u):
+    """Given a unicode, return a unicode.
+    """
+    typecheck(u, unicode)
+    u = linkify(u)  # Do this first, because it calls xthml_escape.
+    u = u.replace(u'\r\n', u'<br />\r\n').replace(u'\n', u'<br />\n')
+    return u if u else '...'
