55b1eb6d1ca8acad4e8fa41905827424c9ff4557
Chad Whitacre <chad@zetaweb.com>
2012-08-20 14:46:55 -0400

    Ensure that objects are owned by $OWNER (#233)

--- makedb.sh
+++ makedb.sh
@@ -10,8 +10,28 @@ DBNAME=${1:-$DBNAME_DEFAULT}
 OWNER_DEFAULT=$DBNAME
 OWNER=${2:-$OWNER_DEFAULT}
 
+
+echo "=============================================================================="
+echo "Creating user ..."
+echo 
+
 createuser -s $OWNER
+
+echo 
+echo "=============================================================================="
+echo "Dropping and creating db ..."
+echo 
+
 dropdb $DBNAME
 createdb $DBNAME -O $OWNER
-psql $DBNAME < enforce-utc.sql
-psql $DBNAME < schema.sql
+
+echo 
+echo "=============================================================================="
+echo "Applying schema.sql ..."
+echo 
+
+psql -U $OWNER $DBNAME < enforce-utc.sql
+psql -U $OWNER $DBNAME < schema.sql
+
+echo 
+echo "=============================================================================="
