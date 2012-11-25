36e6af1d6aad0b810136859aca244983400c78e9
Chad Whitacre <chad@zetaweb.com>
2012-11-24 19:21:06 -0500

    Add a little doco (#289, #313)

--- gittip/networks/twitter.py
+++ gittip/networks/twitter.py
@@ -26,5 +26,14 @@ def resolve(user_id):
     return rec['participant_id']
 
 
-def oauth_url(website, action, then):
+def oauth_url(website, action, then=""):
+    """Return a URL to start oauth dancing with Twitter.
+
+    For GitHub we can pass action and then through a querystring. For Twitter
+    we can't, so we send people through a local URL first where we stash this
+    info in an in-memory cache (eep! needs refactoring to scale).
+
+    Not sure why website is here. Vestige from GitHub forebear?
+
+    """
     return "/on/twitter/redirect?action=%s&then=%s" % (action, then)
