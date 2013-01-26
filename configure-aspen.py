a9ce96b3a4aec2230938a0ec5c93d65f0a2861c4
Chad Whitacre <chad@zetaweb.com>
2013-01-26 00:45:27 -0500

    Upgrade to Aspen 0.22.7; #501
    In order to log to Sentry, we need the error hooks that were added to
    Aspen in 0.22.x.

--- configure-aspen.py
+++ configure-aspen.py
@@ -21,13 +21,15 @@ website.twitter_consumer_key = os.environ['TWITTER_CONSUMER_KEY'].decode('ASCII'
 website.twitter_consumer_secret = os.environ['TWITTER_CONSUMER_SECRET'].decode('ASCII')
 website.twitter_callback = os.environ['TWITTER_CALLBACK'].decode('ASCII')
 
-website.hooks.inbound_early.register(gittip.canonize)
-website.hooks.inbound_early.register(gittip.configure_payments)
-website.hooks.inbound_early.register(gittip.csrf.inbound)
-website.hooks.inbound_early.register(gittip.authentication.inbound)
-website.hooks.outbound_late.register(gittip.authentication.outbound)
-website.hooks.outbound_late.register(gittip.csrf.outbound)
-website.hooks.outbound_late.register(gittip.orm.rollback)
+website.hooks.inbound_early += [ gittip.canonize
+                               , gittip.configure_payments
+                               , gittip.csrf.inbound
+                               , gittip.authentication.inbound
+                                ]
+website.hooks.outbound += [ gittip.authentication.outbound
+                          , gittip.csrf.outbound
+                          , gittip.orm.rollback
+                           ]
 
 
 __version__ = open(os.path.join(website.www_root, 'version.txt')).read().strip()
@@ -40,4 +42,4 @@ def add_stuff(request):
     request.context['github'] = github
     request.context['twitter'] = twitter
 
-website.hooks.inbound_early.register(add_stuff)
+website.hooks.inbound_early += [add_stuff]
