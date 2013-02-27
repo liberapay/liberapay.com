ddc374c8465139f38507225a480884700a28fa82
Roy Liu <carsomyr@gmail.com>
2013-02-26 20:25:26 -0500

    Dedicate the payday steady state library to the public domain

--- gittip/billing/steady_state.py
+++ gittip/billing/steady_state.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2013 Roy Liu
+# Written in 2013 by Roy Liu <carsomyr@gmail.com>
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not
-# use this file except in compliance with the License. You may obtain a copy of
-# the License at
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under
-# the License.
+# You should have received a copy of the CC0 Public Domain Dedication along with
+# this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 """A library for computing the payday steady state. It owes its existence to the knotty problem of determining payouts
 under situations where funds contain funds themselves, and when the payout graph contains cycles.
