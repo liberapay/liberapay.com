3997cd03d6aa9c9e0e0a44f8a45b65d47b821afe
Chad Whitacre <chad@zetaweb.com>
2013-04-03 11:32:45 -0400

    Move migration into a transaction; #141
    This dodges a race condition where we could fail to log some goal
    changes.

--- schema.sql
+++ schema.sql
@@ -367,29 +367,34 @@ CREATE TABLE goals
 , goal                  numeric(35,2)               DEFAULT NULL
  );
 
--- Migrate data from goal column of participants over to new goals table.
-INSERT INTO goals (ctime, mtime, participant, goal)
-     SELECT CURRENT_TIMESTAMP
-          , CURRENT_TIMESTAMP
-          , id
-          , goal
-       FROM participants
-      WHERE goal IS NOT null;
 
--- Create a rule to log changes to participant.goal into goals.
-CREATE RULE log_goal_changes
-AS ON UPDATE TO participants
-          WHERE (OLD.goal IS NULL AND NOT NEW.goal IS NULL)
-             OR (NEW.goal IS NULL AND NOT OLD.goal IS NULL)
-             OR NEW.goal <> OLD.goal
-             DO
-    INSERT INTO goals
-                (ctime, participant, goal)
-         VALUES ( COALESCE (( SELECT ctime
-                                FROM goals
-                               WHERE participant=OLD.id
-                               LIMIT 1
-                             ), CURRENT_TIMESTAMP)
-                , OLD.id
-                , NEW.goal
-                 );
+BEGIN;
+
+    -- Migrate data from goal column of participants over to new goals table.
+    INSERT INTO goals (ctime, mtime, participant, goal)
+         SELECT CURRENT_TIMESTAMP
+              , CURRENT_TIMESTAMP
+              , id
+              , goal
+           FROM participants
+          WHERE goal IS NOT NULL;
+
+    -- Create a rule to log changes to participant.goal into goals.
+    CREATE RULE log_goal_changes
+    AS ON UPDATE TO participants
+              WHERE (OLD.goal IS NULL AND NOT NEW.goal IS NULL)
+                 OR (NEW.goal IS NULL AND NOT OLD.goal IS NULL)
+                 OR NEW.goal <> OLD.goal
+                 DO
+        INSERT INTO goals
+                    (ctime, participant, goal)
+             VALUES ( COALESCE (( SELECT ctime
+                                    FROM goals
+                                   WHERE participant=OLD.id
+                                   LIMIT 1
+                                 ), CURRENT_TIMESTAMP)
+                    , OLD.id
+                    , NEW.goal
+                     );
+
+END;
