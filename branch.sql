aaba853316c94075932d79aa842ab21430872847
Chad Whitacre <chad@zetaweb.com>
2013-04-11 15:52:38 -0400

    Back out wrong index attempt; #449

--- branch.sql
+++ branch.sql
@@ -1,4 +1,3 @@
 -------------------------------------------------------------------------------
 -- https://github.com/gittip/www.gittip.com/issues/449
 
-CREATE INDEX username_index ON participants USING gin(username);
