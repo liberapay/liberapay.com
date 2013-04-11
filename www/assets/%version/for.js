86d957243ce890284d83032f479d439c3891f779
Chad Whitacre <chad@zetaweb.com>
2013-04-10 23:07:01 -0400

    Start wiring up live search; #449

--- www/assets/%version/for.js
+++ www/assets/%version/for.js
@@ -1,6 +1,31 @@
 Gittip.for = {}
 
+Gittip.for.success = function(d)
+{
+    var UL = document.createElement('UL');
+    for (var i=0, item; item = d[i]; i++)
+    {
+        username = item[0];
+        id = item[1];
+
+        console.log(username, id);
+    }
+};
+
+Gittip.for.reload = function(d)
+{
+    var query = $('#deserts input').val();
+    if (query.length < 2)
+        return;
+    jQuery.ajax(
+        { url: '/for/_lookup.json'
+        , data: {query: query}
+        , success: Gittip.for.success
+         }
+    )
+};
+
 Gittip.for.init = function()
 {
-    console.log("yes");
+    $('#deserts input').keyup(Gittip.for.reload)
 };
