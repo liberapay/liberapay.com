f72d6c215ce9b041795ed08474717fb4fd77704e
Chad Whitacre <chad@zetaweb.com>
2013-04-13 07:53:12 -0400

    Prune branch.sql from master

--- branch.sql
+++
@@ -1,4 +0,0 @@
--------------------------------------------------------------------------------
--- https://github.com/gittip/www.gittip.com/issues/449
-
-CREATE INDEX username_index ON participants USING gin(username);
