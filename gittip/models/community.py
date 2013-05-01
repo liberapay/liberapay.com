7b5b3e4560308cc8faecfc0e37a0902424770fa4
Chad Whitacre <chad@zetaweb.com>
2013-05-01 19:45:26 -0400

    Add join/leave UI on community pages; #905
    This uses a hard refresh of the page to update state. Maybe we'll have a
    nice framework for doing that on-page some day (Angular? #833).

--- gittip/models/community.py
+++ gittip/models/community.py
@@ -2,6 +2,7 @@ from sqlalchemy.schema import Column
 from sqlalchemy.types import Text, BigInteger
 
 from gittip.orm import db
+from gittip import db as dear_god_why
 
 class Community(db.Model):
     __tablename__ = 'community_summary'
@@ -9,3 +10,10 @@ class Community(db.Model):
     name = Column(Text)
     slug = Column(Text, primary_key=True)
     nmembers = Column(BigInteger)
+
+    def check_membership(self, user):
+        return dear_god_why.fetchone("""
+
+        SELECT * FROM current_communities WHERE slug=%s AND participant=%s
+
+        """, (self.slug, user.username)) is not None
--- www/assets/%version/gittip.js
+++ www/assets/%version/gittip.js
@@ -517,6 +517,18 @@ Gittip.paymentsResponseHandler = function(response)
     }
 };
 
+Gittip.init = function(username)
+{
+    // username is useful for constructing API endpoints
+    var username = Gittip.getCookie('username');
+    if (username === '')
+        delete Gittip.username;
+    else
+        Gittip.username = username;
+
+    Gittip.initCSRF();
+};
+
 Gittip.initPayment = function(balanced_uri, participantId)
 {
     Gittip.participantId = participantId;
@@ -705,7 +717,7 @@ Gittip.communities.update = function(name, is_member, callback)
 {
     jQuery.ajax(
         { type: 'POST'
-        , url: 'communities.json'
+        , url: '/' + Gittip.username + '/communities.json'
         , data: {name: name, is_member: is_member}
         , dataType: 'json'
         , success: callback
