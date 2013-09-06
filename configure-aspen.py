08b279941650f28dcd16da6cce43b4aac5ac69b3
Chad Whitacre <chad@zetaweb.com>
2013-09-06 09:40:32 -0400

    Round nactive in the header
    In #1377 we rounded the dollar amount. Let's round the number of users
    as well.

--- configure-aspen.py
+++ configure-aspen.py
@@ -89,8 +89,11 @@ def add_stuff(request):
     request.context['github'] = github
     request.context['twitter'] = twitter
     request.context['bountysource'] = bountysource
-    stats = gittip.db.one('SELECT nactive, transfer_volume FROM paydays ORDER BY ts_end DESC LIMIT 1', default=(0, 0.0))
-    request.context['gnactive'] = locale.format("%d", stats[0], grouping=True)
+    stats = gittip.db.one( "SELECT nactive, transfer_volume FROM paydays "
+                           "ORDER BY ts_end DESC LIMIT 1"
+                         , default=(0, 0.0)
+                          )
+    request.context['gnactive'] = locale.format("%d", round(stats[0], -2), grouping=True)
     request.context['gtransfer_volume'] = locale.format("%d", round(stats[1], -2), grouping=True)
 
 website.hooks.inbound_early += [add_stuff]
