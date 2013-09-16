c5478fc0e816b74a387bf9d8b7064a73fa5c150b
Chad Whitacre <chad@zetaweb.com>
2013-09-16 15:24:59 -0400

    This allows py.test to run the test suite
    There are two main python test runners: nosetests and py.test. We use
    nosetests in `make test` but py.test is nicer and we should probably
    switch to it (we talked about this a while ago). This small commit makes
    the suite pass under py.test.

--- tests/test_associate.py
+++ tests/test_associate.py
@@ -1,7 +1,7 @@
 from __future__ import division, print_function, unicode_literals
 
 import mock
-from gittip.testing import Harness, test_website
+from gittip.testing import Harness, test_website as _test_website
 from gittip.testing.client import TestClient
 
 
@@ -9,7 +9,7 @@ class Tests(Harness):
 
     def setUp(self):
         Harness.setUp(self)
-        self.website = test_website
+        self.website = _test_website
         self.client = TestClient()
 
     def tearDown(self):
