09eca38558f7ca6d7352050169fc33a238803826
Chad Whitacre <chad@zetaweb.com>
2013-09-23 17:43:16 -0400

    Here's a failing test for #940

--- tests/test_hook_order.py
+++ tests/test_hook_order.py
@@ -1,7 +1,10 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import os
 from gittip import wireup
 from gittip.testing import Harness
 from gittip.testing.client import TestClient
+from gittip.models.participant import Participant
 
 
 class TestCanonizer(Harness):
@@ -27,3 +30,25 @@ class TestCanonizer(Harness):
         response = self.client.get("/", HTTP_HOST='www.gittip.com', HTTP_X_FORWARDED_PROTO='http')
         assert response.code == 302
         assert response.headers['Location'] == 'https://www.gittip.com/'
+
+    def test_canonize_doesnt_mess_up_auth(self):
+        # https://github.com/gittip/www.gittip.com/issues/940
+
+        self.make_participant('alice')
+
+        # Make a normal authenticated request.
+        normal = self.client.get( "/"
+                                , user='alice'
+                                , HTTP_X_FORWARDED_PROTO='https'
+                                , HTTP_HOST='www.gittip.com'
+                                 )
+        alice = Participant.from_username('alice')
+        assert normal.headers.cookie['session'].value == alice.session_token
+
+        # Now make a request that canonizer will redirect.
+        redirect = self.client.get( "/"
+                                  , user='alice'
+                                  , HTTP_X_FORWARDED_PROTO='http'
+                                  , HOST='www.gittip.com'
+                                   )
+        assert 'session' not in redirect.headers.cookie
