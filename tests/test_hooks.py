2f6d08b99f77a378ad0c55b0bb84304bba4d7d7b
Chad Whitacre <chad@zetaweb.com>
2013-09-23 21:44:12 -0400

    Fix #940
    If we set 'secure' for the session cookie when https is in use (i.e., in
    production and not in dev) then we should never get a session cookie on
    a request for http://www.gittip.com/, and consequently we won't set an
    empty session cookie on the subsequent 302 response redirecting the user
    to https://www.gittip.com/.

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -7,7 +7,7 @@ from gittip.testing.client import TestClient
 from gittip.models.participant import Participant
 
 
-class TestCanonizer(Harness):
+class Tests(Harness):
 
     def setUp(self):
         self.client = TestClient()
@@ -21,6 +21,7 @@ class TestCanonizer(Harness):
         wireup.canonical()
 
     def tearDown(self):
+        Harness.tearDown(self)
         os.environ['CANONICAL_SCHEME'] = self._blech[0]
         os.environ['CANONICAL_HOST'] = self._blech[1]
         wireup.canonical()
@@ -31,9 +32,8 @@ class TestCanonizer(Harness):
         assert response.code == 302
         assert response.headers['Location'] == 'https://www.gittip.com/'
 
-    def test_canonize_doesnt_mess_up_auth(self):
-        # https://github.com/gittip/www.gittip.com/issues/940
 
+    def test_session_cookie_set_in_auth_response(self):
         self.make_participant('alice')
 
         # Make a normal authenticated request.
@@ -45,10 +45,38 @@ class TestCanonizer(Harness):
         alice = Participant.from_username('alice')
         assert normal.headers.cookie['session'].value == alice.session_token
 
+
+    def test_session_cookie_is_sent_for_http_as_well(self):
+        # https://github.com/gittip/www.gittip.com/issues/940
+
+        self.make_participant('alice')
+
         # Now make a request that canonizer will redirect.
         redirect = self.client.get( "/"
                                   , user='alice'
                                   , HTTP_X_FORWARDED_PROTO='http'
-                                  , HOST='www.gittip.com'
+                                  , HTTP_HOST='www.gittip.com'
                                    )
-        assert 'session' not in redirect.headers.cookie
+        assert redirect.code == 302
+        assert redirect.headers.cookie['session'].value == ""
+
+        # This is bad, because it means that the user will be signed out of
+        # https://www.gittip.com/ if they make a request for
+        # http://www.gittip.com/. They might do this themselves accidentally,
+        # but more likely a browser plugin (such as DoNotTrack) will do it for
+        # them. The way we fix this is to set "secure" on the session cookie,
+        # so that the browser won't send the session cookie to the server in
+        # the case of http://www.gittipcom/. Without a session cookie in the
+        # request, gittip.security.authentication.outbound won't set one on the
+        # way out.
+
+
+    def test_session_cookie_is_secure_if_it_should_be(self):
+        # https://github.com/gittip/www.gittip.com/issues/940
+        response = self.client.get( "/"
+                                  , user=self.make_participant('alice').username
+                                  , HTTP_X_FORWARDED_PROTO='https'
+                                  , HTTP_HOST='www.gittip.com'
+                                   )
+        assert response.code == 200
+        assert '; secure' in response.headers.cookie['session'].output()
