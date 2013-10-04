18e5661f83d2fc27105016036dd7a4fdb09e87ab
Chad Whitacre <chad@zetaweb.com>
2013-10-04 13:58:17 -0400

    Here's a failing test for an Aspen upgrade issue

--- tests/test_elsewhere_bitbucket.py
+++ tests/test_elsewhere_bitbucket.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals
 
+from aspen.http.request import UnicodeWithParams
 from gittip.elsewhere import bitbucket
 from gittip.testing import Harness
 
@@ -10,4 +11,10 @@ class TestElsewhereBitbucket(Harness):
         bitbucket.BitbucketAccount("1", {'username': 'alice'}).opt_in('alice')
         expected = {"username": "alice"}
         actual = bitbucket.get_user_info('alice')
+        assert actual == expected
+
+    def test_get_user_info_gets_user_info_from_UnicodeWithParams(self):
+        bitbucket.BitbucketAccount("1", {'username': 'alice'}).opt_in('alice')
+        expected = {"username": "alice"}
+        actual = bitbucket.get_user_info(UnicodeWithParams('alice', {}))
         assert actual == expected
