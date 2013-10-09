6b669159422326e240b87e7dba1ae0428004e579
Luke Strickland <luke@axxim.net>
2013-10-08 21:50:58 -0500

    Adding mixpanel to the globals list

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,8 @@ module.exports = function(grunt) {
           Gittip: true,
           _gttp: true,
           jQuery: true,
-          $: true
+          $: true,
+          mixpanel: true
         }
       }
     }
