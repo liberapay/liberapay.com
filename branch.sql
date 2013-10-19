91df96d994666aa0901e5a9321c3b11d18f7d6a0
Zbynek Winkler <zbynek.winkler@gmail.com>
2013-10-19 20:18:47 +0200

    Fold branch.sql into schema.sql.

--- branch.sql
+++
@@ -1,4 +0,0 @@
--------------------------------------------------------------------------------
--- https://github.com/gittip/www.gittip.com/pull/1582
-
-ALTER TABLE participants ADD COLUMN paypal_email text DEFAULT NULL;
