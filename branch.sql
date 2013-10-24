5051f4ab1fba84acb61557dba3970c180f8212a7
Zbynek Winkler <zbynek.winkler@gmail.com>
2013-10-24 21:56:21 +0200

    Add forgotten modifications.

--- branch.sql
+++ branch.sql
@@ -3,3 +3,9 @@ CREATE INDEX participants_claimed_time ON participants (claimed_time DESC)
     AND claimed_time IS NOT null;
 
 DROP TABLE homepage_new_participants;
+
+ALTER TABLE homepage_top_receivers ADD COLUMN gravatar_id text;
+ALTER TABLE homepage_top_receivers ADD COLUMN twitter_pic text;
+
+ALTER TABLE homepage_top_givers ADD COLUMN gravatar_id text;
+ALTER TABLE homepage_top_givers ADD COLUMN twitter_pic text;
