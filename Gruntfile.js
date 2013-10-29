44a66d9f4dce1aab8e5259b2b618d7cb78229390
rummik <r@9k1.us>
2013-10-29 16:02:15 -0400

    Change Gruntfile and package.json indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,58 @@
 module.exports = function(grunt) {
-  'use strict';
+    'use strict';
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile %>',
-        tasks: 'jshint:gruntfile'
-      },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile %>',
+                tasks: 'jshint:gruntfile'
+            },
 
-      js: {
-        files: '<%= jshint.js %>',
-        tasks: 'jshint:js'
-      }
-    },
+            js: {
+                files: '<%= jshint.js %>',
+                tasks: 'jshint:js'
+            }
+        },
 
-    jshint: {
-      gruntfile: 'Gruntfile.js',
-      js: 'js/**/*.{js,json}',
+        jshint: {
+            gruntfile: 'Gruntfile.js',
+            js: 'js/**/*.{js,json}',
 
-      options: {
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        quotmark: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: false,
-        regexdash: true,
-        smarttabs: false,
-        strict: false,
-        node: true,
-        browser: true,
-        jquery: true,
-        devel: true,
+            options: {
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                quotmark: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: false,
+                regexdash: true,
+                smarttabs: false,
+                strict: false,
+                node: true,
+                browser: true,
+                jquery: true,
+                devel: true,
 
-        globals: {
-          Gittip: true,
-          _gttp: true,
-          gttpURI: true,
-          mixpanel: true,
-          alert: true
+                globals: {
+                    Gittip: true,
+                    _gttp: true,
+                    gttpURI: true,
+                    mixpanel: true,
+                    alert: true
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['test']);
-  grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('default', ['test']);
+    grunt.registerTask('test', ['jshint']);
 };
