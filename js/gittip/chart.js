7f214ec0222d95d587a2fcff98b75635933c2fe1
Chad Whitacre <chad@zetaweb.com>
2013-11-12 18:23:57 -0500

    Four of the six charts are reimplemented
    The two that aren't are cumulative and nwithdrawals, which need some
    math on them. These are the existing charts, mind you. Once these are
    taken care of I'll look at implementing the per-user chart.

--- js/gittip/chart.js
+++ js/gittip/chart.js
@@ -1,11 +1,125 @@
-Gittip.Chart = function() {
+Gittip.charts = {};
 
-    var Withdrawals = $('#withdrawals-');
-    var Charges = $('#charges-');
-    var Volume = $('#volume-');
-    var Cumulative = $('#cumulative-');
-    var Users = $('#users-');
-    var Active = $('#active-');
+Gittip.charts.make = function(series) {
+    // Takes an array of time series data.
+
+    if (!series) {
+        return;
+    }
+
+
+    // Gather charts on the page that advertise an ability to visualize the
+    // information available in the time series.
+
+    var first = series[0];
+    var charts = [];
+
+    for (var varname in first) {
+        var chart = $('#chart-'+varname);
+        if (chart.length) {
+            chart.varname = varname;
+            charts.push(chart);
+        }
+    }
+
+    var H = $('.chart').height();
+    var W = $('.chart').width();
+    var nweeks = series.length - 1; // don't show Gittip #0
+    var w = Math.floor((W - 20) / nweeks);
+    var W = w * nweeks;
+
+    $('.n-weeks').text(nweeks);
+    $('.chart').width(W);
+
+
+    // Compute maxes and scales.
+    // =========================
+
+    var maxes = [];
+    var scales = [];
+
+    for (var i=0, point; point = series[i]; i++) {
+        for (var j=0, chart; chart = charts[j]; j++) {
+            maxes[j] = Math.max(maxes[j] || 0, point[chart.varname]);
+        }
+        /*
+        maxCumulative += parseFloat(point.transfer_volume);
+        */
+    }
+
+    for (var i=0, len=maxes.length; i < len; i++) {
+        scales.push(Math.ceil(maxes[i] / 100) * 100);
+    }
+
+
+    // Draw weeks.
+    // ===========
+
+    function Week(n, max, N, y, title)
+    {
+        var x = nweeks - n;
+        var create = function(x) { return document.createElement(x); };
+        var week = $(create('div')).addClass('week');
+        var shaded = $(create('div')).addClass('shaded');
+        shaded.html( '<span class="y-label">'
+                   + parseInt(y, 10)
+                   + '</span>'
+                    );
+        week.append(shaded);
+        week.attr({x: x, y: y});
+
+        var xTick = $(create('span')).addClass('x-tick');
+        xTick.text(x);
+        xTick.attr('title', title);
+        if ((x % 5) === 0)
+            xTick.addClass('on');
+        week.append(xTick);
+        if (y === max)
+            week.addClass('flagged');
+
+        var y = parseFloat(y);
+        var h = Math.ceil(((y / N) * H));
+        week.height(H);
+        week.width(w);
+        week.css({"left": w * (nweeks - n - 1)});
+        shaded.css({"height": h});
+        return week;
+    }
+
+    //var cumulative = maxCumulative;
+    for (var i=0, point; point = series[i]; i++)
+    {
+        var point = series[i];
+        var weekstamp = point.ts_start.slice(0, 10);
+
+        for (var j=0, chart; chart = charts[j]; j++) {
+
+            var chart = charts[j];
+
+            chart.append(Week( i
+                             , maxes[j]
+                             , scales[j]
+                             , point[charts[j].varname]
+                             , weekstamp
+                              ));
+            /*
+            Cumulative.append(Week( i
+                                  , maxCumulative
+                                  , scaleCumulative
+                                  , cumulative
+                                  , weekstamp
+                                   ));
+            cumulative -= parseFloat(point.transfer_volume);
+            */
+        }
+    }
+
+    $('.week').width(w);
+    $('.shaded').width(w);
+
+
+    // Wire up behaviors.
+    // ==================
 
     function mouseover()
     {
@@ -17,156 +131,15 @@ Gittip.Chart = function() {
 
     function mouseout()
     {
-        $(this).removeClass('hover')
+        $(this).removeClass('hover');
     }
 
-    jQuery.get('/about/paydays.json', function(paydays)
+    $('.week').click(function()
     {
-        var H = $('.chart').height();
-        var W = $('.chart').width();
-        var nweeks = paydays.length - 1; // don't show Gittip #0
-        var w = Math.floor((W - 20) / nweeks);
-        var W = w * nweeks;
-
-        var payday = null;
-
-        $('.n-weeks').text(nweeks);
-        $('.chart').width(W);
-
-
-        // Compute vertical scale.
-        // =======================
-
-        var maxUsers = 0;
-        var maxActive = 0;
-        var maxGifts = 0;
-        var maxCumulative = 0;
-        var maxWithdrawals = 0;
-        var maxCharges = 0;
-
-
-        for (var i=0; i < nweeks; i++)
-        {
-            payday = paydays[i];
-            maxUsers = Math.max( maxUsers
-                               , parseFloat(payday.nparticipants)
-                                );
-            maxActive = Math.max( maxActive
-                                , parseFloat(payday.nactive)
-                                 );
-            maxCumulative += parseFloat(payday.transfer_volume);
-            maxGifts = Math.max( maxGifts
-                               , parseFloat(payday.transfer_volume)
-                                );
-            maxWithdrawals = Math.max( maxWithdrawals
-                                     , -parseFloat(payday.ach_volume)
-                                      );
-            maxCharges = Math.max( maxCharges
-                                 , parseFloat(payday.charge_volume)
-                                  );
-        }
-
-        scaleUsers = Math.ceil(maxUsers / 100) * 100;
-        scaleActive = Math.ceil(maxActive / 100) * 100;
-        scaleGifts = Math.ceil(maxGifts / 100) * 100;
-        scaleCumulative = Math.ceil(maxCumulative / 100) * 100;
-        scaleWithdrawals = Math.ceil(maxWithdrawals / 100) * 100;
-        scaleCharges = Math.ceil(maxCharges / 100) * 100;
-
-
-        // Draw weeks.
-        // ===========
-
-        function Week(n, max, N, y, title)
-        {
-            var x = nweeks - n;
-            var create = function(x) { return document.createElement(x) };
-            var week = $(create('div')).addClass('week');
-            var shaded = $(create('div')).addClass('shaded');
-            shaded.html( '<span class="y-label">'
-                       + parseInt(y)
-                       + '</span>'
-                        );
-            week.append(shaded);
-            week.attr({x: x, y: y});
-
-            var xTick = $(create('span')).addClass('x-tick');
-            xTick.text(x);
-            xTick.attr('title', title);
-            if ((x % 5) === 0)
-                xTick.addClass('on');
-            week.append(xTick);
-            if (y === max)
-                week.addClass('flagged');
-
-            var y = parseFloat(y);
-            var h = Math.ceil(((y / N) * H));
-            week.height(H);
-            week.width(w);
-            week.css({"left": w * (nweeks - n - 1)});
-            shaded.css({"height": h});
-            return week;
-        }
-
-        var cumulative = maxCumulative;
-        for (var i=0; i < nweeks; i++)
-        {
-            var payday = paydays[i];
-            var weekstamp = payday.ts_start.slice(0, 10);
-
-            Users.append(Week( i
-                             , maxUsers
-                             , scaleUsers
-                             , payday.nparticipants
-                             , weekstamp
-                              ));
-            Active.append(Week( i
-                              , maxActive
-                              , scaleActive
-                              , payday.nactive
-                              , weekstamp
-                               ));
-            Volume.append(Week( i
-                              , maxGifts
-                              , scaleGifts
-                              , payday.transfer_volume
-                              , weekstamp
-                               ));
-            Cumulative.append(Week( i
-                                  , maxCumulative
-                                  , scaleCumulative
-                                  , cumulative
-                                  , weekstamp
-                                   ));
-            cumulative -= parseFloat(payday.transfer_volume);
-            Withdrawals.append(Week( i
-                                   , maxWithdrawals
-                                   , scaleWithdrawals
-                                   , -payday.ach_volume
-                                   , weekstamp
-                                    ));
-            Charges.append(Week( i
-                               , maxCharges
-                               , scaleCharges
-                               , payday.charge_volume
-                               , weekstamp
-                                ));
-        }
-
-        $('.week').width(w);
-        $('.shaded').width(w);
-
-
-        // Wire up behaviors.
-        // ==================
-
-        $('.week').click(function()
-        {
-            $(this).toggleClass('flagged');
-            if ($(this).hasClass('flagged'))
-                $(this).removeClass('hover');
-        });
-        $('.week').mouseover(mouseover)
-        $('.week').mouseout(mouseout)
+        $(this).toggleClass('flagged');
+        if ($(this).hasClass('flagged'))
+            $(this).removeClass('hover');
     });
+    $('.week').mouseover(mouseover);
+    $('.week').mouseout(mouseout);
 };
