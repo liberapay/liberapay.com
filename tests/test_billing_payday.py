b0e2fbbda8d5362533dbc1f962bedbc0bc5c3eaf
Chad Whitacre <chad@zetaweb.com>
2013-11-21 16:35:46 -0500

    A test for move_pending_to_balance_for_teams

--- tests/test_billing_payday.py
+++ tests/test_billing_payday.py
@@ -343,6 +343,12 @@ class TestBillingPayday(TestPaydayBase):
         super(TestBillingPayday, self).setUp()
         self.payday = Payday(self.db)
 
+    def test_move_pending_to_balance_for_teams_does_so(self):
+        self.make_participant('A', number='plural', balance=2, pending=3)
+        self.payday.move_pending_to_balance_for_teams()
+        actual = self.db.one("SELECT balance FROM participants WHERE username='A'")
+        assert actual == 5
+
     @mock.patch('gittip.models.participant.Participant.get_tips_and_total')
     def test_charge_and_or_transfer_no_tips(self, get_tips_and_total):
         self.db.run("""
