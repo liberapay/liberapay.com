3f1327d060858921fd890b1ecceceed60b4df783
Chad Whitacre <chad@zetaweb.com>
2013-11-26 14:59:06 -0500

    Tighten up aggregation of npatrons/receipts; #1667
    This addresses several issues. First of all, we had been aggregating
    based on date, which is safe in production (for now?), but makes testing
    much more difficult. Now we aggregate based on the actual timestamps of
    the paydays in question. We do so in a way that isn't too taxing on the
    db. The transfers query takes about 50ms for my user from a recent
    backup. This fixes a bug where a user who received tips for a few weeks
    and then had a week where they didn't get any tips would ... I don't
    know, but it was a bug. We now have a test for that case, as well as the
    case where a transfer happens in between paydays.

--- tests/test_charts_json.py
+++ tests/test_charts_json.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import datetime
 import json
@@ -28,27 +28,101 @@ class Tests(Harness):
         self.make_participants_and_tips()
         assert json.loads(TestClient().get('/carl/charts.json').body) == []
 
-    def test_one_payday_returns_empty_list(self):
+    def test_zeroth_payday_is_ignored(self):
         self.make_participants_and_tips()
-        self.run_payday()
+        self.run_payday()   # zeroeth
         assert json.loads(TestClient().get('/carl/charts.json').body) == []
+
+    def test_first_payday_comes_through(self):
+        alice, bob = self.make_participants_and_tips()
+        self.run_payday()   # zeroeth, ignored
+        self.run_payday()   # first
+
+        expected = [ { "date": datetime.date.today().strftime('%Y-%m-%d')
+                     , "npatrons": 2
+                     , "receipts": 3.00
+                      }
+                    ]
+        actual = json.loads(TestClient().get('/carl/charts.json').body)
+
+        assert actual == expected
 
     def test_second_payday_comes_through(self):
         alice, bob = self.make_participants_and_tips()
+        self.run_payday()   # zeroth, ignored
+        self.run_payday()   # first
 
-        # Suppress tips for the zeroth payday, because it's too hard to make
-        # the two paydays run with different dates, and the charts.json queries
-        # lump together based on payday date.
-        alice.set_tip_to('carl', '0.00')
+        alice.set_tip_to('carl', '5.00')
         bob.set_tip_to('carl', '0.00')
-        self.run_payday()
 
-        # Bring back tips for first payday.
-        alice.set_tip_to('carl', '1.00')
-        bob.set_tip_to('carl', '2.00')
-        self.run_payday()
+        self.run_payday()   # second
 
         expected = [ { "date": datetime.date.today().strftime('%Y-%m-%d')
+                     , "npatrons": 1 # most recent first
+                     , "receipts": 5.00
+                      }
+                   , { "date": datetime.date.today().strftime('%Y-%m-%d')
+                     , "npatrons": 2
+                     , "receipts": 3.00
+                      }
+                    ]
+        actual = json.loads(TestClient().get('/carl/charts.json').body)
+
+        assert actual == expected
+
+    def test_sandwiched_tipless_payday_comes_through(self):
+        alice, bob = self.make_participants_and_tips()
+        self.run_payday()   # zeroth, ignored
+        self.run_payday()   # first
+
+        # Oops! Sorry, Carl. :-(
+        alice.set_tip_to('carl', '0.00')
+        bob.set_tip_to('carl', '0.00')
+        self.run_payday()   # second
+
+        # Bouncing back ...
+        alice.set_tip_to('carl', '5.00')
+        self.run_payday()   # third
+
+        expected = [ { "date": datetime.date.today().strftime('%Y-%m-%d')
+                     , "npatrons": 1 # most recent first
+                     , "receipts": 5.00
+                      }
+                   , { "date": datetime.date.today().strftime('%Y-%m-%d')
+                     , "npatrons": 0
+                     , "receipts": 0.00
+                      }
+                   , { "date": datetime.date.today().strftime('%Y-%m-%d')
+                     , "npatrons": 2
+                     , "receipts": 3.00
+                      }
+                    ]
+        actual = json.loads(TestClient().get('/carl/charts.json').body)
+
+        assert actual == expected
+
+    def test_out_of_band_transfer_gets_included_with_prior_payday(self):
+        alice, bob = self.make_participants_and_tips()
+        self.run_payday()   # zeroth, ignored
+        self.run_payday()   # first
+        self.run_payday()   # second
+
+        # Do an out-of-band transfer.
+        self.db.run("UPDATE participants SET balance=balance - 4 WHERE username='alice'")
+        self.db.run("UPDATE participants SET balance=balance + 4 WHERE username='carl'")
+        self.db.run("INSERT INTO transfers (tipper, tippee, amount) VALUES ('alice', 'carl', 4)")
+
+        self.run_payday()   # third
+
+        expected = [ { "date": datetime.date.today().strftime('%Y-%m-%d')
+                     , "npatrons": 2 # most recent first
+                     , "receipts": 3.00
+                      }
+                   , { "date": datetime.date.today().strftime('%Y-%m-%d')
+                     , "npatrons": 3  # Since this is rare, don't worry that we double-count alice.
+                     , "receipts": 7.00
+                      }
+                   , { "date": datetime.date.today().strftime('%Y-%m-%d')
                      , "npatrons": 2
                      , "receipts": 3.00
                       }
