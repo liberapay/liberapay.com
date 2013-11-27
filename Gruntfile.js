612b71bb108dcf6aff2a716f48f15e19b5fc5a6a
rummik <r@9k1.us>
2013-11-26 23:04:38 -0500

    Add Karma options to Gruntfile, update package.json with new dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,19 @@ module.exports = function(grunt) {
 
             js: {
                 files: '<%= jshint.js %>',
-                tasks: 'jshint:js'
+                tasks: ['jshint:js', 'karma:tests:run']
+            },
+
+            tests: {
+                files: '<%= jshint.tests %>',
+                tasks: ['jshint:tests', 'karma:tests:run']
             }
         },
 
         jshint: {
             gruntfile: 'Gruntfile.js',
             js: 'js/**/*.{js,json}',
+            tests: 'jstests/**/*.js',
 
             options: {
                 jshintrc: '.jshintrc',
@@ -31,12 +37,41 @@ module.exports = function(grunt) {
                     alert: true
                 }
             }
+        },
+
+        karma: {
+            tests: {
+                hostname: '0.0.0.0'
+            },
+
+            singlerun: {
+                singleRun: true
+            },
+
+            options: {
+                browsers: ['PhantomJS'],
+                reporters: 'dots',
+                frameworks: ['mocha', 'browserify'],
+                urlRoot: '/karma/',
+                proxies: { '/': 'http://127.0.0.1:8537/' },
+                files: [
+                    'www/assets/jquery-1.8.3.min.js',
+                    'www/assets/%version/utils.js',
+                    'jstests/**/test_*.js',
+                ],
+
+                browserify: { watch: true },
+                preprocessors: {
+                    'jstests/**/*.js': ['browserify']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-karma');
 
     grunt.registerTask('default', ['test']);
-    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('test', ['jshint', 'karma:singlerun']);
 };
