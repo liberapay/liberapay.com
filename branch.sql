6e13d61b767c357f72d44f8b664930a3a5b50181
Changaco <changaco@changaco.net>
2013-12-06 21:56:15 +0100

    put the SQL schema changes in a transaction

--- branch.sql
+++ branch.sql
@@ -1,7 +1,8 @@
 -------------------------------------------------------------------------------
 -- https://github.com/gittip/www.gittip.com/issues/1683
 
-ALTER TABLE participants RENAME COLUMN anonymous TO anonymous_giving;
-ALTER TABLE participants DROP COLUMN anonymous;
-ALTER TABLE participants ADD COLUMN anonymous_receiving bool NOT NULL DEFAULT FALSE;
-ALTER TABLE homepage_top_receivers ADD COLUMN anonymous boolean;
+BEGIN;
+    ALTER TABLE participants RENAME COLUMN anonymous TO anonymous_giving;
+    ALTER TABLE participants ADD COLUMN anonymous_receiving bool NOT NULL DEFAULT FALSE;
+    ALTER TABLE homepage_top_receivers ADD COLUMN anonymous boolean;
+END;
