58b3930f0bc04bba6e79443e69305ff66e99d953
Luke Strickland <luke@axxim.net>
2013-12-16 21:47:26 -0600

    Initial removal of MixPanel and Gaug.es

--- tests/test_associate.py
+++ tests/test_associate.py
@@ -21,7 +21,6 @@ class Tests(Harness):
 
     @mock.patch('requests.post')
     @mock.patch('requests.get')
-    @mock.patch('gittip.utils.mixpanel.track')
     def test_associate_opts_in(self, track, get, post):
         self.website.oauth_cache = {"deadbeef": ("deadbeef", "opt-in", "")}
 
@@ -39,7 +38,6 @@ class Tests(Harness):
 
     @mock.patch('requests.post')
     @mock.patch('requests.get')
-    @mock.patch('gittip.utils.mixpanel.track')
     def test_associate_connects(self, track, get, post):
         self.make_participant('alice')
         self.website.oauth_cache = {"deadbeef": ("deadbeef", "connect", "")}
@@ -62,7 +60,6 @@ class Tests(Harness):
 
     @mock.patch('requests.post')
     @mock.patch('requests.get')
-    @mock.patch('gittip.utils.mixpanel.track')
     def test_associate_confirms_on_connect(self, track, get, post):
         TwitterAccount(self.db, '1234', {'screen_name': 'alice'}).opt_in('alice')
 
@@ -83,7 +80,6 @@ class Tests(Harness):
 
     @mock.patch('requests.post')
     @mock.patch('requests.get')
-    @mock.patch('gittip.utils.mixpanel.track')
     def test_confirmation_properly_displays_remaining_bitbucket(self, track, get, post):
         alice, foo = TwitterAccount(self.db, '1234', {'screen_name': 'alice'}).opt_in('alice')
         alice.participant.take_over(BitbucketAccount(self.db, '1234', {'username': 'alice_bb'}))
