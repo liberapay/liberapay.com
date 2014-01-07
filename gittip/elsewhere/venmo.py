c3d964465b02a64d4373d083c4d38a8d43cb0df0
Thomas Boyt <thomas.boyt@venmo.com>
2014-01-07 11:23:43 -0500

    Add venmo api keys for dev, began associate

--- gittip/elsewhere/venmo.py
+++ gittip/elsewhere/venmo.py
@@ -1,5 +1,6 @@
 from gittip.elsewhere import AccountElsewhere
 from urllib import urlencode
+import requests
 
 
 class VenmoAccount(AccountElsewhere):
@@ -12,9 +13,19 @@ def oauth_url(website):
     connect_params = {
         'client_id': website.venmo_client_id,
         'scope': 'make_payments',
-        'redirect_uri': website.venmo_callback
+        'redirect_uri': website.venmo_callback,
+        'response_type': 'code'
     }
     url = u"https://api.venmo.com/v1/oauth/authorize?{}".format(
         urlencode(connect_params)
     )
     return url
+
+def oauth_dance(website, qs):
+    data = {
+        'code': qs['code'].encode('US-ASCII'),
+        'client_id': website.venmo,
+        'client_secret': website.venmo_client_secret
+    }
+    r = requests.post("https://github.com/login/oauth/access_token", data=data)
+    assert r.status_code == 200, (r.status_code, r.text)
