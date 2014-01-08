a2271da72d97a3d9255f864fda474fa1f59a5c6e
Kamil Gałuszka <galuszkak@gmail.com>
2014-01-08 14:18:42 +0100

    added suggestions to code

--- js/gittip/upgrade.js
+++ js/gittip/upgrade.js
@@ -2,36 +2,10 @@ Gittip.upgrade = {};
 
 Gittip.upgrade.init = function () {
 
-    var browserSupport = new Gittip.upgrade.BrowserSupport();
-    browserSupport.isSupportedBrowser();
-};
+    var userAgent = navigator.userAgent.toLowerCase();
+    var browser = (userAgent.indexOf('msie') != -1) ? parseInt(userAgent.split('msie')[1]) : -1;
 
-Gittip.upgrade.BrowserSupport = function () {
-    this.userAgent = navigator.userAgent.toLowerCase();
-}
-
-Gittip.upgrade.BrowserSupport.prototype = {
-    /*
-    If method return -1 then this isn't IE browser.
-    Also this work only to IE10. IE 11 has different user agent. (notice for future for gittip)
-     */
-    detectIEVersion: function () {
-        return (this.userAgent.indexOf('msie') != -1) ? parseInt(this.userAgent.split('msie')[1]) : -1;
-    },
-
-    detectBrowser: function () {
-        this.browser = this.detectIEVersion();
-    },
-
-    isSupportedBrowser: function () {
-        this.detectBrowser()
-        // is this old IE?
-        if(this.browser != -1 && this.browser < 9){
-            this.showMessage();
-        }
-    },
-
-    showMessage: function () {
+    if(browser != -1 && browser < 9) {
         var message = '' +
             '<div id="upgrade_browser">' +
             '   <div class="container">' +
@@ -42,6 +16,6 @@ Gittip.upgrade.BrowserSupport.prototype = {
             '</div>';
         $("body").prepend(message);
     }
-}
+};
 
 $(document).ready(Gittip.upgrade.init);
