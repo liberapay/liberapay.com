dcc2f4e4ff89728cc3936816837aa71e8ec7de99
Simon Weber <simon.weber@venmo.com>
2014-01-10 11:02:53 -0500

    wrap schema changes in transaction

--- branch.sql
+++ branch.sql
@@ -1,3 +1,5 @@
-ALTER TABLE elsewhere ADD COLUMN access_token text DEFAULT NULL;
-ALTER TABLE elsewhere ADD COLUMN refresh_token text DEFAULT NULL;
-ALTER TABLE elsewhere ADD COLUMN expires timestamp with time zone DEFAULT NULL;
+BEGIN;
+    ALTER TABLE elsewhere ADD COLUMN access_token text DEFAULT NULL;
+    ALTER TABLE elsewhere ADD COLUMN refresh_token text DEFAULT NULL;
+    ALTER TABLE elsewhere ADD COLUMN expires timestamp with time zone DEFAULT NULL;
+END;
