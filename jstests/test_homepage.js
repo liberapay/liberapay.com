8dfa29e5fbe347daf32c69ab441dce4c828742d2
Sean Linsley <xovatdev@gmail.com>
2014-02-01 14:18:35 -0600

    Gittip.trim: strip Unicode and non-printing characters
    Resolves #1968
    TODO: make tests operational

--- jstests/test_homepage.js
+++ jstests/test_homepage.js
@@ -1,9 +1,9 @@
 var should = require('should');
 
 var $body, $iframe = $('<iframe/>').appendTo('body');
-describe('Test the homepage', function () {
+describe('The homepage', function() {
 
-    beforeEach(function (done) {
+    beforeEach(function(done) {
         this.timeout(10000);
         $iframe.attr('src', '/')
             .one('load', function() {
@@ -12,9 +12,15 @@ describe('Test the homepage', function () {
             });
     });
 
-    it('should render copy correctly', function () {
+    it('should render copy correctly', function() {
         $body.find('h2.top span').text().should.contain('Sustainable Crowdfunding');
         $body.find('h1 span').text().should.contain('Inspiring Generosity');
+    });
+
+    describe('Jump Box (Who inspires you?)', function() {
+        it('should let you search for users through their connected accounts');
+        it('should strip obviously invalid characters before submitting');
+        it('should strip leading and trailing whitespace before submitting');
     });
 
 });
---
+++ jstests/test_trim.js
@@ -0,0 +1,5 @@
+describe('Gittip.trim', function() {
+    it('should strip all Unicode');
+    it('should strip non-printable ASCII');
+    it('should trim leading and trailing whitespace');
+});
