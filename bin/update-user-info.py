1e94bab03fa39c7b95c13af89b9c1b9f70dfd5b5
Zbynek Winkler <zbynek.winkler@gmail.com>
2014-02-14 20:42:36 +0100

    Add __future__, decrease sleep time.

--- bin/update-user-info.py
+++ bin/update-user-info.py
@@ -4,6 +4,7 @@
 This could be generalized for #900.
 
 """
+from __future__ import absolute_import, division, print_function, unicode_literals
 import os
 import time
 import sys
@@ -36,7 +37,7 @@ def update_twitter():
 
         nremaining = int(response.headers['X-RATE-LIMIT-REMAINING'])
         reset = int(response.headers['X-RATE-LIMIT-RESET'])
-        print nremaining, reset, time.time()
+        print(nremaining, reset, time.time())
 
 
         if response.status_code != 200:
@@ -45,7 +46,7 @@ def update_twitter():
             # ========================
             # Supposedly we shouldn't hit 429, at least.
 
-            print response.status_code, response.text
+            print(response.status_code, response.text)
 
         else:
 
@@ -65,7 +66,7 @@ def update_twitter():
 
                     c.one("UPDATE elsewhere SET user_info=%s WHERE user_id=%s AND platform='twitter' RETURNING id", (user_info, user_id))
 
-                    print "updated {} ({})".format(user_info['screen_name'], user_id)
+                    print("updated {} ({})".format(user_info['screen_name'], user_id))
 
 
         # Stay under our rate limit.
@@ -83,11 +84,13 @@ def update_twitter():
 def update_github():
     elsewhere = db.all("SELECT user_id FROM ELSEWHERE WHERE platform='github' ORDER BY id;")
     url = "https://api.github.com/user/%s"
+    client_id = os.environ.get('GITHUB_CLIENT_ID')
+    client_secret = os.environ.get('GITHUB_CLIENT_SECRET')
 
     for user_id in elsewhere:
         response = requests.get(url % user_id, params={
-            'client_id': os.environ.get('GITHUB_CLIENT_ID'),
-            'client_secret': os.environ.get('GITHUB_CLIENT_SECRET')
+            'client_id': client_id,
+            'client_secret': client_secret
         })
 
         # Log the rate-limit.
@@ -95,10 +98,9 @@ def update_github():
 
         nremaining = int(response.headers['X-RATELIMIT-REMAINING'])
         reset = int(response.headers['X-RATELIMIT-RESET'])
-        # reset is supposed to be in UTC but it is not?
         # https://developer.github.com/v3/#rate-limiting
-        now = time.time() # time.timezone
-        print(nremaining, reset, now, reset-now)
+        now = time.time()
+        print(nremaining, reset, now, reset-now, end=' ')
 
         status = response.status_code
 
@@ -122,7 +124,7 @@ def update_github():
             # some other problem
             print(response.status_code, response.text)
 
-        sleep_for = 5
+        sleep_for = 0.5
         if nremaining == 0:
             sleep_for = reset - time.time()
             sleep_for += 10  # Account for potential clock skew between us and them
