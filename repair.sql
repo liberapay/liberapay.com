16c78d64c577c19517548aaa1a776ec890c8e3f8
Chad Whitacre <chad@zetaweb.com>
2014-02-15 01:18:27 -0500

    Prune repair.sql from #1971

--- repair.sql
+++
@@ -1,8 +0,0 @@
-update elsewhere 
-set is_locked = false 
-where exists (
-    select * from 
-    participants 
-    where username=participant 
-    and claimed_time is not null
-) and is_locked = true;
