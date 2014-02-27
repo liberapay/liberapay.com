e75e0946d166e27a3a3e8ae34fd4ae3d9cd640d3
Chad Whitacre <chad@zetaweb.com>
2014-02-27 16:38:17 -0500

    Append branch.sql to schema.sql; #2006
    This time we also had a branch.py. I've noted where that was run.

--- schema.sql
+++ schema.sql
@@ -1159,3 +1159,26 @@ BEGIN;
         WITH FUNCTION load_participant_for_elsewhere(elsewhere);
 
 END;
+
+
+-------------------------------------------------------------------------------
+-- https://github.com/gittip/www.gittip.com/pull/2006
+
+BEGIN;
+    CREATE TABLE events
+    ( id        serial      PRIMARY KEY
+    , ts        timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP
+    , type      text        NOT NULL
+    , payload   json
+     );
+
+    CREATE INDEX events_ts ON events(ts ASC);
+    CREATE INDEX events_type ON events(type);
+
+    /* ran branch.py here, took forever :-/ */
+
+    DROP RULE log_api_key_changes ON participants;
+    DROP RULE log_goal_changes ON participants;
+    DROP TABLE goals, api_keys;
+
+END;
