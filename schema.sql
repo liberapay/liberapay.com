3858a959d14c8677f853d3c062a11b47a5d760d4
Chad Whitacre <chad@zetaweb.com>
2014-02-27 18:20:13 -0500

    Append branch.sql to schema.sql

--- schema.sql
+++ schema.sql
@@ -1182,3 +1182,14 @@ BEGIN;
     DROP TABLE goals, api_keys;
 
 END;
+
+
+-------------------------------------------------------------------------------
+-- https://github.com/gittip/www.gittip.com/pull/1675
+
+BEGIN;
+    ALTER TABLE participants ADD COLUMN paypal_fee_cap numeric(35,2);
+
+    -- Update not needed in dev.
+    -- UPDATE participants SET paypal_fee_cap=20 WHERE paypal_email IS NOT NULL;
+END;
