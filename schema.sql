9fd3cf842fb08e493040b156b4077253179c533c
Luke Strickland <luke@axxim.net>
2014-04-21 19:07:35 -0500

    Append email types and field to schema.sql; #2305

--- schema.sql
+++ schema.sql
@@ -1214,3 +1214,38 @@ CREATE INDEX transfers_tipper_tippee_timestamp_idx
   ON transfers
   USING btree
   (tipper, tippee, timestamp DESC);
+
+
+-------------------------------------------------------------------------------
+-- https://github.com/gittip/www.gittip.com/pull/2305
+
+BEGIN;
+    CREATE TYPE email_address_with_confirmation AS
+    (
+        address text,
+        confirmed boolean
+    );
+
+    ALTER TABLE participants ADD email email_address_with_confirmation
+        DEFAULT NULL;
+
+    CREATE TABLE emails
+    ( id            serial                          PRIMARY KEY
+    , email         email_address_with_confirmation NOT NULL
+    , ctime         timestamp with time zone        NOT NULL
+                                                     DEFAULT CURRENT_TIMESTAMP
+    , participant   text                            NOT NULL
+                                                     REFERENCES participants
+                                                     ON UPDATE CASCADE
+                                                     ON DELETE RESTRICT
+     );
+
+
+    CREATE RULE log_email_changes AS ON UPDATE
+        TO participants WHERE (OLD.email IS NULL AND NOT NEW.email IS NULL)
+                        OR (NEW.email IS NULL AND NOT OLD.email IS NULL)
+                        OR NEW.email <> OLD.email
+        DO INSERT INTO emails (email, participant)
+                VALUES (NEW.email, OLD.username);
+
+END;
