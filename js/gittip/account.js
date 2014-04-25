ccd1bb9d0f07356db6f68ca96815d3179dfe00f0
Luke Strickland <luke@axxim.net>
2014-04-24 21:48:18 -0500

    Moving initial data over to the account tab

---
+++ js/gittip/account.js
@@ -0,0 +1,178 @@
+Gittip.account = {};
+
+Gittip.account.init = function() {
+
+    // Wire up username knob.
+    // ======================
+
+    $('form.username button.edit').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('.username button.edit').hide();
+        $('.username button.save').show();
+        $('.username button.cancel').show();
+        $('.username span.view').hide();
+        $('.username input').show().focus();
+        $('.username .warning').show();
+        return false;
+    });
+    $('form.username').submit(function(e) {
+        e.preventDefault();
+
+        $('#save-username').text('Saving ...');
+
+        function success(d) {
+            window.location.href = "/" + encodeURIComponent(d.username) + "/";
+        }
+        function error(e) {
+            $('#save-username').text('Save');
+            Gittip.notification(JSON.parse(e.responseText).error_message_long, 'error');
+        }
+        jQuery.ajax(
+            { url: "username.json"
+            , type: "POST"
+            , dataType: 'json'
+            , data: { username: $('input[name=username]').val() }
+            , success: success
+            , error: error
+             }
+        );
+        return false;
+    });
+    $('.username button.cancel').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        finish_editing_username();
+        return false;
+    });
+    function finish_editing_username() {
+        $('.username button.edit').show();
+        $('.username button.save').hide();
+        $('.username button.cancel').hide();
+        $('.username span.view').show();
+        $('.username input').hide();
+        $('.username .warning').hide();
+    }
+
+    // Wire up aggregate giving knob.
+    // ==============================
+
+    $('.anonymous-giving input').click(function() {
+        jQuery.ajax(
+            { url: 'anonymous.json'
+            , type: 'POST'
+            , data: {toggle: 'giving'}
+            , dataType: 'json'
+            , success: function(data) {
+                $('.anonymous-giving input').attr('checked', data.giving);
+            }
+            , error: function() {
+                    Gittip.notification("Failed to change your anonymity preference. Please try again.", 'error');
+                }
+             }
+        );
+    });
+
+
+    // Wire up aggregate receiving knob.
+    // ==============================
+
+    $('.anonymous-receiving input').click(function() {
+        jQuery.ajax(
+            { url: 'anonymous.json'
+            , type: 'POST'
+            , data: {toggle: 'receiving'}
+            , dataType: 'json'
+            , success: function(data) {
+                $('.anonymous-receiving input').attr('checked', data.receiving);
+            }
+            , error: function() {
+                    Gittip.notification("Failed to change your anonymity preference. Please try again.", 'error');
+                }
+             }
+        );
+    });
+
+    // Wire up API Key
+    // ===============
+    //
+    $('.api-key')
+        .data('callback', function (data) {
+            $('.api-key span').text(data.api_key || 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+
+            if (data.api_key) {
+                $('.api-key').data('api-key', data.api_key);
+                $('.api-key .show')
+                    .text('Hide Key')
+                    .toggleClass('show hide');
+            }
+            else
+                $('.api-key .hide')
+                    .text('Show Key')
+                    .toggleClass('show hide');
+        })
+        .on('click', '.show', function () {
+            if ($('.api-key').data('api-key'))
+                return $('.api-key').data('callback')({ api_key: $('.api-key').data('api-key') });
+
+            $.get('api-key.json', { action: 'show' }, $('.api-key').data('callback'));
+        })
+        .on('click', '.hide', $('.api-key').data('callback'))
+        .on('click', '.recreate', function () {
+            $.post('api-key.json', { action: 'show' }, $('.api-key').data('callback'));
+        });
+
+    // Wire up email address input.
+    // ============================
+    $('.email').on("click", ".toggle-email", function()
+    {
+        $('.email').toggle();
+        $('input.email').focus();
+    });
+
+    // Wire up email form.
+    $('.email-submit')
+        .on('click', '[type=submit]', function () {
+            var $this = $(this);
+
+            $this.text('Saving...');
+
+            function success(data) {
+                $('.email-address').text(data.email);
+                $('.email').toggle();
+                if (data.email === '') {
+                    html = "<span class=\"none\">None</span>"
+                    html += "<button class=\"toggle-email\">+ Add</button>";
+                } else {
+                    html = "<a class=\"email-address\" href=\"javascript:;\">" + data.email + "</a>";
+                    html += "<button class=\"toggle-email\">Edit</button>";
+                }
+                $('div.email').html(html);
+                $this.text('Save');
+            }
+
+            $.ajax({
+                    url: "email.json",
+                    type: "POST",
+                    dataType: 'json',
+                    success: success,
+                    error: function (data) {
+                        $this.text('Save');
+                        Gittip.notification('Failed to save your email address. '
+                                          + 'Please try again.', 'error');
+                    },
+                    data: {
+                        email: $('input.email').val()
+                    }
+                }
+            )
+
+            return false;
+        })
+        .on('click', '[type=cancel]', function () {
+            $('.email').toggle();
+
+            return false;
+        });
+
+};
