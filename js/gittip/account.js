fb46363c62cd7a8fd8f97856c0623ecf5e8f8015
Sean Linsley <xovatdev@gmail.com>
2014-04-26 15:53:20 -0500

    reformat & restructure the worst parts of account.js

--- js/gittip/account.js
+++ js/gittip/account.js
@@ -67,10 +67,9 @@ Gittip.account.init = function() {
                 $('.anonymous-giving input').attr('checked', data.giving);
             }
             , error: function() {
-                    Gittip.notification("Failed to change your anonymity preference. Please try again.", 'error');
-                }
-             }
-        );
+                Gittip.notification("Failed to change your anonymity preference. Please try again.", 'error');
+            }
+        });
     });
 
 
@@ -87,92 +86,81 @@ Gittip.account.init = function() {
                 $('.anonymous-receiving input').attr('checked', data.receiving);
             }
             , error: function() {
-                    Gittip.notification("Failed to change your anonymity preference. Please try again.", 'error');
-                }
-             }
-        );
+                Gittip.notification("Failed to change your anonymity preference. Please try again.", 'error');
+            }
+        });
     });
 
     // Wire up API Key
     // ===============
     //
-    $('.api-key')
-        .data('callback', function (data) {
-            $('.api-key span').text(data.api_key || 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+    var callback = function(data) {
+        $('.api-key span').text(data.api_key || 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
 
-            if (data.api_key) {
-                $('.api-key').data('api-key', data.api_key);
-                $('.api-key .show')
-                    .text('Hide Key')
-                    .toggleClass('show hide');
-            }
-            else
-                $('.api-key .hide')
-                    .text('Show Key')
-                    .toggleClass('show hide');
-        })
-        .on('click', '.show', function () {
-            if ($('.api-key').data('api-key'))
-                return $('.api-key').data('callback')({ api_key: $('.api-key').data('api-key') });
+        if (data.api_key) {
+            $('.api-key').data('api-key', data.api_key);
+            $('.api-key .show').text('Hide Key').toggleClass('show hide');
+        } else
+            $('.api-key .hide').text('Show Key').toggleClass('show hide');
+    }
 
-            $.get('api-key.json', { action: 'show' }, $('.api-key').data('callback'));
-        })
-        .on('click', '.hide', $('.api-key').data('callback'))
-        .on('click', '.recreate', function () {
-            $.post('api-key.json', { action: 'show' }, $('.api-key').data('callback'));
-        });
+    $('.api-key').on('click', '.show', function() {
+        if ($('.api-key').data('api-key'))
+            callback({api_key: $('.api-key').data('api-key')});
+        else
+            $.get('api-key.json', {action: 'show'}, callback);
+    })
+    .on('click', '.hide', callback)
+    .on('click', '.recreate', function() {
+        $.post('api-key.json', {action: 'show'}, callback);
+    });
 
     // Wire up email address input.
     // ============================
-    $('.email').on("click", ".toggle-email", function()
-    {
+    $('.email').on("click", ".toggle-email", function() {
         $('.email').toggle();
         $('input.email').focus();
     });
 
     // Wire up email form.
-    $('.email-submit')
-        .on('click', '[type=submit]', function () {
-            var $this = $(this);
+    $('.email-submit').on('click', '[type=submit]', function() {
+        var $this = $(this);
 
-            $this.text('Saving...');
+        $this.text('Saving...');
 
-            function success(data) {
-                $('.email-address').text(data.email);
-                $('.email').toggle();
-                if (data.email === '') {
-                    html = "<span class=\"none\">None</span>"
-                    html += "<button class=\"toggle-email\">+ Add</button>";
-                } else {
-                    html = "<a class=\"email-address\" href=\"javascript:;\">" + data.email + "</a>";
-                    html += "<button class=\"toggle-email\">Edit</button>";
-                }
-                $('div.email').html(html);
-                $this.text('Save');
-            }
-
-            $.ajax({
-                    url: "email.json",
-                    type: "POST",
-                    dataType: 'json',
-                    success: success,
-                    error: function (data) {
-                        $this.text('Save');
-                        Gittip.notification('Failed to save your email address. '
-                                          + 'Please try again.', 'error');
-                    },
-                    data: {
-                        email: $('input.email').val()
-                    }
-                }
-            )
-
-            return false;
-        })
-        .on('click', '[type=cancel]', function () {
+        function success(data) {
+            $('.email-address').text(data.email);
             $('.email').toggle();
+            if (data.email === '') {
+                html = '<span class="none">None</span>'
+                html += '<button class="toggle-email">+ Add</button>';
+            } else {
+                html = '<a class="email-address" href="javascript:;">' + data.email + '</a>';
+                html += '<button class="toggle-email">Edit</button>';
+            }
+            $('div.email').html(html);
+            $this.text('Save');
+        }
 
-            return false;
-        });
+        $.ajax({
+            url: 'email.json',
+            type: 'POST',
+            dataType: 'json',
+            success: success,
+            error: function (data) {
+                $this.text('Save');
+                Gittip.notification('Failed to save your email address. '
+                                  + 'Please try again.', 'error');
+            },
+            data: {email: $('input.email').val()}
+        })
+
+        return false;
+    })
+    .on('click', '[type=cancel]', function () {
+        $('.email').toggle();
+
+        return false;
+    });
 
 };
