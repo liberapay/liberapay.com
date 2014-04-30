a7370105b52739909af3c1e9630939cc07493071
Changaco <changaco@changaco.oy.lc>
2014-04-30 17:00:16 +0200

    make sure the transfer was actually a team take

--- branch.sql
+++ branch.sql
@@ -2,8 +2,23 @@ BEGIN;
 
     ALTER TABLE transfers ADD COLUMN as_team_member boolean NOT NULL DEFAULT false;
 
-    UPDATE transfers SET as_team_member = true
-        FROM participants WHERE participants.username = transfers.tipper
-                            AND participants.number = 'plural';
+    UPDATE transfers
+       SET as_team_member = true
+     WHERE amount <= (
+               SELECT amount
+                 FROM takes
+                WHERE takes.team = transfers.tipper
+                  AND takes.member = transfers.tippee
+                  AND takes.ctime < transfers.timestamp
+             ORDER BY takes.ctime DESC
+                LIMIT 1
+           )
+       AND amount != (
+               SELECT amount
+                 FROM tips
+                WHERE tips.ctime < transfers.timestamp
+             ORDER BY tips.ctime DESC
+                LIMIT 1
+           );
 
 END;
