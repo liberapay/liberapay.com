697e7b0e584a18c287f4de7f71b65a51aee40c6a
Changaco <changaco@changaco.oy.lc>
2014-05-07 23:19:37 +0200

    prevent connecting a team account without having another account to log in

--- gittip/models/participant.py
+++ gittip/models/participant.py
@@ -888,6 +888,7 @@ class Participant(Model, MixinTeam):
 
                 SELECT participant
                      , claimed_time IS NULL AS is_stub
+                     , is_team
                   FROM elsewhere
                   JOIN participants ON participant=participants.username
                  WHERE elsewhere.platform=%s AND elsewhere.user_id=%s
@@ -937,6 +938,10 @@ class Participant(Model, MixinTeam):
                                        )
             assert nparticipants in (0, 1)  # sanity check
             we_already_have_that_kind_of_account = nparticipants == 1
+
+            if rec.is_team and we_already_have_that_kind_of_account:
+                if len(self.get_accounts_elsewhere()) == 1:
+                    raise Response(400)
 
             need_confirmation = NeedConfirmation( other_is_a_real_participant
                                                 , this_is_others_last_account_elsewhere
