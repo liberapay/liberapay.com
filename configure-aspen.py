7e269e799eccb7ef5127e92bf72023e2e7d1006a
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-05-09 05:37:25 +0000

    DNT code pushed into util.

--- configure-aspen.py
+++ configure-aspen.py
@@ -10,7 +10,7 @@ import gittip
 import gittip.wireup
 from gittip import canonize
 from gittip.security import authentication, csrf, x_frame_options
-from gittip.utils import cache_static, timer
+from gittip.utils import cache_static, timer, do_not_track
 
 
 from aspen import log_dammit
@@ -147,6 +147,7 @@ algorithm.functions = [ timer.start
 
                       , canonize
                       , authentication.inbound
+                      , do_not_track.inbound
                       , csrf.inbound
                       , add_stuff_to_context
 
