163f0a647906de35ea827e6172603035995420d0
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2014-05-23 18:20:03 +0300

    Test refactored

--- tests/py/test_for_community_json.py
+++ tests/py/test_for_community_json.py
@@ -1,31 +1,27 @@
 from gittip.testing import Harness
 
 import json
+from gittip.models.community import slugize
 
 class TestForCommunityJson(Harness):
 
     def setUp(self):
         Harness.setUp(self)
 
-        self.community = 'Test'
         self.alice = self.make_participant('alice')
-
-        self.client.POST( '/for/communities.json'
-                           , {'name': self.community, 'is_member': 'true'}
-                           , auth_as='alice'
-                            )
+        self.alice.insert_into_communities(True, 'test', slugize('test'))
 
     def test_get_non_existing_community(self):
         response = self.client.GxT('/for/NonExisting/index.json')
         assert response.code == 404
 
     def test_get_existing_community(self):
-        response = self.client.GET('/for/Test/index.json')
+        response = self.client.GET('/for/test/index.json')
         result = json.loads(response.body)
 
         assert len(result["members"]) == 1
-        assert result["name"] == "Test"
+        assert result["name"] == "test"
 
     def test_post_not_supported(self):
-        response = self.client.PxST('/for/Test/index.json')
+        response = self.client.PxST('/for/test/index.json')
         assert response.code == 405
