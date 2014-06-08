7831634897871dac10accff53b125f63e8f257aa
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2014-06-08 14:46:13 +0300

    Added pagination to community members REST API

--- tests/py/test_for_community_json.py
+++ tests/py/test_for_community_json.py
@@ -8,8 +8,12 @@ class TestForCommunityJson(Harness):
     def setUp(self):
         Harness.setUp(self)
 
-        self.alice = self.make_participant('alice')
-        self.alice.insert_into_communities(True, 'test', slugize('test'))
+        self.add_participant('alice')
+        self.add_participant('bob')
+
+    def add_participant(self, participant_name):
+        participant = self.make_participant(participant_name)
+        participant.insert_into_communities(True, 'test', slugize('test'))
 
     def test_get_non_existing_community(self):
         response = self.client.GxT('/for/NonExisting/index.json')
@@ -19,9 +23,31 @@ class TestForCommunityJson(Harness):
         response = self.client.GET('/for/test/index.json')
         result = json.loads(response.body)
 
-        assert len(result["members"]) == 1
+        assert len(result["members"]) == 2
         assert result["name"] == "test"
 
     def test_post_not_supported(self):
         response = self.client.PxST('/for/test/index.json')
         assert response.code == 405
+
+    def test_limit(self):
+        response = self.client.GET('/for/test/index.json?limit=1')
+        result = json.loads(response.body)
+
+        assert len(result["members"]) == 1
+
+    def test_offset(self):
+        response = self.client.GET('/for/test/index.json?offset=1')
+        result = json.loads(response.body)
+
+        assert len(result["members"]) == 1
+
+    def test_invalid_limit(self):
+        response = self.client.GxT('/for/test/index.json?limit=abc')
+
+        assert response.code == 400
+
+    def test_invalid_offset(self):
+        response = self.client.GxT('/for/test/index.json?offset=abc')
+
+        assert response.code == 400
