fbe6cc01d44e0153028c0dc2272b95ac8a0a05b8
Changaco <changaco@changaco.oy.lc>
2014-06-21 11:05:43 +0200

    failing test for #1469

--- tests/py/test_participant.py
+++ tests/py/test_participant.py
@@ -541,6 +541,16 @@ class Tests(Harness):
         alice.set_tip_to(bob, '3.00')
         assert Participant.from_username('bob').receiving == bob.receiving == Decimal('45.00')
 
+    def test_receiving_is_zero_for_patrons(self):
+        alice = self.make_participant('alice', claimed_time='now', last_bill_result='')
+        bob = self.make_participant('bob', claimed_time='now')
+        alice.set_tip_to(bob, '3.00')
+
+        bob.update_goal(Decimal('-1'))
+        assert bob.receiving == 0
+        alice = Participant.from_id(alice.id)
+        assert alice.giving == 0
+
 
     # pledging
 
