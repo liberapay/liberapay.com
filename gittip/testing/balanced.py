6c89f6258ec24ae9d5c3133906e1c806e69d9af0
Changaco <changaco@changaco.oy.lc>
2014-07-01 13:17:19 +0200

    initialize balanced only once

--- gittip/testing/balanced.py
+++ gittip/testing/balanced.py
@@ -3,46 +3,60 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import balanced
 
 from gittip.testing import Harness
+from gittip.testing.vcr import use_cassette
 
 
 class BalancedHarness(Harness):
 
-    @classmethod
-    def setUpClass(cls):
-        super(BalancedHarness, cls).setUpClass()
-        cls.balanced_api_key = balanced.APIKey().save().secret
-        balanced.configure(cls.balanced_api_key)
-        mp = balanced.Marketplace.my_marketplace
-        if not mp:
-            mp = balanced.Marketplace().save()
-        cls.balanced_marketplace = mp
-
-
     def setUp(self):
-        Harness.setUp(self)
-        self.alice = self.make_participant('alice', elsewhere='github')
+        self.david = self.make_participant('david', elsewhere='github',
+                                           balanced_customer_href=self.david_href)
+        self.janet = self.make_participant('janet', is_suspicious=False,
+                                           claimed_time='now',
+                                           balanced_customer_href=self.janet_href,
+                                           last_bill_result='')
+        self.homer = self.make_participant('homer', is_suspicious=False,
+                                           claimed_time='now',
+                                           balanced_customer_href=self.homer_href,
+                                           last_ach_result='')
 
-        self.balanced_customer_href = unicode(balanced.Customer().save().href)
-        self.card_href = unicode(balanced.Card(
-            number='4111111111111111',
-            expiration_month=10,
-            expiration_year=2020,
-            address={
-                'line1': "123 Main Street",
-                'state': 'Confusion',
-                'postal_code': '90210',
-            },
-            # gittip stores some of the address data in the meta fields,
-            # continue using them to support backwards compatibility
-            meta={
-                'address_2': 'Box 2',
-                'city_town': '',
-                'region': 'Confusion',
-            }
-        ).save().href) # XXX Why don't we actually associate this with the customer? See XXX in
-                       # test_billing_payday.TestPaydayChargeOnBalanced.
-        self.bank_account_href = unicode(balanced.BankAccount(
-            name='Homer Jay',
-            account_number='112233a',
-            routing_number='121042882',
-        ).save().href)
+
+with use_cassette('BalancedHarness'):
+    cls = BalancedHarness
+    balanced.configure(balanced.APIKey().save().secret)
+    mp = balanced.Marketplace.my_marketplace
+    if not mp:
+        mp = balanced.Marketplace().save()
+    cls.balanced_marketplace = mp
+
+    cls.david_href = cls.make_balanced_customer()
+
+    cls.janet_href = cls.make_balanced_customer()
+    cls.card = balanced.Card(
+        number='4111111111111111',
+        expiration_month=10,
+        expiration_year=2020,
+        address={
+            'line1': "123 Main Street",
+            'state': 'Confusion',
+            'postal_code': '90210',
+        },
+        # gittip stores some of the address data in the meta fields,
+        # continue using them to support backwards compatibility
+        meta={
+            'address_2': 'Box 2',
+            'city_town': '',
+            'region': 'Confusion',
+        }
+    ).save()
+    cls.card.associate_to_customer(cls.janet_href)
+    cls.card_href = unicode(cls.card.href)
+
+    cls.homer_href = cls.make_balanced_customer()
+    cls.bank_account = balanced.BankAccount(
+        name='Homer Jay',
+        account_number='112233a',
+        routing_number='121042882',
+    ).save()
+    cls.bank_account.associate_to_customer(cls.homer_href)
+    cls.bank_account_href = unicode(cls.bank_account.href)
