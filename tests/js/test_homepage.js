d99fffa001ece1ee7840f6ed56602de3eac051b6
Chad Whitacre <chad@zetaweb.com>
2014-07-11 15:37:58 -0400

    Fix up test suite

--- tests/js/test_homepage.js
+++ tests/js/test_homepage.js
@@ -4,22 +4,9 @@ module.exports = {
         test.expect(2);
 
         test.open('http://localhost:8537')
-            .assert.text('h2.top span').is('Sustainable Crowdfunding')
-            .assert.text('h1 span').is('Inspiring Generosity')
+            .assert.text('.pitch h1').is('Weekly Payments')
+            .assert.text('.action h1').is('Sign In')
             .done();
     },
-
-    'Jump Box (Who inspires you?)': function(test) {
-        test.expect(1);
-
-        test.open('http://localhost:8537')
-            .type('#jump input', '˚aø¶')
-            .submit('#jump')
-            .assert.url().is('http://localhost:8537/on/twitter/a/')
-            .done();
-    },
-
-    //'should strip leading and trailing whitespace before submitting': function(test) {},
-    //'should let you search for users through their connected accounts': function(test) {},
 
 };
