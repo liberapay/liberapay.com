31ea1a608df0de455c5d4e3894d98110f403e4b1
Chad Whitacre <chad@zetaweb.com>
2014-07-22 14:51:22 -0400

    Append branch.sql to schema.sql

--- branch.sql
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- https://github.com/gittip/www.gittip.com/pull/2544
-
-BEGIN;
-
-    DROP TABLE homepage_top_givers;
-    DROP TABLE homepage_top_receivers;
-
-END;
