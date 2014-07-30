034b3f2c90eaf08abe727ae5accd08ba2699ae20
Changaco <changaco@changaco.oy.lc>
2014-07-30 17:46:01 +0200

    remove the `database_maxconn` restriction from `payday()`

--- gittip/cli.py
+++ gittip/cli.py
@@ -7,13 +7,8 @@ def payday():
 
     # Wire things up.
     # ===============
-    # Manually override max db connections so that we only have one connection.
-    # Our db access is serialized right now anyway, and with only one
-    # connection it's easier to trust changes to statement_timeout. The point
-    # here is that we want to turn off statement_timeout for payday.
 
     env = wireup.env()
-    env.database_maxconn = 1
     db = wireup.db(env)
     db.run("SET statement_timeout = 0")
 
