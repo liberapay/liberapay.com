be8c6938fcfad80d95cc86e511667fc21bec02c0
oakes <zsoakes@gmail.com>
2014-08-01 16:22:06 -0400

    Support pluralization

--- gittip/utils/i18n.py
+++ gittip/utils/i18n.py
@@ -8,6 +8,31 @@ import babel.messages.pofile
 from babel.numbers import (
     format_currency, format_decimal, format_number, format_percent
 )
+import re
+
+
+def get_function_from_rule(rule):
+    oldrule = None
+    while oldrule != rule:
+        oldrule = rule
+        rule = re.sub('(.*)\?(.*):(.*)', r'(\1) and (\2) or (\3)', oldrule)
+    rule = re.sub('&&', 'and', rule)
+    rule = re.sub('\|\|', 'or', rule)
+    return eval('lambda n: ' + rule)
+
+
+def get_text(s, loc, count=0):
+    if loc in LANGS:
+        new_s = LANGS[loc].get(s)
+        s = new_s.string if new_s else s
+    if isinstance(s, tuple):
+        try:
+            plural_fn = get_function_from_rule(LANGS[loc].plural_expr)
+            i = int(plural_fn(count))
+            s = s[i] if len(s) >= i + 1 else s[0]
+        except:
+            return s[0]
+    return s
 
 
 def to_age(dt, loc):
@@ -21,12 +46,7 @@ def load_langs(localeDir):
         if len(parts) == 2 and parts[1] == "po":
             lang = parts[0]
             with open(os.path.join(localeDir, file)) as f:
-                catalog = babel.messages.pofile.read_po(f)
-                catalog_dict = {}
-                for message in catalog:
-                    if message.id:
-                        catalog_dict[message.id] = message.string
-                langs[lang] = catalog_dict
+                langs[lang] = babel.messages.pofile.read_po(f)
     return langs
 
 
@@ -44,7 +64,7 @@ def get_locale_for_request(request):
 
 def inbound(request):
     loc = request.context.locale = get_locale_for_request(request)
-    request.context._ = lambda s: LANGS[loc].get(s, s) if loc in LANGS else s
+    request.context._ = lambda s, count=0: get_text(s, loc, count=count)
     request.context.format_number = lambda *a: format_number(*a, locale=loc)
     request.context.format_decimal = lambda *a: format_decimal(*a, locale=loc)
     request.context.format_currency = lambda *a: format_currency(*a, locale=loc)
