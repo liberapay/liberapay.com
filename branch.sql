f38a9bd4cc1877e6386d2c0fbb9926f1eb83b992
Changaco <changaco@changaco.oy.lc>
2014-08-01 14:50:08 +0200

    zero out known anomalous tip

--- branch.sql
+++ branch.sql
@@ -13,4 +13,7 @@ BEGIN;
        SET last_bill_result = NULL
      WHERE last_bill_result = 'NoResultFound()';
 
+    INSERT INTO tips (ctime, tipper, tippee, amount)
+        SELECT ctime, tipper, tippee, 0 FROM tips WHERE id = 46266;
+
 END;
