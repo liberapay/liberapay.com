2ada0289f4e6a15848f57cdf5b96d05846d24250
Changaco <changaco@changaco.oy.lc>
2014-08-03 11:51:19 +0200

    use a shorter `getCookie()` function

--- www/assets/%version/utils.js
+++ www/assets/%version/utils.js
@@ -19,22 +19,7 @@ if (!window.console)
 }
 
 
-// Make sure we have some things.
-// ==============================
-
-function getCookie(name)
-{
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-};
+function getCookie(key) {
+    var o = new RegExp("(?:^|; ?)" + escape(key) + "=([^;]+)").exec(document.cookie);
+    return o && unescape(o[1]);
+}
