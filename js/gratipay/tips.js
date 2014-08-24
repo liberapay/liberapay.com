0553c050940571aa5d9cfcc9477423d51655af2d
oakes <zsoakes@gmail.com>
2014-08-24 14:24:50 -0400

    Fix the decimal symbol in the tip input box and other places

--- js/gratipay/tips.js
+++ js/gratipay/tips.js
@@ -21,8 +21,8 @@ Gratipay.tips.init = function() {
         var $this     = $(this),
             $parent   = $this.parents('form'),
             $confirm  = $parent.find('.confirm-tip'),
-            amount    = parseFloat(unformat($this.val()), 10) || 0,
-            oldAmount = parseFloat(unformat(this.defaultValue), 10),
+            amount    = parseFloat(unlocalizeDecimal($this.val()), 10) || 0,
+            oldAmount = parseFloat(unlocalizeDecimal(this.defaultValue), 10),
             max       = parseFloat($this.prop('max')),
             min       = parseFloat($this.prop('min')),
             inBounds  = amount <= max && amount >= min,
@@ -96,8 +96,8 @@ Gratipay.tips.init = function() {
         event.preventDefault();
         var $this     = $(this),
             $myTip    = $this.find('.my-tip'),
-            amount    = parseFloat(unformat($myTip.val()), 10),
-            oldAmount = parseFloat(unformat($myTip[0].defaultValue), 10),
+            amount    = parseFloat(unlocalizeDecimal($myTip.val()), 10),
+            oldAmount = parseFloat(unlocalizeDecimal($myTip[0].defaultValue), 10),
             tippee    = $myTip.data('tippee'),
             isAnon    = $this.hasClass('anon');
 
@@ -162,14 +162,15 @@ Gratipay.tips.set = function(tippee, amount, callback) {
         if (callback) callback(data);
 
         // update display
-        $('.my-total-giving').text('$'+data.total_giving);
+        $('.my-total-giving').text('$' + localizeDecimal(data.total_giving));
         $('.total-receiving').text(
             // check and see if we are on our giving page or not
             new RegExp('/' + tippee + '/').test(window.location.href) ?
-                '$'+data.total_receiving_tippee : '$'+data.total_receiving);
+                '$' + localizeDecimal(data.total_receiving_tippee) :
+                '$' + localizeDecimal(data.total_receiving));
 
         // update quick stats
-        $('.quick-stats a').text('$' + data.total_giving + '/wk');
+        $('.quick-stats a').text('$' + localizeDecimal(data.total_giving) + '/wk');
     })
     .fail(function() {
         Gratipay.notification('Sorry, something went wrong while changing your tip. :(', 'error');
