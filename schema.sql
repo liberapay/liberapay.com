86017c11c22f59d84d875c301b9e149ab9fd937a
Changaco <changaco@changaco.oy.lc>
2014-08-28 19:40:33 +0200

    remove obsolete and broken transaction from schema.sql

--- schema.sql
+++ schema.sql
@@ -362,32 +362,3 @@ CREATE INDEX transfers_tipper_tippee_timestamp_idx
   ON transfers
   USING btree
   (tipper, tippee, timestamp DESC);
-
-
-------------------------------------------------------------------------------
--- https://github.com/gratipay/gratipay.com/pull/2682
-
-BEGIN;
-
-    CREATE INDEX communities_all ON communities (participant, slug, mtime DESC);
-
-    DROP VIEW community_summary;
-    DROP VIEW current_communities;
-
-    CREATE VIEW current_communities AS
-        SELECT DISTINCT ON (participant, slug) c.*
-          FROM communities c
-      ORDER BY participant, slug, mtime DESC;
-
-    CREATE VIEW community_summary AS
-        SELECT max(name) AS name -- gotta pick one, this is good enough for now
-             , slug
-             , count(participant) AS nmembers
-          FROM current_communities
-          JOIN participants p ON p.username = participant
-         WHERE is_member
-           AND p.is_suspicious IS NOT true
-      GROUP BY slug
-      ORDER BY nmembers DESC, slug;
-
-END;
