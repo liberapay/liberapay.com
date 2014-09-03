8f417b27549b258a38d90d44f2af79cc57e3a32b
Changaco <changaco@changaco.oy.lc>
2014-09-03 18:00:07 +0200

    only enforce the new constraint on future rows
    We can't enforce it on all existing rows because we have an anomalous row that we can't delete.

--- branch.sql
+++ branch.sql
@@ -1,4 +1,4 @@
 BEGIN;
     DELETE FROM transfers WHERE amount = 0 AND context = 'take-over';
-    ALTER TABLE transfers ADD CONSTRAINT positive CHECK (amount > 0);
+    ALTER TABLE transfers ADD CONSTRAINT positive CHECK (amount > 0) NOT VALID;
 END;
