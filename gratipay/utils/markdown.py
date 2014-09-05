d5843c3f53f00a04ed5785f51e4488f24e65ccd5
Chad Whitacre <chad@zetaweb.com>
2014-09-05 11:44:08 -0400

    Simplify module again
    Don't need the hijinks now that it's in it's own file.

--- gratipay/utils/markdown.py
+++ gratipay/utils/markdown.py
@@ -1,9 +1,7 @@
-import misaka  # http://misaka.61924.nl/
-
-m = misaka
+import misaka as m  # http://misaka.61924.nl/
 
 def render(markdown):
-    return misaka.html( markdown
-                      , extensions=m.EXT_AUTOLINK | m.EXT_STRIKETHROUGH
-                      , render_flags=m.HTML_SKIP_HTML | m.HTML_TOC | m.HTML_SMARTYPANTS
-                       )
+    return m.html( markdown
+                 , extensions=m.EXT_AUTOLINK | m.EXT_STRIKETHROUGH
+                 , render_flags=m.HTML_SKIP_HTML | m.HTML_TOC | m.HTML_SMARTYPANTS
+                  )
