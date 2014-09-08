5ba6cfe8e37e3cc22c32b66256efbfc759f75fa3
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2014-09-08 18:01:28 +0530

    JS now only allows numeric input, cuts off at maximum length

--- js/gratipay/payments.js
+++ js/gratipay/payments.js
@@ -217,7 +217,130 @@ Gratipay.payments.cc.init = function(balanced_uri, participantId) {
         Gratipay.havePayments = true;
         $('input[type!="hidden"]').eq(0).focus();
     });
+    Gratipay.payments.cc.formatInputs(
+        $('#card_number'),
+        $('#expiration_month'),
+        $('#expiration_year'),
+        $('#cvv')
+    );
 };
+
+Gratipay.payments.cc.formatInputs = function (cardNumberInput, expirationMonthInput, expirationYearInput, cvvInput) {    
+    function getInputValue(e, element) {
+        var inputValue = element.val().trim();
+        inputValue = inputValue + String.fromCharCode(e.which);
+        return inputValue.replace(/[^\d]/g, "");
+    };
+
+    function isEscapedKeyStroke(e) {
+        // Key event is for a browser shortcut
+        if (e.metaKey || e.ctrlKey) return true;
+
+        // If keycode is a space
+        if (e.which === 32) return false;
+
+        // If keycode is a special char (WebKit)
+        if (e.which === 0) return true;
+
+        // If char is a special char (Firefox)
+        if (e.which < 33) return true;
+
+        return false;
+    };
+
+    function isNumberEvent(e) {
+        return (/^\d+$/.test(String.fromCharCode(e.which)));
+    }
+
+    function onlyAllowNumeric(e, maximumLength, element) {
+        e.preventDefault();
+        // Ensure that it is a number and stop the keypress
+        if (!isNumberEvent(e)) {            
+            return false;
+        }
+        return true;
+    };
+
+    var isAmericanExpress = function(number) {
+        return number.match("^(34|37)");
+    };
+
+    function shouldProcessInput(e, maximumLength, element) {
+        var target = e.currentTarget;
+        if (getInputValue(e, element).length > maximumLength) {
+          e.preventDefault();
+          return false;
+        }
+        if ((target.selectionStart !== target.value.length)) {
+          return false;
+        }
+        return (!isEscapedKeyStroke(e)) && onlyAllowNumeric(e, maximumLength, element);
+    };
+
+    function addSpaces(number, spaces) {
+      var parts = []
+      var j = 0;
+      for (var i=0; i<spaces.length; i++) {
+        if (number.length > spaces[i]) {
+          parts.push(number.slice(j, spaces[i]));
+          j = spaces[i];
+        } else {
+          if (i < spaces.length) {
+            parts.push(number.slice(j, spaces[i]));
+          } else {
+            parts.push(number.slice(j));
+          }
+          break;
+        }
+      }
+
+      if (parts.length > 0) {
+        return parts.join(" ");
+      } else {
+        return number;
+      }
+    };
+
+    var americanExpressSpaces = [4, 10, 15];
+    var defaultSpaces = [4, 8, 12, 16];
+
+    cardNumberInput.on("keypress", function(e) {
+        var number = getInputValue(e, cardNumberInput);
+        var isAmericanExpressCard = isAmericanExpress(number);
+        var maximumLength = (isAmericanExpressCard ? 15 : 16);
+        if (shouldProcessInput(e, maximumLength, cardNumberInput)) {
+            var newInput;
+            newInput = isAmericanExpressCard ? addSpaces(number, americanExpressSpaces) : addSpaces(number, defaultSpaces);
+            cardNumberInput.val(newInput);            
+        }
+    });
+
+    expirationMonthInput.on("keypress", function(e) {
+        var maximumLength = 2;
+        if (shouldProcessInput(e, maximumLength, expirationMonthInput)) {
+            var newInput = getInputValue(e, expirationMonthInput);
+            newInput < 13 ? expirationMonthInput.val(newInput) : e.preventDefault();                        
+        }
+    });
+
+    expirationYearInput.on("keypress", function(e) {
+        var maximumLength = 2;
+        if (shouldProcessInput(e, maximumLength, expirationYearInput)) {
+            var newInput = getInputValue(e, expirationYearInput);
+            expirationYearInput.val(newInput);            
+        }
+    });
+
+    cvvInput.on("keypress", function(e) {
+        var number = getInputValue(e, cardNumberInput);
+        var isAmericanExpressCard = isAmericanExpress(number);
+        var maximumLength = (isAmericanExpressCard ? 4 : 3);
+        if (shouldProcessInput(e, maximumLength, cvvInput)) {
+            var newInput = getInputValue(e, cvvInput);
+            cvvInput.val(newInput);            
+        }
+    });
+}
 
 Gratipay.payments.cc.submit = function(e) {
 
