3ffe4c751ad87613f1a30d53cf1d5e88daf30541
Chad Whitacre <chad@zetaweb.com>
2014-09-18 17:05:20 -0400

    Failing test for setting exchange status TTW

--- tests/py/test_record_an_exchange.py
+++ tests/py/test_record_an_exchange.py
@@ -10,15 +10,15 @@ class TestRecordAnExchange(Harness):
     # fixture
     # =======
 
-    def record_an_exchange(self, amount, fee, note, make_participants=True):
+    def record_an_exchange(self, amount, fee, note, status=None, make_participants=True):
         if make_participants:
             now = utcnow()
             self.make_participant('alice', claimed_time=now, is_admin=True)
             self.make_participant('bob', claimed_time=now)
-        return self.client.PxST( '/bob/history/record-an-exchange'
-                               , {'amount': amount, 'fee': fee, 'note': note}
-                               , auth_as='alice'
-                                )
+        data = {'amount': amount, 'fee': fee, 'note': note}
+        if status is not None:
+            data['status'] = 'status'
+        return self.client.PxST('/bob/history/record-an-exchange', data, auth_as='alice')
 
     # tests
     # =====
@@ -30,7 +30,7 @@ class TestRecordAnExchange(Harness):
     def test_non_admin_is_404(self):
         self.make_participant('alice', claimed_time=utcnow())
         self.make_participant('bob', claimed_time=utcnow())
-        actual = self.record_an_exchange('10', '0', 'foo', False).code
+        actual = self.record_an_exchange('10', '0', 'foo', make_participants=False).code
         assert actual == 404
 
     def test_non_post_is_405(self):
@@ -85,7 +85,7 @@ class TestRecordAnExchange(Harness):
     def test_withdrawals_work(self):
         self.make_participant('alice', claimed_time=utcnow(), is_admin=True)
         self.make_participant('bob', claimed_time=utcnow(), balance=20)
-        self.record_an_exchange('-7', '0', 'noted', False)
+        self.record_an_exchange('-7', '0', 'noted', make_participants=False)
         expected = Decimal('13.00')
         SQL = "SELECT balance FROM participants WHERE username='bob'"
         actual = self.db.one(SQL)
@@ -94,6 +94,10 @@ class TestRecordAnExchange(Harness):
     def test_withdrawals_take_fee_out_of_balance(self):
         self.make_participant('alice', claimed_time=utcnow(), is_admin=True)
         self.make_participant('bob', claimed_time=utcnow(), balance=20)
-        self.record_an_exchange('-7', '1.13', 'noted', False)
+        self.record_an_exchange('-7', '1.13', 'noted', make_participants=False)
         SQL = "SELECT balance FROM participants WHERE username='bob'"
         assert self.db.one(SQL) == Decimal('11.87')
+
+    def test_can_set_status(self):
+        self.record_an_exchange('10', '0', 'noted', 'succeeded')
+        assert self.db.one("SELECT status FROM exchanges") == 'succeeded'
