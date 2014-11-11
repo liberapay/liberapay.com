eaf8708ce70a72256fb72545df964913b0b20cba
Chad Whitacre <chad@zetaweb.com>
2014-11-11 14:30:19 -0500

    Here's the error log from the update generation
    This should be pruned during landing.

--- branch.sql
+++ branch.sql
@@ -1,4 +1,19 @@
 BEGIN;
+
     -- Update user_id and is_team. See branch.py for doco.
-    \i update.sql
+    --\i update.sql
+
+
+    -- Drop all Bitbucket accounts that aren't required for account access.
+
+    DELETE FROM elsewhere WHERE id IN (
+        SELECT e1.id
+          FROM elsewhere e1
+          JOIN elsewhere e2
+            ON e1.participant = e2.participant
+         WHERE e1.platform='bitbucket'
+               AND e2.platform IN ('twitter', 'github', 'facebook', 'google', 'openstreetmap')
+               AND NOT e2.is_team
+    );
+
 END;
