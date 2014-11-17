c963bd530eed8c40e007a69894e5b167abfef297
anatoly techtonik <techtonik@gmail.com>
2014-11-17 20:55:56 +0300

    vagrant: Simplify scripts/vagrant-setup.sh
    Windows Git doesn't mess with newlines by default anymore, so remove
    that warning. Modify `/etc/profile.d` with sudo, demystify PostreSQL
    command line.

--- scripts/vagrant-setup.sh
+++ scripts/vagrant-setup.sh
@@ -3,25 +3,12 @@
 set -e
 
 # Set login directory to root vagrant share
-echo "cd /vagrant" > /etc/profile.d/login-directory.sh
+sudo sh -c "echo 'cd /vagrant' > /etc/profile.d/login-directory.sh"
 
-# Configure Postgres
+# Configure Postgres (using system user 'postgres' run command
+# 'psql' with PostreSQL user 'postgres` to quietly execute scripts)
 sudo -u postgres psql -U postgres -qf /vagrant/scripts/create_db.sql
 sudo -u postgres psql -U postgres -qf /vagrant/scripts/create_test_db.sql
-
-cd /vagrant
-
-# Warn if Windows newlines are detected and try to fix the problem
-if grep --quiet --binary --binary-files=without-match $(printf '\r') README.md; then
-    echo
-    cat scripts/crlf-warning.txt
-    echo
-
-    echo 'Running "git config core.autocrlf false"'
-    git config core.autocrlf false
-
-    exit 1
-fi
 
 # Set up the environment, the database, and run Gratipay
 cd /vagrant && make clean env schema data
