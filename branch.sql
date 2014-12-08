688b5abf6c0f4fd18197da3c804c6137744feaf8
Chad Whitacre <chad@zetaweb.com>
2014-12-08 15:43:09 -0500

    Reformat branch.sql for 100 chars

--- branch.sql
+++ branch.sql
@@ -4,23 +4,33 @@ BEGIN;
     , address                           text                        NOT NULL
     , verified                          boolean                     DEFAULT NULL
     , nonce                             text
-    , ctime                             timestamp with time zone    NOT NULL DEFAULT CURRENT_TIMESTAMP
+    , ctime                             timestamp with time zone    NOT NULL
+                                                                      DEFAULT CURRENT_TIMESTAMP
     , mtime                             timestamp with time zone
-    , participant                       text                        NOT NULL REFERENCES participants ON UPDATE CASCADE ON DELETE RESTRICT
-    , UNIQUE (address, verified) -- A verified email address can't be linked to multiple participants.
+    , participant                       text                        NOT NULL
+                                                                      REFERENCES participants
+                                                                      ON UPDATE CASCADE
+                                                                      ON DELETE RESTRICT
+    , UNIQUE (address, verified) -- A verified email address can't be linked to multiple
+                                 -- participants.
     , UNIQUE (participant, address)
      );
 
-    -- The participants table currently has an `email` attribute of type email_address_with confirmation
-    -- This should be deleted in the future, once the emails are migrated.
-    -- The column we're going to replace it with is named `email_address`. This is only for **verified** emails.
-    -- All unverified email stuff happens in the emails table and won't touch this attribute.
+    -- The participants table currently has an `email` attribute of type
+    -- email_address_with confirmation. This should be deleted in the future,
+    -- once the emails are migrated. The column we're going to replace it with
+    -- is named `email_address`. This is only for **verified** emails. All
+    -- unverified email stuff happens in the emails table and won't touch this
+    -- attribute.
 
     ALTER TABLE participants ADD COLUMN email_address text UNIQUE,
                              ADD COLUMN email_lang text;
 
     UPDATE events
-       SET payload = replace(replace(payload::text, '"set"', '"add"'), '"current_email"', '"email"')::json
+       SET payload = replace(replace( payload::text, '"set"', '"add"')
+                                    , '"current_email"'
+                                    , '"email"'
+                                     )::json
      WHERE payload->>'action' = 'set'
        AND (payload->'values'->'current_email') IS NOT NULL;
 
