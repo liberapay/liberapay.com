de46d3b1ee10dc9aaabdd0696c6d78b46e5b621d
Changaco <changaco@changaco.oy.lc>
2014-12-12 15:00:59 +0100

    fix traceback hack in `threaded_map()`

--- gratipay/billing/payday.py
+++ gratipay/billing/payday.py
@@ -30,16 +30,23 @@ with open('fake_payday.sql') as f:
     FAKE_PAYDAY = f.read()
 
 
+class ExceptionWrapped(Exception): pass
+
+
 def threaded_map(func, iterable, threads=5):
     pool = ThreadPool(threads)
     def g(*a, **kw):
         # Without this wrapper we get a traceback from inside multiprocessing.
         try:
             return func(*a, **kw)
-        except Exception:
-            import sys, traceback
-            raise sys.exc_info()[0](traceback.format_exc())
-    r = pool.map(g, iterable)
+        except Exception as e:
+            import traceback
+            raise ExceptionWrapped(e, traceback.format_exc())
+    try:
+        r = pool.map(g, iterable)
+    except ExceptionWrapped as e:
+        print(e.args[1])
+        raise e.args[0]
     pool.close()
     pool.join()
     return r
