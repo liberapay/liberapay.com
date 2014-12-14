6005f2159266423bad21090497019276af4b2033
Changaco <changaco@changaco.oy.lc>
2014-12-14 15:27:27 +0100

    reduce schema.sql

--- schema.sql
+++ schema.sql
@@ -53,6 +53,8 @@ CREATE TABLE participants
 , taking                numeric(35,2)               NOT NULL DEFAULT 0
 , npatrons              integer                     NOT NULL DEFAULT 0
 , is_free_rider         boolean                     DEFAULT NULL
+, email_address         text                        UNIQUE
+, email_lang            text
 , is_locked             boolean                     NOT NULL DEFAULT FALSE
 , CONSTRAINT claimed_not_locked CHECK (NOT (claimed_time IS NOT NULL AND is_locked))
  );
@@ -363,41 +365,28 @@ CREATE INDEX transfers_tipper_tippee_timestamp_idx
 
 
 -- https://github.com/gratipay/gratipay.com/pulls/2752
-BEGIN;
-    CREATE TABLE emails
-    ( id                    serial                      PRIMARY KEY
-    , address               text                        NOT NULL
-    , verified              boolean                     DEFAULT NULL
-                                                          CONSTRAINT verified_cant_be_false
-                                                            -- Only use TRUE and NULL, so that the
-                                                            -- unique constraint below functions
-                                                            -- properly.
-                                                            CHECK (verified IS NOT FALSE)
-    , nonce                 text
-    , verification_start    timestamp with time zone    NOT NULL
-                                                          DEFAULT CURRENT_TIMESTAMP
-    , verification_end      timestamp with time zone
-    , participant           text                        NOT NULL
-                                                          REFERENCES participants
-                                                          ON UPDATE CASCADE
-                                                          ON DELETE RESTRICT
+CREATE TABLE emails
+( id                    serial                      PRIMARY KEY
+, address               text                        NOT NULL
+, verified              boolean                     DEFAULT NULL
+                                                      CONSTRAINT verified_cant_be_false
+                                                        -- Only use TRUE and NULL, so that the
+                                                        -- unique constraint below functions
+                                                        -- properly.
+                                                        CHECK (verified IS NOT FALSE)
+, nonce                 text
+, verification_start    timestamp with time zone    NOT NULL
+                                                      DEFAULT CURRENT_TIMESTAMP
+, verification_end      timestamp with time zone
+, participant           text                        NOT NULL
+                                                      REFERENCES participants
+                                                      ON UPDATE CASCADE
+                                                      ON DELETE RESTRICT
 
-    , UNIQUE (address, verified) -- A verified email address can't be linked to multiple
-                                 -- participants. However, an *un*verified address *can*
-                                 -- be linked to multiple participants. We implement this
-                                 -- by using NULL instead of FALSE for the unverified
-                                 -- state, hence the check constraint on verified.
-    , UNIQUE (participant, address)
-     );
-
-    -- The participants table currently has an `email` attribute of type
-    -- email_address_with confirmation. This should be deleted in the future,
-    -- once the emails are migrated. The column we're going to replace it with
-    -- is named `email_address`. This is only for **verified** emails. All
-    -- unverified email stuff happens in the emails table and won't touch this
-    -- attribute.
-
-    ALTER TABLE participants ADD COLUMN email_address text UNIQUE,
-                             ADD COLUMN email_lang text;
-
-END;
+, UNIQUE (address, verified) -- A verified email address can't be linked to multiple
+                             -- participants. However, an *un*verified address *can*
+                             -- be linked to multiple participants. We implement this
+                             -- by using NULL instead of FALSE for the unverified
+                             -- state, hence the check constraint on verified.
+, UNIQUE (participant, address)
+ );
