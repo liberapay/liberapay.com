ed66b54a92f355d22bd57bd9c589bb03b2e35183
Changaco <changaco@changaco.oy.lc>
2014-12-28 18:01:06 +0100

    fix is-suspicious on profile pages

--- www/assets/admin.js
+++ www/assets/admin.js
@@ -1,31 +1,20 @@
-$(document).ready(function()
-{
+$(document).ready(function() {
     // Wire up is_suspicious toggle.
     // =============================
 
-    $('label.is-suspicious-knob').click(function()
-    {
+    $('.is-suspicious-label input').change(function() {
         var username = $(this).attr('data-username');
-        jQuery.ajax(
-            { url: '/' + username + '/toggle-is-suspicious.json'
-            , type: 'POST'
-            , dataType: 'json'
-            , success: function(data)
-            {
-                if (data.is_suspicious)
-                    $(".on-profile").addClass('is-suspicious');
-                else
-                    $(".on-profile").removeClass('is-suspicious');
-                $('input.is-suspicious-knob').attr( 'checked'
-                                                  , data.is_suspicious
-                                                   );
-            }
-            , error: function() {
-                    alert( "Failed to change is_suspicious. Please "
-                         + "try again."
-                          );
-                }
-             }
-        );
+        jQuery.ajax({
+            url: '/' + username + '/toggle-is-suspicious.json',
+            type: 'POST',
+            dataType: 'json',
+            success: function (data) {
+                $('.avatar').toggleClass('is-suspicious', data.is_suspicious);
+                $('.is-suspicious-label input').prop('checked', data.is_suspicious);
+            },
+            error: function(r) {
+                Gratipay.notification(r.responseJSON.error_message_long || 'Failure', 'error');
+            },
+        });
     });
 });
