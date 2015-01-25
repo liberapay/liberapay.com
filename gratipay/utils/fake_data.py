e1aff60c5df1824ee690f15cd2756f420f1fa28d
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-01-25 20:16:59 +0530

    Performance improvements
    reduce running time back to 1 minute + formatting fixes

--- gratipay/utils/fake_data.py
+++ gratipay/utils/fake_data.py
@@ -52,22 +52,22 @@ def fake_participant(db, number="singular", is_admin=False):
     """
     username = faker.first_name() + fake_text_id(3)
     try:
-      _fake_thing( db
-                 , "participants"
-                 , username=username
-                 , username_lower=username.lower()
-                 , ctime=faker.date_time_this_year()
-                 , is_admin=is_admin
-                 , balance=0
-                 , anonymous_giving=(random.randrange(5) == 0)
-                 , anonymous_receiving=(number != 'plural' and random.randrange(5) == 0)
-                 , balanced_customer_href=faker.uri()
-                 , last_ach_result=''
-                 , is_suspicious=False
-                 , last_bill_result=''  # Needed to not be suspicious
-                 , claimed_time=faker.date_time_this_year()
-                 , number=number
-                  )
+        _fake_thing( db
+                   , "participants"
+                   , username=username
+                   , username_lower=username.lower()
+                   , ctime=faker.date_time_this_year()
+                   , is_admin=is_admin
+                   , balance=0
+                   , anonymous_giving=(random.randrange(5) == 0)
+                   , anonymous_receiving=(number != 'plural' and random.randrange(5) == 0)
+                   , balanced_customer_href=faker.uri()
+                   , last_ach_result=''
+                   , is_suspicious=False
+                   , last_bill_result=''  # Needed to not be suspicious
+                   , claimed_time=faker.date_time_this_year()
+                   , number=number
+                    )
     except IntegrityError:
       return fake_participant(db, number, is_admin)
 
@@ -104,13 +104,13 @@ def fake_tip(db, tipper, tippee):
     """Create a fake tip.
     """
     return _fake_thing( db
-               , "tips"
-               , ctime=faker.date_time_this_year()
-               , mtime=faker.date_time_this_month()
-               , tipper=tipper.username
-               , tippee=tippee.username
-               , amount=fake_tip_amount()
-                )
+                      , "tips"
+                      , ctime=faker.date_time_this_year()
+                      , mtime=faker.date_time_this_month()
+                      , tipper=tipper.username
+                      , tippee=tippee.username
+                      , amount=fake_tip_amount()
+                       )
 
 
 def fake_elsewhere(db, participant, platform):
@@ -127,24 +127,107 @@ def fake_elsewhere(db, participant, platform):
 
 
 def fake_transfer(db, tipper, tippee):
-        return _fake_thing( db
-               , "transfers"
-               , timestamp=faker.date_time_this_year()
-               , tipper=tipper.username
-               , tippee=tippee.username
-               , amount=fake_tip_amount()
-               , context='tip'
-                )
+    return _fake_thing( db
+           , "transfers"
+           , timestamp=faker.date_time_this_year()
+           , tipper=tipper.username
+           , tippee=tippee.username
+           , amount=fake_tip_amount()
+           , context='tip'
+            )
 
 def fake_exchange(db, participant, amount, fee, timestamp):
-        return _fake_thing( db
-               , "exchanges"
-               , timestamp=timestamp
-               , participant=participant.username
-               , amount=amount
-               , fee=fee
-               , status='succeeded'
-                )
+    return _fake_thing( db
+                      , "exchanges"
+                      , timestamp=timestamp
+                      , participant=participant.username
+                      , amount=amount
+                      , fee=fee
+                      , status='succeeded'
+                       )
+
+def prep_db(db):
+    db.run("""
+        CREATE OR REPLACE FUNCTION process_transfer() RETURNS trigger AS $$
+            BEGIN
+                UPDATE participants
+                   SET balance = balance + NEW.amount
+                 WHERE username = NEW.tippee;
+
+                UPDATE participants
+                   SET balance = balance - NEW.amount
+                 WHERE username = NEW.tipper;
+
+                RETURN NULL;
+            END;
+        $$ language plpgsql;
+
+        CREATE TRIGGER process_transfer AFTER INSERT ON transfers
+            FOR EACH ROW EXECUTE PROCEDURE process_transfer();
+
+        CREATE OR REPLACE FUNCTION process_exchange() RETURNS trigger AS $$
+            BEGIN
+                IF NEW.amount > 0 THEN
+                    UPDATE participants
+                       SET balance = balance + NEW.amount
+                     WHERE username = NEW.participant;
+                ELSE
+                    UPDATE participants
+                       SET balance = balance + NEW.amount - NEW.fee
+                     WHERE username = NEW.participant;
+                END IF;
+
+                RETURN NULL;
+            END;
+        $$ language plpgsql;
+
+        CREATE TRIGGER process_exchange AFTER INSERT ON exchanges
+            FOR EACH ROW EXECUTE PROCEDURE process_exchange();
+
+        CREATE OR REPLACE FUNCTION process_payday() RETURNS trigger AS $$
+            BEGIN
+                SELECT COALESCE(SUM(amount+fee), 0)
+                  FROM exchanges
+                 WHERE timestamp > NEW.ts_start
+                   AND timestamp < NEW.ts_end
+                   AND amount > 0
+                  INTO NEW.charge_volume;
+
+                SELECT COALESCE(SUM(fee), 0)
+                  FROM exchanges
+                 WHERE timestamp > NEW.ts_start
+                   AND timestamp < NEW.ts_end
+                   AND amount > 0
+                  INTO NEW.charge_fees_volume;
+
+                SELECT COALESCE(SUM(amount), 0)
+                  FROM exchanges
+                 WHERE timestamp > NEW.ts_start
+                   AND timestamp < NEW.ts_end
+                   AND amount < 0
+                  INTO NEW.ach_volume;
+
+                SELECT COALESCE(SUM(fee), 0)
+                  FROM exchanges
+                 WHERE timestamp > NEW.ts_start
+                   AND timestamp < NEW.ts_end
+                   AND amount < 0
+                  INTO NEW.ach_fees_volume;
+
+                RETURN NEW;
+            END;
+        $$ language plpgsql;
+
+        CREATE TRIGGER process_payday BEFORE INSERT ON paydays
+            FOR EACH ROW EXECUTE PROCEDURE process_payday();
+    """)
+
+def clean_db(db):
+    db.run("""
+        DROP FUNCTION process_transfer() CASCADE;
+        DROP FUNCTION process_exchange() CASCADE;
+        DROP FUNCTION process_payday() CASCADE;
+    """)
 
 
 def populate_db(db, num_participants=100, num_tips=200, num_teams=5, num_transfers=5000, num_communities=20):
@@ -194,18 +277,6 @@ def populate_db(db, num_participants=100, num_tips=200, num_teams=5, num_transfe
         transfer = fake_transfer(db, tipper, tippee)
         transfers.append(transfer)
 
-        db.run("""
-          UPDATE participants
-             SET balance = balance + %s
-           WHERE username = %s
-        """, (transfer['amount'], tippee.username))
-
-        db.run("""
-          UPDATE participants
-             SET balance = balance - %s
-           WHERE username = %s
-        """, (transfer['amount'], tipper.username))
-
     print("Making Paydays")
     #First determine the boundaries - min and max date
     min_date = min(min(x['ctime'] for x in tips), \
@@ -219,38 +290,22 @@ def populate_db(db, num_participants=100, num_tips=200, num_teams=5, num_transfe
         week_tips = filter(lambda x: date < x['ctime'] < end_date, tips)
         week_transfers = filter(lambda x: date < x['timestamp'] < end_date, transfers)
         week_participants = filter(lambda x: x.ctime.replace(tzinfo=None) < end_date, participants)
-        credits = [] # Bank withdrawals
-        debits = [] # Credit Card charges
         for p in week_participants:
-          transfers_in = filter(lambda x: x['tippee'] == p.username, week_transfers)
-          transfers_out = filter(lambda x: x['tipper'] == p.username, week_transfers)
-          amount_in = sum([t['amount'] for t in transfers_in])
-          amount_out = sum([t['amount'] for t in transfers_out])
-          amount = amount_out - amount_in
-          fee = amount * D('0.02')
-          fee = abs(fee.quantize(D('.01')))
-          if amount != 0:
-            exchange = fake_exchange(
-              db=db,
-              participant=p,
-              amount=amount,
-              fee=fee,
-              timestamp=(end_date - datetime.timedelta(seconds=1))
-            )
-            if amount > 0:
-              debits.append(exchange)
-              db.run("""
-                UPDATE participants
-                   SET balance = balance + %s
-                 WHERE username = %s
-              """, (amount, p.username))
-            else:
-              credits.append(exchange)
-              db.run("""
-                UPDATE participants
-                   SET balance = balance + %s
-                 WHERE username = %s
-              """, (amount-fee, p.username))
+            transfers_in = filter(lambda x: x['tippee'] == p.username, week_transfers)
+            transfers_out = filter(lambda x: x['tipper'] == p.username, week_transfers)
+            amount_in = sum([t['amount'] for t in transfers_in])
+            amount_out = sum([t['amount'] for t in transfers_out])
+            amount = amount_out - amount_in
+            if amount != 0:
+                fee = amount * D('0.02')
+                fee = abs(fee.quantize(D('.01')))
+                exchange = fake_exchange(
+                    db=db,
+                    participant=p,
+                    amount=amount,
+                    fee=fee,
+                    timestamp=(end_date - datetime.timedelta(seconds=1))
+                )
         actives=set()
         tippers=set()
         for xfers in week_tips, week_transfers:
@@ -267,16 +322,14 @@ def populate_db(db, num_participants=100, num_tips=200, num_teams=5, num_transfe
             'nactive': len(actives),
             'transfer_volume': sum(x['amount'] for x in week_transfers)
         }
-        payday['ach_volume'] = sum([e['amount'] for e in credits])
-        payday['ach_fees_volume'] = sum([e['fee'] for e in credits])
-        payday['charge_volume'] = sum([(e['amount'] + e['fee']) for e in debits])
-        payday['charge_fees_volume'] = sum([e['fee'] for e in debits])
         _fake_thing(db, "paydays", **payday)
         date = end_date
 
 def main():
     db = wireup.db(wireup.env())
+    prep_db(db)
     populate_db(db)
+    clean_db(db)
     check_db(db)
 
 
