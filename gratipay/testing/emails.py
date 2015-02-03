dc2302b7d8a4acaa86e3faf0833b98fc0da27d0d
Rohit Paul Kuruvilla <rohitpaulk@live.com>
2015-02-03 13:27:03 +0530

    Cleanup tests

--- gratipay/testing/emails.py
+++ gratipay/testing/emails.py
@@ -1,6 +1,14 @@
+import threading
+
 def get_last_email(mailer):
     return {
         'to': mailer.call_args[1]['message']['to'][0]['email'],
         'message_text': mailer.call_args[1]['message']['text'],
         'message_html': mailer.call_args[1]['message']['html']
     }
+
+def wait_for_email_thread():
+        # Emails are processed in a thread, wait for it to complete
+        email_thread = filter(lambda x: x.name == 'email', threading.enumerate())
+        if email_thread:
+            email_thread[0].join()
--- tests/py/test_transactional_emails.py
+++ tests/py/test_transactional_emails.py
@@ -1,52 +1,51 @@
 import mock
-import threading
 
 from gratipay.models.participant import Participant
 from gratipay.models.account_elsewhere import AccountElsewhere
 from gratipay.testing import Harness
-from gratipay.testing.emails import get_last_email
+from gratipay.testing.emails import get_last_email, wait_for_email_thread
 
 class TestTransactionalEmails(Harness):
 
-    @mock.patch.object(Participant._mailer.messages, 'send')
-    def test_opt_in_sends_notifications_to_patrons(self, mailer):
-        alice = self.make_elsewhere('twitter', 1, 'alice')
-        bob = self.make_participant('bob', claimed_time='now', email_address='bob@gmail.com')
-        dan = self.make_participant('dan', claimed_time='now', email_address='dan@gmail.com')
-        self.make_participant('roy', claimed_time='now', email_address='roy@gmail.com', notify_on_opt_in=False)
-        bob.set_tip_to(alice.participant.username, '100')
-        dan.set_tip_to(alice.participant.username, '100')
-        alice = AccountElsewhere.from_user_name('twitter', 'alice')
-        alice.opt_in('alice')
+    def setUp(self):
+        self.bob = self.make_participant('bob', claimed_time='now', email_address='bob@gmail.com')
+        self.dan = self.make_participant('dan', claimed_time='now', email_address='dan@gmail.com')
+        self.alice = self.make_participant('alice', claimed_time='now', email_address='alice@gmail.com')
 
-        # Emails are processed in a thread, wait for it to complete
-        email_thread = filter(lambda x: x.name == 'email', threading.enumerate())
-        if email_thread:
-            email_thread[0].join()
+        self.mailer_patcher = mock.patch.object(Participant._mailer.messages, 'send')
+        self.test_mailer = self.mailer_patcher.start()
+        self.addCleanup(self.mailer_patcher.stop)
 
-        assert mailer.call_count == 2 # Emails should only be sent to bob and dan
-        last_email = get_last_email(mailer)
+    def test_opt_in_sends_notifications_to_patrons(self):
+        carl_twitter = self.make_elsewhere('twitter', 1, 'carl')
+        roy = self.make_participant('roy', claimed_time='now', email_address='roy@gmail.com', notify_on_opt_in=False)
+
+        self.bob.set_tip_to(carl_twitter.participant.username, '100')
+        self.dan.set_tip_to(carl_twitter.participant.username, '100')
+        roy.set_tip_to(carl_twitter.participant.username, '100') # Roy will NOT receive an email.
+
+        AccountElsewhere.from_user_name('twitter', 'carl').opt_in('carl')
+
+        wait_for_email_thread()
+
+        assert self.test_mailer.call_count == 2 # Emails should only be sent to bob and dan
+        last_email = get_last_email(self.test_mailer)
         assert last_email['to'] == 'dan@gmail.com'
-        expected = "You had pledged to alice. They've just joined Gratipay!"
+        expected = "You had pledged to carl. They've just joined Gratipay!"
         assert expected in last_email['message_text']
 
-    @mock.patch.object(Participant._mailer.messages, 'send')
-    def test_take_over_sends_notifications_to_patrons(self, mailer):
-        bob = self.make_participant('bob', claimed_time='now', email_address='bob@gmail.com')
-        dan = self.make_participant('dan', claimed_time='now', email_address='dan@gmail.com')
+    def test_take_over_sends_notifications_to_patrons(self):
         dan_twitter = self.make_elsewhere('twitter', 1, 'dan')
-        bob.set_tip_to(dan_twitter.participant.username, '100')
-        dan.take_over(dan_twitter, have_confirmation=True)
 
-        # Emails are processed in a thread, wait for it to complete
-        email_thread = filter(lambda x: x.name == 'email', threading.enumerate())
-        if email_thread:
-            email_thread[0].join()
+        self.alice.set_tip_to(self.dan, '100') # Alice shouldn't receive an email.
+        self.bob.set_tip_to(dan_twitter.participant.username, '100') # Bob should receive an email.
 
-        assert mailer.call_count == 1
-        last_email = get_last_email(mailer)
+        self.dan.take_over(dan_twitter, have_confirmation=True)
+
+        wait_for_email_thread()
+
+        assert self.test_mailer.call_count == 1
+        last_email = get_last_email(self.test_mailer)
         assert last_email['to'] == 'bob@gmail.com'
         expected = "You had pledged to dan. They've just joined Gratipay!"
         assert expected in last_email['message_text']
-
-
