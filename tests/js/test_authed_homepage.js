1ef2ba1405d8993e1a81e35321f6e1b3f0ea45e4
Chad Whitacre <chad@zetaweb.com>
2015-02-16 21:11:35 -0500

    Update jstest

--- tests/js/test_authed_homepage.js
+++ tests/js/test_authed_homepage.js
@@ -23,7 +23,7 @@ describe('authed homepage', function() {
         browser
             .url('http://localhost:8537')
             .getText('#content h1', function(err, text) {
-                assert.equal(text, 'Welcome, alice!');
+                assert.equal(text, 'alice');
             })
             .deleteCookie('session')
             .call(done);
