2a22c37fe8b15d86e8e5c239f648bb6cf6367bb3
Changaco <changaco@changaco.oy.lc>
2015-02-21 12:19:59 +0100

    prune unused method

--- gratipay/security/user.py
+++ gratipay/security/user.py
@@ -87,26 +87,3 @@ class User(object):
     @property
     def ANON(self):
         return self.participant is None
-
-    def get_highest_role(self, owner):
-        """Return a string representing the highest role this user has.
-
-        :param string owner: the username of the owner of the resource we're
-            concerned with, or None
-
-        """
-        def is_owner():
-            if self.participant is not None:
-                if owner is not None:
-                    if self.participant.username == owner:
-                        return True
-            return False
-
-        if self.ADMIN:
-            return 'admin'
-        elif is_owner():
-            return 'owner'
-        elif not self.ANON:
-            return 'authenticated'
-        else:
-            return 'anonymous'
