c012620f95df59d9e9f8f951be32902fb8df80da
Changaco <changaco@changaco.oy.lc>
2015-02-26 11:26:49 +0100

    Revert "Use `return {}` idiom for csrf"
    This reverts commit 124e7221f226b83462ce4a80c4c75d4dd753e0f6.

--- gratipay/security/csrf.py
+++ gratipay/security/csrf.py
@@ -61,7 +61,7 @@ def _sanitize_token(token):
 
 
 
-def get_csrf_token_from_request(request):
+def get_csrf_token_from_request(request, state):
     """Given a Request object, reject it if it's a forgery.
     """
     if request.line.uri.startswith('/assets/'): return
@@ -72,7 +72,7 @@ def get_csrf_token_from_request(request):
     except KeyError:
         cookie_token = None
 
-    cookie_token or _get_new_csrf_key()
+    state['csrf_token'] = cookie_token or _get_new_csrf_key()
 
     # Assume that anything not defined as 'safe' by RC2616 needs protection
     if request.line.method not in ('GET', 'HEAD', 'OPTIONS', 'TRACE'):
@@ -93,8 +93,6 @@ def get_csrf_token_from_request(request):
 
         if not constant_time_compare(second_token, cookie_token):
             raise Response(403, REASON_BAD_TOKEN)
-
-        return {'csrf_token': second_token}
 
 
 def add_csrf_token_to_response(response, csrf_token=None):
