e5d92fd500f717cfd583178bec189dee06389782
Changaco <changaco@changaco.oy.lc>
2015-02-27 17:14:40 +0100

    add `OR REPLACE` to `enumerate.sql`

--- sql/enumerate.sql
+++ sql/enumerate.sql
@@ -1,3 +1,5 @@
-CREATE FUNCTION enumerate(anyarray) RETURNS TABLE (rank bigint, value anyelement) AS $$
+CREATE OR REPLACE FUNCTION enumerate(anyarray)
+RETURNS TABLE (rank bigint, value anyelement)
+AS $$
     SELECT row_number() over() as rank, value FROM unnest($1) value;
 $$ LANGUAGE sql STABLE;
