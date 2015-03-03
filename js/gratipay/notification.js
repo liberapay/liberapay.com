fd37da43e288a424307236958e15fae4fa2b3b99
Changaco <changaco@changaco.oy.lc>
2015-03-03 10:28:53 +0100

    add a `closeCallback` argument to `Gratipay.notification()`

--- js/gratipay/notification.js
+++ js/gratipay/notification.js
@@ -3,7 +3,7 @@
  * @param {string} text  Notification text
  * @param {string} [type=notice]  Notification type (one of: notice, error, success)
  */
-Gratipay.notification = function(text, type, timeout) {
+Gratipay.notification = function(text, type, timeout, closeCallback) {
     var type = type || 'notice';
     var timeout = timeout || (type == 'error' ? 10000 : 5000);
 
@@ -13,19 +13,23 @@ Gratipay.notification = function(text, type, timeout) {
         Gratipay.jsonml(dialog)
     ]);
 
+    // Close if we're on the page the notification links to.
+    var links = $dialog.eq(1).find('a');
+    if (links.length == 1 && links[0].pathname == location.pathname) {
+        return closeCallback()
+    }
+
     if (!$('#notification-area').length)
         $('body').prepend('<div id="notification-area"><div class="notifications-fixed"></div></div>');
 
     $('#notification-area').prepend($dialog.get(0));
     $('#notification-area .notifications-fixed').prepend($dialog.get(1));
 
-    function fadeOut() {
+    function close() {
         $dialog.addClass('fade-out');
+        if (closeCallback) closeCallback();
     }
 
-    var $btn_close = $('<span class="btn-close">&times;</span>');
-    $btn_close.click(fadeOut);
-    $btn_close.appendTo($dialog.get(1));
-
-    if (timeout > 0) setTimeout(fadeOut, timeout);
+    $dialog.append($('<span class="btn-close">&times;</span>').click(close));
+    if (timeout > 0) setTimeout(close, timeout);
 };
