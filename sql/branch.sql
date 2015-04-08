4f6bf7f8a3e9fa107616d2af826712a6dc30f910
Changaco <changaco@changaco.oy.lc>
2015-04-08 21:01:09 +0200

    add UI for the `notify_charge` setting

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,3 +1,7 @@
 BEGIN;
     ALTER TABLE participants ADD COLUMN notify_charge int DEFAULT 1;
+    ALTER TABLE participants
+        ALTER COLUMN notify_on_opt_in DROP DEFAULT,
+        ALTER COLUMN notify_on_opt_in TYPE int USING notify_on_opt_in::int,
+        ALTER COLUMN notify_on_opt_in SET DEFAULT 1;
 END;
