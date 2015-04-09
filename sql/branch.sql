14c12e02ab415605da2ef457c96c7beab77e8a36
Changaco <changaco@changaco.oy.lc>
2015-04-09 23:41:28 +0200

    send notifications for successful charges by default

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,5 +1,5 @@
 BEGIN;
-    ALTER TABLE participants ADD COLUMN notify_charge int DEFAULT 1;
+    ALTER TABLE participants ADD COLUMN notify_charge int DEFAULT 3;
     ALTER TABLE participants
         ALTER COLUMN notify_on_opt_in DROP DEFAULT,
         ALTER COLUMN notify_on_opt_in TYPE int USING notify_on_opt_in::int,
