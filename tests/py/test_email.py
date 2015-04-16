0688646a60f32c93a7ca0a121f6ae027d08e97e9
Chad Whitacre <chad@zetaweb.com>
2015-04-16 19:55:30 -0400

    Failing test for skipping emails w/o address

--- tests/py/test_email.py
+++ tests/py/test_email.py
@@ -206,9 +206,20 @@ class TestEmail(EmailHarness):
         larry = self.make_participant('larry', email_address='larry@example.com')
         larry.queue_email("verification")
 
+        assert self.db.one("SELECT spt_name FROM email_queue") == "verification"
         Participant.dequeue_emails()
         assert self.mailer.call_count == 1
         last_email = self.get_last_email()
         assert last_email['to'][0]['email'] == 'larry@example.com'
         expected = "connect larry"
         assert expected in last_email['text']
+        assert self.db.one("SELECT spt_name FROM email_queue") is None
+
+    def test_dequeueing_an_email_without_address_just_skips_it(self):
+        larry = self.make_participant('larry')
+        larry.queue_email("verification")
+
+        assert self.db.one("SELECT spt_name FROM email_queue") == "verification"
+        Participant.dequeue_emails()
+        assert self.mailer.call_count == 0
+        assert self.db.one("SELECT spt_name FROM email_queue") is None
