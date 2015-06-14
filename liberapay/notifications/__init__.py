764d7a18750dc1807e8de5b4d3ac954a9ca800b7
Changaco <changaco@changaco.oy.lc>
2015-06-14 14:08:39 +0200

    bring web notifications up to par with emails

--- liberapay/notifications/__init__.py
+++ liberapay/notifications/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import division, print_function, unicode_literals
+
 from collections import namedtuple, OrderedDict
 
 
--- liberapay/notifications/web.py
+++ liberapay/notifications/web.py
@@ -1,27 +1,45 @@
+from __future__ import division, print_function, unicode_literals
 
 
-def ba_withdrawal_failed(_, user):
+def withdrawal_failed(_, user):
     href = '/%s/routes/bank-account.html' % user.username
-    return ('error',
+    return ('danger',
         ['a',
             {'href': href}, _("The transfer to your bank account has failed!"),
-        ],
+        ]
     )
 
 
-def credit_card_failed(_, user):
+def withdrawal_pending(_, user, exchange, format_currency):
+    return ('success',
+        ['span', _("We have initiated a transfer of {0} from your Liberapay wallet to your bank account.",
+                   format_currency(exchange.amount - exchange.fee, 'USD'))
+        ]
+    )
+
+
+def charge_failed(_, user, exchange, format_currency):
     href = '/%s/routes/credit-card.html' % user.username
-    return ('error',
-        ['span', _("Your credit card has failed!") + " ",
-            ['a', {'href': href}, _("Fix your card")]
-        ],
+    return ('danger',
+        ['a', {'href': href},
+              _("We tried to charge your credit card {0}, but it failed!",
+                format_currency(exchange.amount + exchange.fee, 'USD'))
+        ]
+    )
+
+
+def charge_succeeded(_, user, exchange, format_currency):
+    return ('success',
+        ['span', _("We charged your credit card {0} to fund your ongoing donations.",
+                   format_currency(exchange.amount + exchange.fee, 'USD'))
+        ]
     )
 
 
 def pledgee_joined(_, user_name, platform, profile_url):
-    return ('notice',
+    return ('info',
         ['a',
             {'href': profile_url},
             _("{0} from {1} has joined Liberapay!", user_name, platform),
-        ],
+        ]
     )
