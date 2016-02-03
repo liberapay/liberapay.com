f3efaf40702d379f440fc365132263cba01ed177
Changaco <changaco@changaco.oy.lc>
2016-02-03 15:06:09 +0100

    send SIGHUP to gunicorn after hot deployment

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ from os import chdir, environ, execlp
 
 chdir(environ['OPENSHIFT_REPO_DIR'])
 program = 'gunicorn'
+pid_file = environ['OPENSHIFT_DATA_DIR'] + '/gunicorn.pid'
 bind = environ['OPENSHIFT_PYTHON_IP'] + ':' + environ['OPENSHIFT_PYTHON_PORT']
-execlp(program, program, 'liberapay.main:website', '--bind', bind)
+execlp(
+    program, program,
+    'liberapay.main:website',
+    '--pid', pid_file,
+    '--bind', bind,
+)
