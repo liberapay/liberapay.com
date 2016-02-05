4549bdabcbec862ca81662bf0baaae6e79710eeb
Changaco <changaco@changaco.oy.lc>
2016-02-05 21:12:41 +0100

    fix auto-transcoding of the message inside the base64

--- liberapay/utils/__init__.py
+++ liberapay/utils/__init__.py
@@ -75,9 +75,9 @@ def get_participant(state, restrict=True, redirect_stub=True, allow_member=False
 
 
 def b64decode_s(s, **kw):
-    udecode = lambda a: a
+    udecode = lambda a: a.decode('utf8')
     if s[:1] == b'.':
-        udecode = lambda a: a.decode('utf8')
+        udecode = lambda a: a
         s = s[1:]
     s = s.replace(b'~', b'=')
     try:
@@ -94,11 +94,15 @@ def b64decode_s(s, **kw):
 
 
 def b64encode_s(s):
+    prefix = b''
     if not isinstance(s, bytes):
         s = s.encode('utf8')
-        prefix = b'.'
     else:
-        prefix = b''
+        # Check whether the string is binary or already utf8
+        try:
+            s.decode('utf8')
+        except UnicodeError:
+            prefix = b'.'
     return prefix + b64encode(s, b'-_').replace(b'=', b'~')
 
 
