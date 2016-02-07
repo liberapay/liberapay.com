e4ff3739f67b7f1435c1d51f26c8db2b4908e261
Changaco <changaco@changaco.oy.lc>
2016-02-07 23:32:30 +0100

    prune now unnecessary joins in current_takes view

--- sql/branch.sql
+++ sql/branch.sql
@@ -6,11 +6,7 @@ BEGIN;
         SELECT * FROM (
              SELECT DISTINCT ON (member, team) t.*
                FROM takes t
-               JOIN participants p1 ON p1.id = member
-               JOIN participants p2 ON p2.id = team
-           ORDER BY member
-                  , team
-                  , mtime DESC
+           ORDER BY member, team, mtime DESC
         ) AS anon WHERE amount IS NOT NULL;
 
 END;
