d31cec32753e0f79a0b89aa888dee79275fc46a0
Changaco <changaco@changaco.oy.lc>
2016-02-09 20:20:01 +0100

    fix order of changes in migrations.sql
    ```
    BEGIN
    ERROR:  cannot drop table participants column is_suspicious because other objects depend on it
    DETAIL:  view current_takes depends on table participants column is_suspicious
    HINT:  Use DROP ... CASCADE to drop the dependent objects too.
    ```

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -5,10 +5,10 @@ CREATE TABLE db_meta (key text PRIMARY KEY, value jsonb);
 INSERT INTO db_meta (key, value) VALUES ('schema_version', '1'::jsonb);
 
 -- migration #2
-ALTER TABLE participants DROP COLUMN is_suspicious;
 CREATE OR REPLACE VIEW current_takes AS
     SELECT * FROM (
          SELECT DISTINCT ON (member, team) t.*
            FROM takes t
        ORDER BY member, team, mtime DESC
     ) AS anon WHERE amount IS NOT NULL;
+ALTER TABLE participants DROP COLUMN is_suspicious;
