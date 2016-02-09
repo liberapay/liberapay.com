468df04390db332184f3c7a04f83c6d4b9739ccf
Changaco <changaco@changaco.oy.lc>
2016-02-09 23:15:42 +0100

    [release.sh] fix order of commands, completes #141
    We have to compile branch.sql before we merge it into schema.sql, otherwise it's likely to fail.

--- release.sh
+++ release.sh
@@ -59,33 +59,34 @@ if [ -e sql/branch.sql ]; then
     schema_version=$(sed -n -r -e "s/.*$schema_version_re.*/\2/p" sql/schema.sql)
     new_version=$(($schema_version + 1))
 
+    # Run branch.sql on the test DB in echo mode to get back a "compiled"
+    # version on stdout without commands like \i
+    branch_c=sql/branch-compiled.sql
+    echo "Compiling branch.sql into $branch_c..."
+    echo >>$branch_c
+    echo "UPDATE db_meta SET value = '$new_version'::jsonb WHERE key = 'schema_version';" >>$branch_c
+    $(make echo var=with_tests_env) sh -eu -c "
+        psql \$DATABASE_URL <sql/recreate-schema.sql >/dev/null
+        psql -e \$DATABASE_URL -o /dev/null <$branch_c >$branch_c.
+    "
+    mv $branch_c{.,}
+    echo "Done."
+
     # Merge branch.sql into migrations.sql
     out=sql/migrations.sql
     echo -e '\n-- migration' "#$new_version" >>$out
     ./env/bin/python -c "print(open('sql/branch.sql').read().strip())" >>$out
 
     # Merge branch.sql into schema.sql
-    git rm --cached sql/branch.sql
     sed -i -r -e "s/$schema_version_re/\1 '$new_version'/" sql/schema.sql
     echo -e '\n' | cat - sql/branch.sql >>sql/schema.sql
 
     # Let the user do the rest, then commit
+    git rm sql/branch.sql
     echo "sql/branch.sql has been merged into sql/schema.sql and sql/migrations.sql"
     read -p "Please make the necessary manual modifications to those files now, then press Enter to continue... " enter
     git add sql/{schema,migrations}.sql
     git commit -m "merge branch.sql"
-
-    # Run branch.sql on the test DB in echo mode to get back a "compiled"
-    # version on stdout without commands like \i
-    echo "Compiling branch.sql..."
-    echo >>sql/branch.sql
-    echo "UPDATE db_meta SET value = '$new_version'::jsonb WHERE key = 'schema_version';" >>sql/branch.sql
-    $(make echo var=with_tests_env) sh -eu -c '
-        psql $DATABASE_URL <sql/recreate-schema.sql >/dev/null
-        psql -e $DATABASE_URL -o /dev/null <sql/branch.sql >sql/branch_.sql
-    '
-    mv sql/branch{_,}.sql
-    echo "Done."
 
     # Deployment options
     if yesno "Should branch.sql be applied before deploying instead of after?"; then
@@ -104,11 +105,11 @@ git tag $version
 
 # Deploy
 [ "${maintenance-}" = "yes" ] && rhc app stop $APPNAME
-[ "${run_sql-}" = "before" ] && rhc ssh $APPNAME 'psql -v ON_ERROR_STOP=on' <sql/branch.sql
+[ "${run_sql-}" = "before" ] && rhc ssh $APPNAME 'psql -v ON_ERROR_STOP=on' <$branch_c
 git push --force openshift master
 [ "${maintenance-}" = "yes" ] && rhc app start $APPNAME
-[ "${run_sql-}" = "after" ] && rhc ssh $APPNAME 'psql -v ON_ERROR_STOP=on' <sql/branch.sql
-rm -f sql/branch.sql
+[ "${run_sql-}" = "after" ] && rhc ssh $APPNAME 'psql -v ON_ERROR_STOP=on' <$branch_c
+rm -f $branch_c
 
 # Push to GitHub
 git push
