3622574004b9f2c53e6b5af5461a9ba408e2857e
Changaco <changaco@changaco.oy.lc>
2016-02-18 10:17:20 +0100

    refactor test_tip_json.py

--- tests/py/test_tip_json.py
+++ tests/py/test_tip_json.py
@@ -7,6 +7,15 @@ from liberapay.testing import Harness
 
 class TestTipJson(Harness):
 
+    def tip(self, tipper, tippee, amount, raise_immediately=True):
+        return self.client.POST(
+            "/%s/tip.json" % tippee,
+            {'amount': amount},
+            auth_as=tipper,
+            xhr=True,
+            raise_immediately=raise_immediately,
+        )
+
     def test_get_amount_and_total_back_from_api(self):
         "Test that we get correct amounts and totals back on POSTs to tip.json"
 
@@ -17,17 +26,8 @@ class TestTipJson(Harness):
         test_tipper = self.make_participant("test_tipper", balance=100)
 
         # Then, add a $1.50 and $3.00 tip
-        response1 = self.client.POST( "/test_tippee1/tip.json"
-                                    , {'amount': "1.00"}
-                                    , auth_as=test_tipper
-                                    , xhr=True
-                                     )
-
-        response2 = self.client.POST( "/test_tippee2/tip.json"
-                                    , {'amount': "3.00"}
-                                    , auth_as=test_tipper
-                                    , xhr=True
-                                     )
+        response1 = self.tip(test_tipper, "test_tippee1", "1.00")
+        response2 = self.tip(test_tipper, "test_tippee2", "3.00")
 
         # Confirm we get back the right amounts.
         first_data = json.loads(response1.body)
@@ -41,19 +41,11 @@ class TestTipJson(Harness):
         self.make_participant("alice")
         bob = self.make_participant("bob")
 
-        response = self.client.POST( "/alice/tip.json"
-                                   , {'amount': "110.00"}
-                                   , auth_as=bob
-                                   , raise_immediately=False
-                                    )
+        response = self.tip(bob, "alice", "110.00", raise_immediately=False)
         assert "not a valid donation amount" in response.text
         assert response.code == 400
 
-        response = self.client.POST( "/alice/tip.json"
-                                   , {'amount': "-1.00"}
-                                   , auth_as=bob
-                                   , raise_immediately=False
-                                    )
+        response = self.tip(bob, "alice", "-1.00", raise_immediately=False)
         assert "not a valid donation amount" in response.text
         assert response.code == 400
 
@@ -61,22 +53,14 @@ class TestTipJson(Harness):
         self.make_participant("alice", goal='-1')
         bob = self.make_participant("bob")
 
-        response = self.client.POST( "/alice/tip.json"
-                                   , {'amount': "10.00"}
-                                   , auth_as=bob
-                                   , raise_immediately=False
-                                    )
+        response = self.tip(bob, "alice", "10.00", raise_immediately=False)
         assert "doesn't accept donations" in response.text, response.text
         assert response.code == 400
 
     def test_tip_to_unclaimed(self):
         alice = self.make_elsewhere('twitter', 1, 'alice')
         bob = self.make_participant("bob")
-        response = self.client.POST( "/%s/tip.json" % alice.participant.username
-                                   , {'amount': "10.00"}
-                                   , auth_as=bob
-                                   , xhr=True
-                                    )
+        response = self.tip(bob, alice.participant.username, "10.00")
         data = json.loads(response.body)
         assert response.code == 200
         assert data['amount'] == "10.00"
