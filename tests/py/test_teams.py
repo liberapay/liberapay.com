5d2262694e796dac733ba812a865f66322ef4cc8
Changaco <changaco@changaco.oy.lc>
2016-04-10 10:40:20 +0200

    refactor team tests

--- tests/py/test_teams.py
+++ tests/py/test_teams.py
@@ -9,39 +9,34 @@ class Tests(Harness):
 
     def setUp(self):
         Harness.setUp(self)
-        self.team = self.make_participant('A-Team', kind='group')
+        self.a_team = self.make_participant('A-Team', kind='group')
+        self.alice = self.make_participant('alice')
+        self.a_team.add_member(self.alice)
+        self.bob = self.make_participant('bob', email='bob@example.net')
 
-    def test_can_add_members(self):
-        alice = self.make_participant('alice')
-        expected = True
-        self.team.add_member(alice)
-        actual = alice.member_of(self.team)
-        assert actual == expected
+    def test_member_of(self):
+        actual = self.alice.member_of(self.a_team)
+        assert actual is True
 
     def test_get_teams_for_member(self):
-        alice = self.make_participant('alice')
-        bob = self.make_participant('bob')
-        team = self.make_participant('B-Team', kind='group')
-        self.team.add_member(alice)
-        team.add_member(bob)
-        expected = 1
-        actual = alice.get_teams().pop().nmembers
-        assert actual == expected
+        b_team = self.make_participant('B-Team', kind='group')
+        b_team.add_member(self.bob)
+        actual = self.alice.get_teams().pop().nmembers
+        assert actual == 1
 
     def test_preclude_adding_stub_participant(self):
         stub_participant = self.make_stub()
         with self.assertRaises(InactiveParticipantAdded):
-            self.team.add_member(stub_participant)
+            self.a_team.add_member(stub_participant)
 
     def test_remove_all_members(self):
-        alice = self.make_participant('alice')
-        self.team.add_member(alice)
-        bob = self.make_participant('bob')
-        self.team.add_member(bob)
+        self.a_team.add_member(self.bob)
+        assert len(self.a_team.get_current_takes()) == 2  # sanity check
+        self.a_team.remove_all_members()
+        assert len(self.a_team.get_current_takes()) == 0
 
-        assert len(self.team.get_current_takes()) == 2  # sanity check
-        self.team.remove_all_members()
-        assert len(self.team.get_current_takes()) == 0
+
+class Tests2(Harness):
 
     def test_create_close_and_reopen_team(self):
         alice = self.make_participant('alice')
