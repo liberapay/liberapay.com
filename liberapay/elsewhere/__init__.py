fa07a341dd82177181d5f58449bb2245a5bf4da9
Changaco <changaco@changaco.oy.lc>
2016-06-14 23:40:40 +0200

    fix anomaly detection in two modules
    `# flake8: noqa` disables checks for the whole file!
    That answers the question in 72b5a5fa067ca9f0e09ce2cb4014357cc96961ab.

--- liberapay/elsewhere/__init__.py
+++ liberapay/elsewhere/__init__.py
@@ -1,16 +1,16 @@
 """This subpackage contains functionality for working with accounts elsewhere.
 """
 
-def _import():  # flake8: noqa
-    from .bitbucket import Bitbucket
-    from .bountysource import Bountysource
-    from .facebook import Facebook
-    from .github import GitHub
-    from .gitlab import GitLab
-    from .google import Google
-    from .linuxfr import LinuxFr
-    from .openstreetmap import OpenStreetMap
-    from .twitter import Twitter
+def _import():
+    from .bitbucket import Bitbucket  # noqa
+    from .bountysource import Bountysource  # noqa
+    from .facebook import Facebook  # noqa
+    from .github import GitHub  # noqa
+    from .gitlab import GitLab  # noqa
+    from .google import Google  # noqa
+    from .linuxfr import LinuxFr  # noqa
+    from .openstreetmap import OpenStreetMap  # noqa
+    from .twitter import Twitter  # noqa
     return list(locals().values())
 
 CLASSES = _import()
