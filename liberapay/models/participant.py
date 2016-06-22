29fcc36da62e249f1f69f88c1b15aa6cd9a6f7d7
Changaco <changaco@changaco.oy.lc>
2016-06-22 11:50:42 +0200

    disallow sending final gifts to suspended accounts

--- liberapay/models/participant.py
+++ liberapay/models/participant.py
@@ -404,7 +404,7 @@ class Participant(Model, MixinTeam):
             return
 
         tips, total, _, _ = self.get_giving_for_profile()
-        tips = [t for t in tips if t.is_identified]
+        tips = [t for t in tips if t.is_identified and not t.is_suspended]
         transfers = []
         distributed = D_ZERO
 
