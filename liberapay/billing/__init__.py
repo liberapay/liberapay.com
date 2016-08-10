99530369a6ef22ba8d08dd5d16cbf203a1a46f72
Changaco <changaco@changaco.oy.lc>
2016-08-10 11:00:17 +0200

    patch mangopay library to always reuse connections

--- liberapay/billing/__init__.py
+++ liberapay/billing/__init__.py
@@ -1,7 +1,18 @@
 from mangopaysdk.mangopayapi import MangoPayApi
+from mangopaysdk.tools.resttool import BaseRestTool
 from mangopaysdk.types.dto import Dto
 
+
+class CustomRestTool(BaseRestTool):
+
+    def _sendRequest(self, request):
+        request.headers.pop('Connection', None)
+        return super(CustomRestTool, self)._sendRequest(request)
+
+
 mangoapi = MangoPayApi()
+mangoapi.Config.RestToolClass = CustomRestTool
+
 
 class NS(Dto):
     def __init__(self, **kw):
