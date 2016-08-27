cb8999d44f6aa210c6e176664da9aaf4d0fb9e29
Changaco <changaco@changaco.oy.lc>
2016-08-27 18:22:20 +0200

    update code for parsing of email simplates

--- liberapay/utils/emails.py
+++ liberapay/utils/emails.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from aspen.simplates.pagination import parse_specline, split_and_escape
+from aspen.simplates.simplate import _decode
 from aspen_jinja2_renderer import SimplateLoader
 from jinja2 import Environment
 
@@ -25,10 +26,10 @@ jinja_env_html = Environment(
 
 def compile_email_spt(fpath):
     r = {}
-    with open(fpath) as f:
-        pages = list(split_and_escape(f.read()))
+    with open(fpath, 'rb') as f:
+        pages = list(split_and_escape(_decode(f.read())))
     for i, page in enumerate(pages, 1):
-        tmpl = b'\n' * page.offset + page.content
+        tmpl = '\n' * page.offset + page.content
         content_type, renderer = parse_specline(page.header)
         key = 'subject' if i == 1 else content_type
         env = jinja_env_html if content_type == 'text/html' else jinja_env
