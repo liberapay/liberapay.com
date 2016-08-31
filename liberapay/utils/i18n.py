97f3fac6669e9f6c1c69dbb1d369fa672c4b5443
Changaco <changaco@changaco.oy.lc>
2016-08-31 11:52:27 +0200

    modify i18n extraction to flag messages properly

--- liberapay/utils/i18n.py
+++ liberapay/utils/i18n.py
@@ -318,3 +318,34 @@ def extract_spt(fileobj, *args, **kw):
         if extractor:
             for match in extractor(f, *args, **kw):
                 yield match
+
+
+if __name__ == '__main__':
+    import sys
+
+    from babel.messages.pofile import read_po, write_po
+
+    if sys.argv[1] == 'po-reflag':
+        # This adds the `python-brace-format` flag to messages that contain braces
+        # https://github.com/python-babel/babel/issues/333
+        pot_path = sys.argv[2]
+        print('rewriting PO template file', pot_path)
+        # read PO file
+        with open(pot_path, 'rb') as pot:
+            catalog = read_po(pot)
+        # tweak message flags
+        for m in catalog:
+            msg = m.id
+            contains_brace = any(
+                '{' in s for s in (msg if isinstance(msg, tuple) else (msg,))
+            )
+            if contains_brace:
+                m.flags.add('python-brace-format')
+            m.flags.discard('python-format')
+        # write back
+        with open(pot_path, 'wb') as pot:
+            write_po(pot, catalog, width=0)
+
+    else:
+        print("unknown command")
+        raise SystemExit(1)
