2f11e1bfea696bdb62f6314e9714152016116226
Changaco <changaco@changaco.oy.lc>
2016-12-08 14:16:33 +0100

    merge branch.sql

--- sql/branch.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-    UPDATE emails
-       SET address = trim(substring(address, '^.+@') || lower(substring(address, '[^@]+$')))
-     WHERE address <> lower(trim(address))
-       AND verified IS TRUE;
-    UPDATE participants
-       SET email = trim(substring(email, '^.+@') || lower(substring(email, '[^@]+$')))
-     WHERE email <> lower(trim(email));
-END;
