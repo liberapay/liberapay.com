4c4a3752e6dc982fabb7ac862987872ec5640dec
Changaco <changaco@changaco.oy.lc>
2017-01-05 11:14:26 +0100

    improve python3 compat of constant_time_compare

--- liberapay/security/crypto.py
+++ liberapay/security/crypto.py
@@ -55,6 +55,10 @@ def constant_time_compare(val1, val2):
     if len(val1) != len(val2):
         return False
     result = 0
-    for x, y in zip(val1, val2):
-        result |= ord(x) ^ ord(y)
+    if isinstance(val1, bytes) and bytes != str:
+        for x, y in zip(val1, val2):
+            result |= x ^ y
+    else:
+        for x, y in zip(val1, val2):
+            result |= ord(x) ^ ord(y)
     return result == 0
