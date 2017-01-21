680421293b9ca8089537256480ef8b5aa5a914e6
Changaco <changaco@changaco.oy.lc>
2017-01-21 13:55:33 +0100

    regroup composite DB types in one file

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,5 +1,3 @@
 DROP TYPE community_with_participant CASCADE;
-\i sql/community_with_participant.sql
-
 DROP TYPE elsewhere_with_participant CASCADE;
-\i sql/elsewhere_with_participant.sql
+\i sql/composites.sql
