bb948bcde9898bbff29f3cae257f0528c9d7311c
Changaco <changaco@changaco.oy.lc>
2017-03-30 10:43:43 +0200

    fix tables creation order in `schema.sql`
    `invoices` is referenced in `transfers`

--- sql/schema.sql
+++ sql/schema.sql
@@ -174,6 +174,36 @@ CREATE TRIGGER update_current_tip INSTEAD OF UPDATE ON current_tips
     FOR EACH ROW EXECUTE PROCEDURE update_tip();
 
 
+-- invoices
+
+CREATE TYPE invoice_nature AS ENUM ('expense');
+
+CREATE TYPE invoice_status AS ENUM
+    ('pre', 'canceled', 'new', 'retracted', 'accepted', 'paid', 'rejected');
+
+CREATE TABLE invoices
+( id            serial            PRIMARY KEY
+, ctime         timestamptz       NOT NULL DEFAULT CURRENT_TIMESTAMP
+, sender        bigint            NOT NULL REFERENCES participants
+, addressee     bigint            NOT NULL REFERENCES participants
+, nature        invoice_nature    NOT NULL
+, amount        numeric(35,2)     NOT NULL CHECK (amount > 0)
+, description   text              NOT NULL
+, details       text
+, documents     jsonb             NOT NULL
+, status        invoice_status    NOT NULL
+);
+
+CREATE TABLE invoice_events
+( id            serial            PRIMARY KEY
+, invoice       int               NOT NULL REFERENCES invoices
+, participant   bigint            NOT NULL REFERENCES participants
+, ts            timestamptz       NOT NULL DEFAULT CURRENT_TIMESTAMP
+, status        invoice_status    NOT NULL
+, message       text
+);
+
+
 -- transfers -- balance transfers from one user to another
 
 CREATE TYPE transfer_context AS ENUM
@@ -507,36 +537,6 @@ CREATE TABLE newsletter_texts
 CREATE INDEX newsletter_texts_not_sent_idx
           ON newsletter_texts (scheduled_for ASC)
        WHERE sent_at IS NULL AND scheduled_for IS NOT NULL;
-
-
--- invoices
-
-CREATE TYPE invoice_nature AS ENUM ('expense');
-
-CREATE TYPE invoice_status AS ENUM
-    ('pre', 'canceled', 'new', 'retracted', 'accepted', 'paid', 'rejected');
-
-CREATE TABLE invoices
-( id            serial            PRIMARY KEY
-, ctime         timestamptz       NOT NULL DEFAULT CURRENT_TIMESTAMP
-, sender        bigint            NOT NULL REFERENCES participants
-, addressee     bigint            NOT NULL REFERENCES participants
-, nature        invoice_nature    NOT NULL
-, amount        numeric(35,2)     NOT NULL CHECK (amount > 0)
-, description   text              NOT NULL
-, details       text
-, documents     jsonb             NOT NULL
-, status        invoice_status    NOT NULL
-);
-
-CREATE TABLE invoice_events
-( id            serial            PRIMARY KEY
-, invoice       int               NOT NULL REFERENCES invoices
-, participant   bigint            NOT NULL REFERENCES participants
-, ts            timestamptz       NOT NULL DEFAULT CURRENT_TIMESTAMP
-, status        invoice_status    NOT NULL
-, message       text
-);
 
 
 -- composite types, keep this at the end of the file
