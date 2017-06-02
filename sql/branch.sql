0244f441d1d2534e88fa9a7f64d6f4ea44c289bd
Changaco <changaco@changaco.oy.lc>
2017-06-02 18:24:18 +0200

    add DB table for repositories

---
+++ sql/branch.sql
@@ -0,0 +1,18 @@
+CREATE TABLE repositories
+( id                    bigserial       PRIMARY KEY
+, platform              text            NOT NULL
+, remote_id             text            NOT NULL
+, owner_id              text            NOT NULL
+, name                  text            NOT NULL
+, slug                  text            NOT NULL
+, description           text
+, last_update           timestamptz     NOT NULL
+, is_fork               boolean
+, stars_count           int
+, extra_info            json
+, info_fetched_at       timestamptz     NOT NULL DEFAULT now()
+, participant           bigint          NOT NULL REFERENCES participants
+, show_on_profile       boolean         NOT NULL DEFAULT FALSE
+, UNIQUE (platform, remote_id)
+, UNIQUE (platform, slug)
+);
