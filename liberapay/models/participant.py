69ec9cec4cc4fb0e0e2234443a248ec390e81255
Eliot Berriot <contact@eliotberriot.com>
2017-06-18 16:23:01 +0200

    Removed style argument in render_verifications

--- liberapay/models/participant.py
+++ liberapay/models/participant.py
@@ -915,12 +915,10 @@ class Participant(Model, MixinTeam):
           ORDER BY is_new DESC, id DESC
         """, (self.id,))
 
-    def render_notifications(self, state, notifs=None, style=True):
+    def render_notifications(self, state, notifs=None):
         """Render notifications as HTML.
 
         The `notifs` argument allows rendering arbitrary notifications.
-
-        The `style` argument can be used to turn off inline button styles.
 
         """
         notifs = notifs or self.get_notifs()
@@ -932,8 +930,6 @@ class Participant(Model, MixinTeam):
                 context = dict(state)
                 self.fill_notification_context(context)
                 context.update(notif_context)
-                if not style:
-                    context['button_style'] = lambda variant: ''
                 spt = website.emails[event]
                 html = spt['text/html'].render(context).strip()
                 typ = notif_context.get('type', 'info')
