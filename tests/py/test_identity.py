a11adde67a53fa9a65d1a48805e37757058362b9
Changaco <changaco@changaco.oy.lc>
2017-07-20 11:41:31 +0200

    extend identity form test

--- tests/py/test_identity.py
+++ tests/py/test_identity.py
@@ -2,7 +2,12 @@
 
 from __future__ import print_function, unicode_literals
 
+from decimal import Decimal as D
+
+from liberapay.billing.transactions import charge, create_wallet
+from liberapay.models.exchange_route import ExchangeRoute
 from liberapay.testing import Harness
+from liberapay.testing.mangopay import create_card
 
 
 class TestIdentity(Harness):
@@ -41,17 +46,24 @@ class TestIdentity(Harness):
         janeway2 = janeway.refetch()
         assert janeway2.mangopay_user_id == janeway.mangopay_user_id
 
-        # Create a mangopay legal user
-        self.db.run("UPDATE participants SET kind = 'organization'")
-        self.db.run("UPDATE participants SET mangopay_user_id = mangopay_wallet_id = NULL")
-        data = {'LegalRepresentative'+k: v for k, v in user_data.items()}
+        # Add some money for the next test
+        create_wallet(self.db, janeway)
+        cr = create_card(janeway.mangopay_user_id)
+        ExchangeRoute.insert(janeway, 'mango-cc', cr.CardId)
+        charge(self.db, janeway, D('20.00'), 'http://127.0.0.1/')
+
+        # Switch to a legal user
+        data = dict(data2)
+        data['organization'] = 'yes'
         data['LegalPersonType'] = 'BUSINESS'
         data['Name'] = 'Starfleet'
-        data['terms'] = 'agree'
+        data['confirmed'] = 'yes'
         r = self.client.POST('/janeway/identity', data, **kw)
         assert r.code == 200, r.text
         janeway = janeway.refetch()
-        assert janeway.mangopay_user_id
+        assert janeway.mangopay_user_id != janeway2.mangopay_user_id
+        assert janeway.kind == 'organization'
+        self.db.self_check()
 
         # Edit the legal user
         data2 = dict(data, LegalPersonType='ORGANIZATION')
