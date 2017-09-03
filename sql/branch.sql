a547e45d1f378006f2fb6eec6cd071e67869bc2c
Changaco <changaco@changaco.oy.lc>
2017-09-03 10:17:13 +0200

    add a ctime column to the exchange_routes table

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,1 +1,13 @@
 DROP VIEW current_exchange_routes CASCADE;
+
+BEGIN;
+    ALTER TABLE exchange_routes ADD COLUMN ctime timestamptz;
+    UPDATE exchange_routes r
+       SET ctime = (
+               SELECT min(e.timestamp)
+                 FROM exchanges e
+                WHERE e.route = r.id
+           )
+     WHERE ctime IS NULL;
+    ALTER TABLE exchange_routes ALTER COLUMN ctime SET DEFAULT now();
+END;
