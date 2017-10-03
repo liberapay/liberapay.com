8cbb380f7c0dd676af598a8bb8d2b2fa25a254ff
Changaco <changaco@changaco.oy.lc>
2017-10-03 19:32:35 +0200

    fix rate limiting of password logins

--- liberapay/security/authentication.py
+++ liberapay/security/authentication.py
@@ -45,7 +45,7 @@ def sign_in_with_form_data(body, state):
         password = body.pop('log-in.password', None)
         k = 'email' if '@' in id else 'username'
         if password:
-            website.db.hit_rate_limit('log-in.password', k, TooManyPasswordLogins)
+            website.db.hit_rate_limit('log-in.password', id, TooManyPasswordLogins)
             p = Participant.authenticate(
                 k, 'password',
                 id, password,
