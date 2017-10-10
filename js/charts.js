af4cb5c091a63af772f8c04179761080312f330f
Bat <baptiste@gelez.xyz>
2017-10-10 17:25:35 +0100

    Redesign profile page

--- js/charts.js
+++ js/charts.js
@@ -14,7 +14,7 @@ Liberapay.charts.make = function(series, button) {
     } else if (button && $(button).data('msg-empty')) {
         var $btn = $(button);
         $btn.attr('disabled', '').prop('disabled');
-        $btn.after($('<span>').text(' '+$btn.data('msg-empty')));
+        $('#income').text(' ' + $btn.data('msg-empty'));
         return;
     } else {
         return;
