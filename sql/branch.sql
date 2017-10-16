bacf160cf3f1ae137563b570cf6e91d57e345ed2
Changaco <changaco@changaco.oy.lc>
2017-10-16 13:13:05 +0200

    create SQL composite type `currency_amount`

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,3 +1,6 @@
-CREATE TYPE currency AS ENUM ('EUR', 'USD');
+BEGIN;
+    \i sql/currencies.sql
+END;
+
 ALTER TABLE participants ADD COLUMN main_currency currency NOT NULL DEFAULT 'EUR';
 ALTER TABLE participants ADD COLUMN accept_all_currencies boolean NOT NULL DEFAULT FALSE;
---
+++ sql/currencies.sql
@@ -0,0 +1,240 @@
+-- Base types
+
+CREATE TYPE currency AS ENUM ('EUR', 'USD');
+CREATE TYPE currency_amount AS (amount numeric, currency currency);
+
+
+-- Arithmetic operators
+
+CREATE FUNCTION currency_amount_add(currency_amount, currency_amount)
+RETURNS currency_amount AS $$
+    BEGIN
+        IF ($1.currency <> $2.currency) THEN
+            RAISE 'currency mistmatch: % != %', $1.currency, $2.currency;
+        END IF;
+        RETURN ($1.amount + $2.amount, $1.currency);
+    END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR + (
+    leftarg = currency_amount,
+    rightarg = currency_amount,
+    procedure = currency_amount_add,
+    commutator = +
+);
+
+CREATE FUNCTION currency_amount_sub(currency_amount, currency_amount)
+RETURNS currency_amount AS $$
+    BEGIN
+        IF ($1.currency <> $2.currency) THEN
+            RAISE 'currency mistmatch: % != %', $1.currency, $2.currency;
+        END IF;
+        RETURN ($1.amount - $2.amount, $1.currency);
+    END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR - (
+    leftarg = currency_amount,
+    rightarg = currency_amount,
+    procedure = currency_amount_sub
+);
+
+CREATE FUNCTION currency_amount_neg(currency_amount)
+RETURNS currency_amount AS $$
+    BEGIN RETURN (-$1.amount, $1.currency); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR - (
+    rightarg = currency_amount,
+    procedure = currency_amount_neg
+);
+
+
+-- Aggregate functions
+
+CREATE AGGREGATE sum(currency_amount) (
+    sfunc = currency_amount_add,
+    stype = currency_amount
+);
+
+
+-- Convenience functions
+
+CREATE FUNCTION zero(currency_amount) RETURNS currency_amount AS $$
+    BEGIN RETURN (0::numeric, $1.currency); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+
+-- Comparison operators
+
+CREATE FUNCTION currency_amount_eq(currency_amount, currency_amount)
+RETURNS boolean AS $$
+    BEGIN RETURN ($1.currency = $2.currency AND $1.amount = $2.amount); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR = (
+    leftarg = currency_amount,
+    rightarg = currency_amount,
+    procedure = currency_amount_eq,
+    commutator = =
+);
+
+CREATE FUNCTION currency_amount_ne(currency_amount, currency_amount)
+RETURNS boolean AS $$
+    BEGIN RETURN ($1.currency <> $2.currency OR $1.amount <> $2.amount); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR <> (
+    leftarg = currency_amount,
+    rightarg = currency_amount,
+    procedure = currency_amount_ne,
+    commutator = <>
+);
+
+CREATE FUNCTION currency_amount_gt(currency_amount, currency_amount)
+RETURNS boolean AS $$
+    BEGIN
+        IF ($1.currency <> $2.currency) THEN
+            RAISE 'currency mistmatch: % != %', $1.currency, $2.currency;
+        END IF;
+        RETURN ($1.amount > $2.amount);
+    END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR > (
+    leftarg = currency_amount,
+    rightarg = currency_amount,
+    procedure = currency_amount_gt,
+    commutator = <,
+    negator = <=
+);
+
+CREATE FUNCTION currency_amount_gte(currency_amount, currency_amount)
+RETURNS boolean AS $$
+    BEGIN
+        IF ($1.currency <> $2.currency) THEN
+            RAISE 'currency mistmatch: % != %', $1.currency, $2.currency;
+        END IF;
+        RETURN ($1.amount >= $2.amount);
+    END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR >= (
+    leftarg = currency_amount,
+    rightarg = currency_amount,
+    procedure = currency_amount_gte,
+    commutator = <=,
+    negator = <
+);
+
+CREATE FUNCTION currency_amount_lt(currency_amount, currency_amount)
+RETURNS boolean AS $$
+    BEGIN
+        IF ($1.currency <> $2.currency) THEN
+            RAISE 'currency mistmatch: % != %', $1.currency, $2.currency;
+        END IF;
+        RETURN ($1.amount < $2.amount);
+    END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR < (
+    leftarg = currency_amount,
+    rightarg = currency_amount,
+    procedure = currency_amount_lt,
+    commutator = >,
+    negator = >=
+);
+
+CREATE FUNCTION currency_amount_lte(currency_amount, currency_amount)
+RETURNS boolean AS $$
+    BEGIN
+        IF ($1.currency <> $2.currency) THEN
+            RAISE 'currency mistmatch: % != %', $1.currency, $2.currency;
+        END IF;
+        RETURN ($1.amount <= $2.amount);
+    END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR <= (
+    leftarg = currency_amount,
+    rightarg = currency_amount,
+    procedure = currency_amount_lte,
+    commutator = >=,
+    negator = >
+);
+
+CREATE FUNCTION currency_amount_eq_numeric(currency_amount, numeric)
+RETURNS boolean AS $$
+    BEGIN RETURN ($1.amount = $2); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR = (
+    leftarg = currency_amount,
+    rightarg = numeric,
+    procedure = currency_amount_eq_numeric,
+    commutator = =
+);
+
+CREATE FUNCTION currency_amount_ne_numeric(currency_amount, numeric)
+RETURNS boolean AS $$
+    BEGIN RETURN ($1.amount <> $2); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR <> (
+    leftarg = currency_amount,
+    rightarg = numeric,
+    procedure = currency_amount_ne_numeric,
+    commutator = <>
+);
+
+CREATE FUNCTION currency_amount_gt_numeric(currency_amount, numeric)
+RETURNS boolean AS $$
+    BEGIN RETURN ($1.amount > $2); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR > (
+    leftarg = currency_amount,
+    rightarg = numeric,
+    procedure = currency_amount_gt_numeric,
+    commutator = <,
+    negator = <=
+);
+
+CREATE FUNCTION currency_amount_gte_numeric(currency_amount, numeric)
+RETURNS boolean AS $$
+    BEGIN RETURN ($1.amount >= $2); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR >= (
+    leftarg = currency_amount,
+    rightarg = numeric,
+    procedure = currency_amount_gte_numeric,
+    commutator = <=,
+    negator = <
+);
+
+CREATE FUNCTION currency_amount_lt_numeric(currency_amount, numeric)
+RETURNS boolean AS $$
+    BEGIN RETURN ($1.amount < $2); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR < (
+    leftarg = currency_amount,
+    rightarg = numeric,
+    procedure = currency_amount_lt_numeric,
+    commutator = >,
+    negator = >=
+);
+
+CREATE FUNCTION currency_amount_lte_numeric(currency_amount, numeric)
+RETURNS boolean AS $$
+    BEGIN RETURN ($1.amount <= $2); END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+CREATE OPERATOR <= (
+    leftarg = currency_amount,
+    rightarg = numeric,
+    procedure = currency_amount_lte_numeric,
+    commutator = >=,
+    negator = >
+);
