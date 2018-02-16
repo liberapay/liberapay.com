8edd4430c3d3d9a8724c01b0c03737f3c04bffff
Changaco <changaco@changaco.oy.lc>
2018-02-16 09:36:07 +0100

    merge branch.sql

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -1252,3 +1252,23 @@ UPDATE participants
             LIMIT 1
        )
  WHERE length(email_lang) > 0;
+
+-- migration #60
+CREATE OR REPLACE FUNCTION convert(currency_amount, currency, boolean) RETURNS currency_amount AS $$
+    DECLARE
+        rate numeric;
+        result currency_amount;
+    BEGIN
+        IF ($1.currency = $2) THEN RETURN $1; END IF;
+        rate := (
+            SELECT r.rate
+              FROM currency_exchange_rates r
+             WHERE r.source_currency = $1.currency
+        );
+        IF (rate IS NULL) THEN
+            RAISE 'missing exchange rate %->%', $1.currency, $2;
+        END IF;
+        result := ($1.amount * rate, $2);
+        RETURN (CASE WHEN $3 THEN round(result) ELSE result END);
+    END;
+$$ LANGUAGE plpgsql STRICT;
