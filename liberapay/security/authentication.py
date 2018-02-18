6768da0a2d0ecc0bcaa1d5af5efdde2d30f6d675
Changaco <changaco@changaco.oy.lc>
2018-02-18 14:31:15 +0100

    fix missing argument in call to `hit_rate_limit`

--- liberapay/security/authentication.py
+++ liberapay/security/authentication.py
@@ -70,7 +70,7 @@ def sign_in_with_form_data(body, state):
                 )
             elif p:
                 if not p.get_email(email).verified:
-                    website.db.hit_rate_limit('log-in.email.not-verified', TooManyLoginEmails)
+                    website.db.hit_rate_limit('log-in.email.not-verified', email, TooManyLoginEmails)
                 website.db.hit_rate_limit('log-in.email', p.id, TooManyLoginEmails)
                 p.start_session()
                 qs = {'log-in.id': p.id, 'log-in.token': p.session_token}
