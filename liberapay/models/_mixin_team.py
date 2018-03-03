942df84e256f49df5863c75eafd4dcd513ede43b
Changaco <changaco@changaco.oy.lc>
2018-03-03 19:58:29 +0100

    change the default currency of a take
    From the team's main currency to the member's main currency.

--- liberapay/models/_mixin_team.py
+++ liberapay/models/_mixin_team.py
@@ -39,7 +39,7 @@ class MixinTeam(object):
             raise MemberLimitReached
         if member.status != 'active':
             raise InactiveParticipantAdded
-        self.set_take_for(member, ZERO[self.main_currency], self, cursor=cursor)
+        self.set_take_for(member, ZERO[member.main_currency], self, cursor=cursor)
 
     def remove_all_members(self, cursor=None):
         (cursor or self.db).run("""
