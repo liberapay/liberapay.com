b3a8a14ffde4bc928fdabce65b216c69791ffba6
Changaco <changaco@changaco.oy.lc>
2018-03-07 10:58:24 +0100

    restore donations that were zeroed out in the past

---
+++ sql/branch.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+    WITH zeroed_tips AS (
+             SELECT t.id
+               FROM events e
+               JOIN current_tips t ON t.tippee = e.participant
+                                  AND t.mtime = e.ts
+                                  AND t.amount = 0
+              WHERE e.type = 'set_status' AND e.payload = '"closed"'
+                 OR e.type = 'set_goal' AND e.payload::text LIKE '"-%"'
+         )
+    DELETE FROM tips t WHERE EXISTS (SELECT 1 FROM zeroed_tips z WHERE z.id = t.id);
+
+END;
