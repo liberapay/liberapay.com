a8af6da4a3f825713a46c45a67026bddf8347f59
Changaco <changaco@changaco.oy.lc>
2018-03-16 10:26:31 +0100

    fix parsing of language plural rules

--- liberapay/utils/i18n.py
+++ liberapay/utils/i18n.py
@@ -222,18 +222,25 @@ HTTP_ERRORS = {
 del _
 
 
-ternary_re = re.compile(r'^\(? *(.+?) *\? *(.+?) *: *(.+?) *\)?$')
+ternary_re = re.compile(r'^(.+?) *\? *(.+?) *: *(.+?)$')
 and_re = re.compile(r' *&& *')
 or_re = re.compile(r' *\|\| *')
 
 
+def strip_parentheses(s):
+    s = s.strip()
+    if s[:1] == '(' and s[-1:] == ')':
+        s = s[1:-1].strip()
+    return s
+
+
 def ternary_sub(m):
     g1, g2, g3 = m.groups()
-    return '%s if %s else %s' % (g2, g1, ternary_re.sub(ternary_sub, g3))
+    return '%s if %s else %s' % (g2, g1, ternary_re.sub(ternary_sub, strip_parentheses(g3)))
 
 
 def get_function_from_rule(rule):
-    rule = ternary_re.sub(ternary_sub, rule.strip())
+    rule = ternary_re.sub(ternary_sub, strip_parentheses(rule))
     rule = and_re.sub(' and ', rule)
     rule = or_re.sub(' or ', rule)
     return eval('lambda n: ' + rule, {'__builtins__': {}})
