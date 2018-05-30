168b78833a5dda9e6b00e86be33c5a20a7f4e9cd
Changaco <changaco@changaco.oy.lc>
2018-05-30 10:30:30 +0200

    merge branch.sql

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -1421,3 +1421,18 @@ UPDATE elsewhere
        SET description = extra_info->'snippet'->>'description'
      WHERE platform = 'youtube'
        AND length(extra_info->'snippet'->>'description') > 0;
+
+-- migration #68
+WITH zeroed_tips AS (
+         SELECT t.id
+           FROM events e
+           JOIN current_tips t ON t.tippee = e.participant
+                              AND t.mtime = e.ts
+                              AND t.amount = 0
+          WHERE e.type = 'set_status' AND e.payload = '"closed"'
+             OR e.type = 'set_goal' AND e.payload::text LIKE '"-%"'
+     )
+DELETE FROM tips t WHERE EXISTS (SELECT 1 FROM zeroed_tips z WHERE z.id = t.id);
+UPDATE events
+   SET recorder = (payload->>'invitee')::int
+ WHERE type IN ('invite_accept', 'invite_refuse');
