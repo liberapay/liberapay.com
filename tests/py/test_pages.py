e5b5eb90e30e7968cda9a9d8e71b8cd6e5162427
Changaco <changaco@changaco.oy.lc>
2018-11-17 17:51:54 +0100

    make it easier to profile Liberapay's code

--- tests/py/test_pages.py
+++ tests/py/test_pages.py
@@ -14,7 +14,7 @@ import pytest
 from liberapay.billing.payday import Payday
 from liberapay.constants import SESSION
 from liberapay.testing import EUR
-from liberapay.testing.mangopay import MangopayHarness
+from liberapay.testing.mangopay import Harness, MangopayHarness
 from liberapay.utils import b64encode_s, find_files
 from liberapay.wireup import NoDB
 
@@ -93,6 +93,21 @@ class TestTranslations(BrowseTestHarness):
         self.browse_setup()
         for _, l, _, _ in self.client.website.lang_list:
             self.browse(HTTP_ACCEPT_LANGUAGE=l.encode('ascii'))
+
+
+@pytest.mark.skipif(
+    os.environ.get('LIBERAPAY_PROFILING') != 'yes',
+    reason="these tests are only for profiling",
+)
+class TestPerformance(Harness):
+
+    def test_performance_of_homepage(self):
+        for i in range(1000):
+            self.client.GET('/')
+
+    def test_performance_when_serving_static_file(self):
+        for i in range(10000):
+            self.client.GET('/assets/avatar-default.png')
 
 
 class TestPages(BrowseTestHarness):
