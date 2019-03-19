396d1f29025194ee5866af0c8cc5386d5cf90131
Charly C <changaco@changaco.oy.lc>
2019-03-19 10:45:10 +0100

    fix the SQL `compute_payment_providers` function (#1471)
    We need to exclude zeroed takes because that's what the Python function `resolve_destination` does. Without this users can be encouraged to donate but end up getting an error message saying that their donation can't be processed.

---
+++ sql/branch.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION compute_payment_providers(bigint) RETURNS bigint AS $$
+    SELECT coalesce((
+        SELECT sum(DISTINCT array_position(
+                                enum_range(NULL::payment_providers),
+                                a.provider::payment_providers
+                            ))
+          FROM payment_accounts a
+         WHERE ( a.participant = $1 OR
+                 a.participant IN (
+                     SELECT t.member
+                       FROM current_takes t
+                      WHERE t.team = $1
+                        AND t.amount <> 0
+                 )
+               )
+           AND a.is_current IS TRUE
+           AND a.verified IS TRUE
+           AND coalesce(a.charges_enabled, true)
+    ), 0);
+$$ LANGUAGE SQL STRICT;
+
+UPDATE participants SET payment_providers = compute_payment_providers(id) WHERE kind = 'group';
