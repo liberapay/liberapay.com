7224d31b21b4b7c4f78329add9f6b35e27808757
Changaco <changaco@changaco.oy.lc>
2019-03-20 11:12:34 +0100

    merge branch.sql

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -2282,3 +2282,26 @@ ALTER TABLE notifications ADD COLUMN context_is_cbor boolean;
 
 -- migration #99
 ALTER TABLE notifications DROP COLUMN context_is_cbor;
+
+-- migration #100
+CREATE OR REPLACE FUNCTION compute_payment_providers(bigint) RETURNS bigint AS $$
+    SELECT coalesce((
+        SELECT sum(DISTINCT array_position(
+                                enum_range(NULL::payment_providers),
+                                a.provider::payment_providers
+                            ))
+          FROM payment_accounts a
+         WHERE ( a.participant = $1 OR
+                 a.participant IN (
+                     SELECT t.member
+                       FROM current_takes t
+                      WHERE t.team = $1
+                        AND t.amount <> 0
+                 )
+               )
+           AND a.is_current IS TRUE
+           AND a.verified IS TRUE
+           AND coalesce(a.charges_enabled, true)
+    ), 0);
+$$ LANGUAGE SQL STRICT;
+UPDATE participants SET payment_providers = compute_payment_providers(id) WHERE kind = 'group';
