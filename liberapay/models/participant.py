189102b97fc2856147d3f4d5ee92354a9b7031ae
Changaco <changaco@changaco.oy.lc>
2019-06-10 09:28:19 +0200

    improve `Participant.get_email_address()`

--- liberapay/models/participant.py
+++ liberapay/models/participant.py
@@ -1123,10 +1123,16 @@ class Participant(Model, MixinTeam):
         """
         return self.email or (cursor or self.db).one("""
             SELECT address
-              FROM emails
+              FROM emails e
              WHERE participant = %s
                AND ( %s OR disavowed IS NOT true )
-          ORDER BY disavowed IS NOT true DESC, added_time ASC
+          ORDER BY disavowed IS NOT true DESC
+                 , ( SELECT count(b)
+                       FROM email_blacklist b
+                      WHERE lower(b.address) = lower(e.address)
+                        AND (b.ignore_after IS NULL OR b.ignore_after > current_timestamp)
+                   ) ASC
+                 , added_time ASC
              LIMIT 1
         """, (self.id, allow_disavowed))
 
