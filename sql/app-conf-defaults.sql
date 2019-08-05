2912fd9f9474a6bd6147499cb0cc972f2fa69eb3
Changaco <changaco@changaco.oy.lc>
2019-08-05 18:08:07 +0200

    complete the merge of branch.sql

--- sql/app-conf-defaults.sql
+++ sql/app-conf-defaults.sql
@@ -10,6 +10,7 @@ INSERT INTO app_conf (key, value) VALUES
     ('bountysource_callback', '"http://127.0.0.1:8339/on/bountysource/associate"'::jsonb),
     ('bountysource_id', 'null'::jsonb),
     ('bountysource_secret', '""'::jsonb),
+    ('check_avatar_urls', 'true'::jsonb),
     ('check_email_domains', 'true'::jsonb),
     ('cron_intervals', jsonb_build_object()),
     ('facebook_callback', '"http://localhost:8339/on/facebook/associate"'::jsonb),
