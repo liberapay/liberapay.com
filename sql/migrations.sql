e54f67d934e6d5325099c67d98ba010a24ac1b9c
Changaco <changaco@changaco.oy.lc>
2019-10-16 14:24:50 +0200

    merge branch.sql

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -2374,3 +2374,23 @@ ALTER TABLE payin_refunds ALTER COLUMN reason DROP NOT NULL;
 
 -- migration #108
 UPDATE emails SET verified = null WHERE participant IS null AND verified IS true;
+
+-- migration #109
+CREATE OR REPLACE FUNCTION update_payment_providers() RETURNS trigger AS $$
+    DECLARE
+        rec record;
+    BEGIN
+        rec := (CASE WHEN TG_OP = 'DELETE' THEN OLD ELSE NEW END);
+        UPDATE participants
+           SET payment_providers = compute_payment_providers(id)
+         WHERE id = rec.participant
+            OR id IN (
+                   SELECT t.team FROM current_takes t WHERE t.member = rec.participant
+               );
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+UPDATE participants
+   SET payment_providers = compute_payment_providers(id)
+ WHERE kind = 'group'
+   AND payment_providers <> compute_payment_providers(id);
