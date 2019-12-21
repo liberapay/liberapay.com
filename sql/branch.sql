79c0e48ae5e4f289b54101a8f016a89de1d9d547
Changaco <changaco@changaco.oy.lc>
2019-12-21 14:14:08 +0100

    use a partial index for `exchange_routes.is_default`
    Should be more efficient.

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,5 +1,5 @@
 BEGIN;
     ALTER TABLE exchange_routes DROP CONSTRAINT currency_chk;
-    ALTER TABLE exchange_routes ADD COLUMN is_default boolean CHECK (NOT (is_default IS FALSE));
-    CREATE UNIQUE INDEX exchange_routes_is_default_key ON exchange_routes (participant, is_default);
+    ALTER TABLE exchange_routes ADD COLUMN is_default boolean;
+    CREATE UNIQUE INDEX exchange_routes_is_default_key ON exchange_routes (participant, is_default) WHERE is_default IS TRUE;
 END;
