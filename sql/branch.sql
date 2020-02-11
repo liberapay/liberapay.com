7a256333e50cb8962fef4aa20af786cdbb88f477
Changaco <changaco@changaco.oy.lc>
2020-02-11 13:52:07 +0100

    tweak DB correction to avoid creating anomalies

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,4 +1,11 @@
 UPDATE scheduled_payins
    SET execution_date = '2020-02-14'
  WHERE execution_date < '2020-02-14'::date
-   AND automatic IS TRUE;
+   AND automatic IS TRUE
+   AND payin IS NULL;
+
+UPDATE scheduled_payins
+   SET execution_date = (SELECT pi.ctime::date FROM payins pi WHERE pi.id = payin)
+ WHERE execution_date < '2020-02-14'::date
+   AND automatic IS TRUE
+   AND payin IS NOT NULL;
