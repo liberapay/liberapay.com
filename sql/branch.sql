a148c8ace1ffb8c77c9eacb64ad4044e2ca20095
Changaco <changaco@changaco.oy.lc>
2020-03-14 11:57:38 +0100

    create index on `repositories.info_fetched_at`

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,1 +1,3 @@
 CREATE INDEX repositories_participant_idx ON repositories (participant, show_on_profile);
+CREATE INDEX repositories_info_fetched_at_idx ON repositories (info_fetched_at ASC)
+    WHERE participant IS NOT NULL AND show_on_profile;
