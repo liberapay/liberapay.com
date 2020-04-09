liberapay/liberapay.com

commit 70f23e64c55e2882cedd12f93e5857f2e7f85fc0

only extract/generate/return a CSRF token as needed (#1716)
This commit optimizes the anti-CSRF mechanism to only extract/generate/return a token on demand, i.e. when there's a form in the page being rendered.

This change is probably a good step towards fixing <https://github.com/liberapay/liberapay.com/issues/1131>.

Theoretically this change also makes it easier to cache some dynamic pages like the homepage (<https://github.com/liberapay/liberapay.com/issues/124>), however in practice it still seems to be difficult.

liberapay/main.py
@@ -184,6 +184,7 @@ algorithm.functions = [
     canonize,
     algorithm['extract_accept_header'],
     set_default_security_headers,
+    csrf.add_csrf_token_to_state,
     set_up_i18n,
     insert_constants,
     authentication.start_user_as_anon,

liberapay/security/csrf.py
@@ -21,13 +21,58 @@ CSRF_TOKEN = 'csrf_token'
 CSRF_TIMEOUT = timedelta(days=7)
 
 
+class CSRF_Token:
+    """A lazy anti-CSRF token generator.
+    """
+
+    __slots__ = ('state', '_token')
+
+    def __init__(self, state):
+        self.state = state
+        self._token = None
+
+    def __bool__(self):
+        return bool(self._token)
+
+    def __eq__(self, other):
+        return self._token == other
+
+    def __ne__(self, other):
+        return self._token != other
+
+    def __repr__(self):
+        return f"<CSRF_Token _token={self._token!r}>"
+
+    def __str__(self):
+        return self.token
+
+    @property
+    def token(self):
+        if not self._token:
+            try:
+                cookie_token = self.state['request'].headers.cookie[CSRF_TOKEN].value
+            except KeyError:
+                cookie_token = ''
+            if len(cookie_token) == TOKEN_LENGTH:
+                self._token = cookie_token
+            else:
+                self._token = get_random_string(TOKEN_LENGTH)
+        return self._token
+
+
+def add_csrf_token_to_state(state):
+    state['csrf_token'] = CSRF_Token(state)
+
+
 def reject_forgeries(state, request, response, website, _):
     request_path = request.path.raw
     off = (
-        # Don't generate CSRF tokens for assets, to avoid busting the cache.
-        request_path.startswith('/assets/') or
-        # Don't generate or check CSRF tokens for callbacks, it's not necessary.
-        request_path.startswith('/callbacks/')
+        # Assume that methods defined as 'safe' by RFC7231 don't need protection.
+        request.method in SAFE_METHODS or
+        # Don't check CSRF tokens for callbacks, it's not necessary.
+        request_path.startswith('/callbacks/') or
+        # CSRF protection is turned off for this request.
+        request_path == '/migrate' and not request.qs
     )
     if off:
         return
@@ -36,20 +81,6 @@ def reject_forgeries(state, request, response, website, _):
     try:
         cookie_token = request.headers.cookie[CSRF_TOKEN].value
     except KeyError:
-        cookie_token = None
-
-    if cookie_token and len(cookie_token) == TOKEN_LENGTH:
-        state['csrf_token'] = cookie_token
-    else:
-        state['csrf_token'] = get_random_string(TOKEN_LENGTH)
-
-    if request.method in SAFE_METHODS:
-        # Assume that methods defined as 'safe' by RFC7231 don't need protection.
-        return
-    elif request_path == '/migrate' and not request.qs:
-        # CSRF protection is turned off for this request.
-        return
-    elif not cookie_token:
         raise response.error(403, _(
             "A security check has failed. Please make sure your browser is "
             "configured to allow cookies for {domain}, then try again.",
@@ -72,7 +103,7 @@ def reject_forgeries(state, request, response, website, _):
         if not second_token:
             raise response.error(403, "The X-CSRF-TOKEN header is missing.")
 
-    if not constant_time_compare(second_token, state['csrf_token']):
+    if not constant_time_compare(second_token, cookie_token):
         raise response.error(403, "The anti-CSRF tokens don't match.")
 
 
@@ -82,4 +113,4 @@ def add_token_to_response(response, csrf_token=None):
     if csrf_token:
         # Don't set httponly so that we can POST using XHR.
         # https://github.com/gratipay/gratipay.com/issues/3030
-        response.set_cookie(CSRF_TOKEN, csrf_token, expires=CSRF_TIMEOUT, httponly=False)
+        response.set_cookie(CSRF_TOKEN, str(csrf_token), expires=CSRF_TIMEOUT, httponly=False)

tests/py/test_state_chain.py
@@ -79,12 +79,13 @@ class Tests(Harness):
 
     def test_csrf_cookie_properties(self):
         r = self.client.GET(
-            '/',
+            '/log-in',
             HTTP_X_FORWARDED_PROTO=b'https', HTTP_HOST=b'en.example.com',
             csrf_token=None, raise_immediately=False,
         )
-        assert r.code == 200
+        assert r.code == 200, r.text
         cookie = r.headers.cookie[csrf.CSRF_TOKEN]
+        assert len(cookie.value) == 32
         assert cookie['domain'] == '.example.com'
         assert cookie['expires'][-4:] == ' GMT'
         assert cookie['path'] == '/'
@@ -135,45 +136,51 @@ class Tests2(Harness):
         r = self.client.POST('/', csrf_token=False, raise_immediately=False)
         assert r.code == 403
         assert "cookie" in r.text
-        assert csrf.CSRF_TOKEN in r.headers.cookie
+        assert csrf.CSRF_TOKEN not in r.headers.cookie
 
     def test_no_csrf_cookie_unknown_method_on_asset(self):
         r = self.client.hit('UNKNOWN', '/assets/base.css', csrf_token=False,
                             raise_immediately=False)
-        assert r.code == 405
+        assert r.code == 403
 
-    def test_bad_csrf_cookie(self):
-        r = self.client.POST('/', csrf_token='bad_token', raise_immediately=False)
+    def test_csrf_cookie_mismatch(self):
+        r = self.client.POST('/', {'csrf_token': 'a'*32}, csrf_token='b'*32, raise_immediately=False)
         assert r.code == 403
         assert "The anti-CSRF tokens don't match." in r.text
-        assert r.headers.cookie[csrf.CSRF_TOKEN].value != 'bad_token'
 
-    def test_csrf_cookie_set_for_most_requests(self):
-        r = self.client.GET('/')
+    def test_csrf_cookie_set_for_log_in_page(self):
+        r = self.client.GET('/log-in')
         assert csrf.CSRF_TOKEN in r.headers.cookie
+
+    def test_no_csrf_cookie_set_for_homepage(self):
+        r = self.client.GET('/')
+        assert csrf.CSRF_TOKEN not in r.headers.cookie
 
     def test_no_csrf_cookie_set_for_assets(self):
         r = self.client.GET('/assets/base.css')
         assert csrf.CSRF_TOKEN not in r.headers.cookie
 
-    def test_reject_forgeries_accepts_good_token(self):
+    def test_CSRF_Token_accepts_good_token(self):
         token = 'ddddeeeeaaaaddddbbbbeeeeeeeeffff'
-        state = self.client.GET('/', csrf_token=token, return_after='reject_forgeries', want='state')
+        state = self.client.GET('/log-in', csrf_token=token, want='state')
         assert state['csrf_token'] == token
+        assert token in state['response'].text
 
-    def test_reject_forgeries_rejects_overlong_token(self):
+    def test_CSRF_Token_rejects_overlong_token(self):
         token = 'ddddeeeeaaaaddddbbbbeeeeeeeefffff'
-        state = self.client.GET('/', csrf_token=token, return_after='reject_forgeries', want='state')
+        state = self.client.GET('/log-in', csrf_token=token, want='state')
+        assert state['csrf_token']
         assert state['csrf_token'] != token
 
-    def test_reject_forgeries_rejects_underlong_token(self):
+    def test_CSRF_Token_rejects_underlong_token(self):
         token = 'ddddeeeeaaaaddddbbbbeeeeeeeefff'
-        state = self.client.GET('/', csrf_token=token, return_after='reject_forgeries', want='state')
+        state = self.client.GET('/log-in', csrf_token=token, want='state')
+        assert state['csrf_token']
         assert state['csrf_token'] != token
 
-    def test_reject_forgeries_accepts_token_with_non_base64_chars(self):
+    def test_CSRF_Token_accepts_token_with_non_base64_chars(self):
         token = 'ddddeeeeaaaadddd bbbbeeeeeeeefff'
-        state = self.client.GET('/', csrf_token=token, return_after='reject_forgeries', want='state')
+        state = self.client.GET('/log-in', csrf_token=token, want='state')
         assert state['csrf_token'] == token
 
     def test_malformed_body(self):

www/migrate.spt
@@ -45,11 +45,11 @@ elif step == '2':
             r = requests.post(
                 gratipay_migrate_url % (body['username'],),
                 data={
-                    'csrf_token': csrf_token,
+                    'csrf_token': 'ThisIsATokenThatIsThirtyTwoBytes',
                     'secret': website.env.secret_for_gratipay,
                 },
                 cookies={
-                    'csrf_token': csrf_token,
+                    'csrf_token': 'ThisIsATokenThatIsThirtyTwoBytes',
                     'session': body['session_token'],
                 }
             )

commit d1ee3399916a308af7f46ec6a1b262483393b801

fix the execution order of our state functions
This commit fixes the CSRF token regression in #1716.

liberapay/main.py
@@ -210,17 +210,16 @@ algorithm.functions = [
     turn_socket_error_into_50X,
     algorithm['get_response_for_exception'],
 
+    delegate_error_to_simplate,
+    tell_sentry,
+
     bypass_csp_for_form_redirects,
     authentication.add_auth_to_response,
     csrf.add_token_to_response,
     http_caching.add_caching_to_response,
-
-    delegate_error_to_simplate,
-    tell_sentry,
-    return_500_for_exception,
-
     overwrite_status_code_of_gateway_errors,
 
+    return_500_for_exception,
     tell_sentry,
 ]
 