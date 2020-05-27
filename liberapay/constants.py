f8a86c289bc43a258744fd7dee329514e79c0ba5
Charly C <changaco@changaco.oy.lc>
2020-05-27 14:35:44 +0200

    update the list of countries supported by Stripe
    https://stripe.com/blog/stripe-launches-in-five-more-european-countries

--- liberapay/constants.py
+++ liberapay/constants.py
@@ -359,8 +359,8 @@ PAYOUT_COUNTRIES = {
     """.split()),  # https://www.paypal.com/us/webapps/mpp/country-worldwide
 
     'stripe': set("""
-        AT AU BE CA CH CZ DE DK EE ES FI FR GB GR HK IE IT JP LT LU LV MX MY NL NO
-        NZ PL PT SE SG SI SK US
+        AT AU BE BG CA CH CY CZ DE DK EE ES FI FR GB GR HK IE IT JP LT LU LV MT
+        MX MY NL NO NZ PL PT RO SE SG SI SK US
         PR
     """.split()),  # https://stripe.com/global
 }
