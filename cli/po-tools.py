a69feb8e00eea3e4be116f11f09bb1343c64d10c
Changaco <changaco@changaco.oy.lc>
2021-08-15 10:20:38 +0200

    fix a sentence in `/about/teams`

--- cli/po-tools.py
+++ cli/po-tools.py
@@ -42,6 +42,32 @@ elif sys.argv[1] == 'copy':
         with open(po_path, 'wb') as po:
             write_po(po, catalog, width=0)
 
+elif sys.argv[1] == 'fix':
+    po_path = sys.argv[2]
+    old_msg = sys.argv[3]
+    new_msg = sys.argv[4]
+    new_msg_plural = sys.argv[5] if len(sys.argv) > 5 else None
+    print('fixing a message in PO file', po_path)
+    # read PO file
+    lang = po_path.rsplit('/', 1)[-1].split('.', 1)[0]
+    with open(po_path, 'rb') as po:
+        catalog = read_po(po, locale=lang)
+    # check that the new msg doesn't already exist
+    m = catalog.get(new_msg)
+    if m and any(m.string):
+        raise SystemExit(0)
+    # modify the old msg
+    m = catalog.get(old_msg)
+    if new_msg_plural:
+        assert isinstance(m.id, tuple)
+        m.id = (new_msg, new_msg_plural)
+    else:
+        assert not isinstance(m.id, tuple)
+        m.id = new_msg
+    # write back
+    with open(po_path, 'wb') as po:
+        write_po(po, catalog, width=0)
+
 elif sys.argv[1] == 'fuzz':
     po_path = sys.argv[2]
     old_msg = sys.argv[3]
