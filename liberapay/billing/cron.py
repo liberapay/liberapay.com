998cc4319b93eecf351962f122d94a3094ee6d94
sjcu <sjcu@umich.edu>
2021-12-12 19:20:56 -0500

    turned 'missing_profile_notification' into a cron job, saved in new file billing/cron.py, and moved tests to test_cron.py

---
+++ liberapay/billing/cron.py
@@ -0,0 +1,26 @@
+from ..cron import logger
+from ..website import website
+
+def generate_profile_description_missing_notifications():
+    """Send a notification to users without a profile description to add one
+    if they begin to receive payments. #2013"""
+
+    tippees_without_description = website.db.all("""
+        SELECT DISTINCT tippee FROM transfers t
+        WHERE status = 'succeeded'
+        AND t.context IN ('tip', 'take', 'partial-take')
+        AND t.tippee NOT IN (
+            SELECT DISTINCT participant
+            FROM statements
+        )
+        AND t.tippee NOT IN (
+            SELECT DISTINCT participant FROM notifications n
+            WHERE n.event = 'profile_description_missing'
+            AND timestamp >= (current_timestamp - interval '180 days')
+        )
+    """)
+
+    for tippee_id in tippees_without_description:
+        p = website.db.Participant.from_id(tippee_id)
+        p.notify('profile_description_missing', force_email=True)
+        logger.info("Sent update_profile notification to user %s." % p.id)
--- liberapay/main.py
+++ liberapay/main.py
@@ -26,6 +26,7 @@ from pando.utils import maybe_encode
 
 from liberapay import utils
 from liberapay.billing.payday import Payday, create_payday_issue
+from liberapay.billing.cron import generate_profile_description_missing_notifications
 from liberapay.cron import Cron, Daily, Weekly
 from liberapay.exceptions import PayinMethodIsUnavailable, TooManyAttempts
 from liberapay.i18n.base import (
@@ -179,6 +180,7 @@ if conf:
     cron(Daily(hour=18), Payday.update_cached_amounts, True)
     cron(intervals.get('notify_patrons', 1200), Participant.notify_patrons, True)
     cron(intervals.get('migrate_identities', 120), Participant.migrate_identities, True)
+    cron(Daily(hour=19), generate_profile_description_missing_notifications, True)
     if conf.ses_feedback_queue_url:
         cron(intervals.get('fetch_email_bounces', 60), handle_email_bounces, True)
     cron(Daily(hour=1), clean_up_emails, True)
