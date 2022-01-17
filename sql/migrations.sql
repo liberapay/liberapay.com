7fd2cee2df00cd62b035092f544012ef9ed5c402
Changaco <changaco@changaco.oy.lc>
2022-01-17 11:08:52 +0100

    merge branch.sql

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -3078,3 +3078,18 @@ UPDATE exchange_routes
 
 -- migration #152
 CREATE INDEX transfers_team_idx ON transfers (team) WHERE team IS NOT NULL;
+
+-- migration #153
+UPDATE exchange_routes AS r
+   SET is_default = null
+ WHERE is_default
+   AND NOT EXISTS (
+           SELECT 1
+             FROM events e
+            WHERE e.participant = r.participant
+              AND e.type = 'set_default_route'
+              AND e.payload = jsonb_build_object(
+                      'id', r.id,
+                      'network', r.network
+                  )
+       );
