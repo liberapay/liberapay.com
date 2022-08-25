c931c1472680000c42921535265f6bd11a800e7c
Changaco <changaco@changaco.oy.lc>
2022-08-25 15:37:55 +0200

    merge branch.sql

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -3201,3 +3201,24 @@ UPDATE payin_transfers SET remote_id = null WHERE remote_id = '';
 ALTER TABLE participants ALTER COLUMN email_notif_bits SET DEFAULT 2147483646;
 DELETE FROM notifications WHERE event = 'income~v2' AND NOT email;
 UPDATE notifications SET web = false WHERE event = 'income~v2';
+
+-- migration #159
+UPDATE payins SET error = 'abandoned py payer' WHERE error = 'expired';
+UPDATE payin_events SET error = 'abandoned py payer' WHERE error = 'expired';
+UPDATE payin_transfers SET error = 'abandoned py payer' WHERE error = 'expired';
+UPDATE payin_transfer_events SET error = 'abandoned py payer' WHERE error = 'expired';
+WITH closed as (
+    UPDATE participants
+       SET status = 'closed'
+     WHERE kind = 'group'
+       AND status = 'active'
+       AND NOT EXISTS (
+               SELECT 1
+                 FROM current_takes take
+                WHERE take.team = participants.id
+           )
+ RETURNING id
+)
+INSERT INTO events (participant, type, payload)
+     SELECT id, 'set_status', '"closed"'
+       FROM closed;
