65080386bf023baccf766cff7e0acafde9636ad9
Changaco <changaco@changaco.oy.lc>
2022-12-31 20:51:11 +0100

    merge branch.sql

--- sql/migrations.sql
+++ sql/migrations.sql
@@ -3252,3 +3252,41 @@ $$ LANGUAGE SQL STRICT;
 ALTER TYPE payin_status ADD VALUE IF NOT EXISTS 'awaiting_review';
 ALTER TYPE payin_transfer_status ADD VALUE IF NOT EXISTS 'awaiting_review';
 CREATE INDEX payins_awating_review ON payins (status) WHERE status = 'awaiting_review';
+
+-- migration #162
+INSERT INTO currency_exchange_rates
+     VALUES ('HRK', 'EUR', 1 / 7.53450)
+          , ('EUR', 'HRK', 7.53450)
+ON CONFLICT (source_currency, target_currency) DO UPDATE
+        SET rate = excluded.rate;
+UPDATE participants
+   SET main_currency = 'EUR'
+     , goal = convert(goal, 'EUR')
+     , giving = convert(giving, 'EUR')
+     , receiving = convert(receiving, 'EUR')
+     , taking = convert(taking, 'EUR')
+ WHERE main_currency = 'HRK';
+UPDATE participants p
+   SET accepted_currencies = (CASE
+           WHEN accepted_currencies LIKE '%EUR%'
+           THEN replace(regexp_replace(regexp_replace(accepted_currencies, '^HRK,', ''), ',HRK$', ''), ',HRK,', ',')
+           ELSE replace(accepted_currencies, 'HRK', 'EUR')
+       END)
+ WHERE accepted_currencies LIKE '%HRK%';
+INSERT INTO tips
+          ( ctime, tipper, tippee
+          , amount, period, periodic_amount
+          , paid_in_advance, is_funded, renewal_mode, visibility )
+     SELECT ctime, tipper, tippee
+          , convert(amount, 'EUR'), period, convert(periodic_amount, 'EUR')
+          , convert(paid_in_advance, 'EUR'), is_funded, renewal_mode, visibility
+       FROM current_tips
+      WHERE (amount).currency = 'HRK';
+UPDATE scheduled_payins
+   SET amount = convert(amount, 'EUR')
+ WHERE (amount).currency = 'HRK';
+INSERT INTO takes
+            (ctime, member, team, amount, actual_amount, recorder, paid_in_advance)
+     SELECT ctime, member, team, convert(amount, 'EUR'), actual_amount, recorder, convert(paid_in_advance, 'EUR')
+       FROM current_takes
+      WHERE (amount).currency = 'HRK';
