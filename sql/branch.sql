2edb48abcec865043fa533fd37d1774824ebde79
Changaco <changaco@changaco.oy.lc>
2023-04-26 10:26:08 +0200

    fix resetting `marked_as` back to `null`

---
+++ sql/branch.sql
@@ -0,0 +1,34 @@
+CREATE OR REPLACE FUNCTION update_profile_visibility() RETURNS trigger AS $$
+    BEGIN
+        IF (OLD.marked_as IS NULL AND NEW.marked_as IS NULL) THEN
+            RETURN NEW;
+        END IF;
+        IF (NEW.marked_as = 'trusted') THEN
+            NEW.is_suspended = false;
+        ELSIF (NEW.marked_as IN ('fraud', 'spam')) THEN
+            NEW.is_suspended = true;
+        ELSE
+            NEW.is_suspended = null;
+        END IF;
+        IF (NEW.marked_as = 'unsettling') THEN
+            NEW.is_unsettling = NEW.is_unsettling | 2;
+        ELSE
+            NEW.is_unsettling = NEW.is_unsettling & 2147483645;
+        END IF;
+        IF (NEW.marked_as IN ('okay', 'trusted')) THEN
+            NEW.profile_noindex = NEW.profile_noindex & 2147483645;
+            NEW.hide_from_lists = NEW.hide_from_lists & 2147483645;
+            NEW.hide_from_search = NEW.hide_from_search & 2147483645;
+        ELSE
+            NEW.profile_noindex = NEW.profile_noindex | 2;
+            NEW.hide_from_lists = NEW.hide_from_lists | 2;
+            NEW.hide_from_search = NEW.hide_from_search | 2;
+        END IF;
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+UPDATE participants
+   SET marked_as = marked_as
+ WHERE marked_as IS NULL AND ( is_suspended OR hide_from_lists & 2 = 2 )
+    OR marked_as = 'unsettling';
