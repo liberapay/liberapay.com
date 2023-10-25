f312a4cd74e6fb5701136b5ef3d6c44faa53ce2a
Changaco <changaco@changaco.oy.lc>
2023-10-25 16:31:37 +0200

    merge branch.sql

--- sql/branch.sql
+++
@@ -1,2 +0,0 @@
-UPDATE app_conf SET value = '"https://api.openstreetmap.org/api/0.6"'::jsonb WHERE key = 'openstreetmap_api_url';
-UPDATE app_conf SET value = '"https://www.openstreetmap.org"'::jsonb WHERE key = 'openstreetmap_auth_url';
--- sql/migrations.sql
+++ sql/migrations.sql
@@ -3423,3 +3423,7 @@ DELETE FROM rate_limiting WHERE key LIKE 'refetch_%';
 
 -- migration #170
 CREATE TYPE localized_string AS (string text, lang text);
+
+-- migration #171
+UPDATE app_conf SET value = '"https://api.openstreetmap.org/api/0.6"'::jsonb WHERE key = 'openstreetmap_api_url';
+UPDATE app_conf SET value = '"https://www.openstreetmap.org"'::jsonb WHERE key = 'openstreetmap_auth_url';
