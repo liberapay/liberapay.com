6d004ea7bc50952f6b44d91c47aac647de9ddce0
Changaco <changaco@changaco.oy.lc>
2024-04-12 11:20:36 +0200

    disable Twitter pledging
    It looks like we no longer have access to these Twitter API endpoints.

--- liberapay/elsewhere/twitter.py
+++ liberapay/elsewhere/twitter.py
@@ -20,10 +20,10 @@ class Twitter(PlatformOAuth1):
                                           prev='previous_cursor',
                                           next='next_cursor')
     api_url = 'https://api.twitter.com/1.1'
-    api_user_info_path = '/users/show.json?user_id={user_id}&include_entities=false'
-    api_user_name_info_path = '/users/show.json?screen_name={user_name}&include_entities=false'
+    # api_user_info_path = '/users/show.json?user_id={user_id}&include_entities=false'
+    # api_user_name_info_path = '/users/show.json?screen_name={user_name}&include_entities=false'
     api_user_self_info_path = '/account/verify_credentials.json?include_entities=false&skip_status=true'
-    api_follows_path = '/friends/list.json?user_id={user_id}&include_user_entities=false&skip_status=true'
+    # api_follows_path = '/friends/list.json?user_id={user_id}&include_user_entities=false&skip_status=true'
     ratelimit_headers_prefix = 'x-rate-limit-'
 
     # User info extractors
