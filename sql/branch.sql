a76f4f358866d3956bd3470185f95b1accc02324
Changaco <changaco@changaco.oy.lc>
2025-06-13 12:55:41 +0200

    drop unique constraint on payment accounts per country
    closes #1842

--- sql/branch.sql
+++ sql/branch.sql
@@ -4,7 +4,10 @@ BEGIN;
         ADD COLUMN independent boolean DEFAULT true,
         ADD COLUMN loss_taker loss_taker DEFAULT 'provider',
         ADD COLUMN details_submitted boolean,
-        ADD COLUMN allow_payout boolean;
+        ADD COLUMN allow_payout boolean,
+        DROP CONSTRAINT payment_accounts_participant_provider_country_is_current_key;
+    CREATE INDEX payment_accounts_participant_provider_country_is_current_idx
+        ON payment_accounts (participant, provider, country, is_current);
 END;
 SELECT 'after deployment';
 BEGIN;
