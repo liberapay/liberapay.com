26e5f2dc4d1a131088025d0f668cb02b66d42da1
Changaco <changaco@changaco.oy.lc>
2026-01-20 16:38:43 +0100

    fix `destination_amount` in recipients' ledgers
    fixes LIBERAPAYCOM-2D8

--- liberapay/utils/history.py
+++ liberapay/utils/history.py
@@ -486,9 +486,8 @@ def iter_payin_events(db, participant, period_start, period_end, minimize=False)
     """, params, back_as=dict)
     incoming_transfers = db.all("""
         SELECT tr.id, tr.ctime, tr.payin, tr.payer, tr.context, tr.status, tr.error
-             , coalesce(tr.destination_amount, tr.amount) AS amount, tr.fee
-             , tr.unit_amount, tr.n_units, tr.period
-             , tr.reversed_amount, tr.visibility
+             , tr.amount, tr.fee, tr.unit_amount, tr.n_units, tr.period
+             , tr.reversed_amount, tr.visibility, tr.destination_amount
              , p.username AS payer_username, p2.username AS team_name
              , r.network AS payin_method
           FROM payin_transfers tr
