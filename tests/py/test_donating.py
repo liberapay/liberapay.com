d374fb25d20b611e216dc2c3920c8ced4c994765
Matt Mullenweg <matt@git.wordpress.org>
2026-04-27 02:21:22 -0500

    Handle invalid custom donation amounts

--- tests/py/test_donating.py
+++ tests/py/test_donating.py
@@ -76,7 +76,8 @@ class TestDonating(Harness):
         assert tip.visibility == 3
 
     def test_donation_form_v2_enforces_amount_limits(self):
-        self.make_participant('creator', accepted_currencies=None)
+        creator = self.make_participant('creator', accepted_currencies=None)
+        creator.update_recipient_settings(patron_visibilities=7)
         donor = self.make_participant('donor')
         r = self.client.PxST(
             '/creator/tip',
@@ -113,3 +114,129 @@ class TestDonating(Harness):
         )
         assert r.code == 400
         assert r.__class__.__name__ == 'InvalidNumber'
+
+        r = self.client.PxST(
+            '/creator/tip.json',
+            {
+                'currency': 'USD',
+                'selected_amount': 'custom',
+                'period': 'monthly',
+                'amount': '1111',
+                'renewal_mode': '1',
+            },
+            auth_as=donor,
+        )
+        assert r.code == 400
+        assert r.__class__.__name__ == 'BadAmount'
+
+        r = self.client.POST(
+            '/creator/tip',
+            {
+                'currency': 'USD',
+                'selected_amount': 'custom',
+                'period': 'monthly',
+                'amount': '1111',
+                'renewal_mode': '1',
+                'visibility': '3',
+            },
+            auth_as=donor,
+        )
+        assert r.code == 400
+        assert '$1,111.00' in r.text
+        assert 'not a valid monthly donation amount' in r.text
+        currency_values = {
+            i.attrib['value'] for i in r.html_tree.findall(".//{*}input[@name='currency']")
+        }
+        assert currency_values == {'USD'}
+        assert ' name="period" value="monthly" checked' in r.text
+        assert ' value="1,111.00"' in r.text
+        checked_renewal_modes = [
+            i.attrib['value'] for i in r.html_tree.findall(".//{*}input[@name='renewal_mode']")
+            if 'checked' in i.attrib
+        ]
+        assert checked_renewal_modes == ['1']
+        checked_visibilities = [
+            i.attrib['value'] for i in r.html_tree.findall(".//{*}input[@name='visibility']")
+            if 'checked' in i.attrib
+        ]
+        assert checked_visibilities == ['3']
+
+    def test_donation_form_v2_rejects_custom_amounts_in_unsupported_currency(self):
+        self.make_participant('creator', accepted_currencies='USD')
+        donor = self.make_participant('donor')
+
+        r = self.client.PxST(
+            '/creator/tip',
+            {
+                'currency': 'KRW',
+                'selected_amount': 'custom',
+                'period': 'monthly',
+                'amount': '1111111',
+                'renewal_mode': '1',
+            },
+            auth_as=donor,
+        )
+
+        assert r.code == 400
+        assert r.__class__.__name__ == 'BadAmount'
+
+    def test_donation_form_v2_preserves_hidden_visibility_on_error(self):
+        creator = self.make_participant('creator', accepted_currencies=None)
+        creator.update_recipient_settings(patron_visibilities=2)
+        donor = self.make_participant('donor')
+
+        r = self.client.POST(
+            '/creator/tip',
+            {
+                'currency': 'USD',
+                'selected_amount': 'custom',
+                'period': 'monthly',
+                'amount': '1111',
+                'renewal_mode': '1',
+                'visibility': '3',
+            },
+            auth_as=donor,
+        )
+
+        assert r.code == 400
+        hidden_visibilities = [
+            i.attrib['value'] for i in r.html_tree.findall(".//{*}input[@name='visibility']")
+            if i.attrib['type'] == 'hidden'
+        ]
+        assert hidden_visibilities == ['2']
+
+    def test_donation_form_v2_preserves_custom_amount_over_standard_tip_on_error(self):
+        creator = self.make_participant('creator', accepted_currencies=None)
+        creator.update_recipient_settings(patron_visibilities=7)
+        donor = self.make_participant('donor')
+        donor.set_tip_to(creator, USD('1.00'), renewal_mode=1, visibility=2)
+
+        r = self.client.POST(
+            '/creator/tip',
+            {
+                'currency': 'USD',
+                'selected_amount': 'custom',
+                'period': 'monthly',
+                'amount': '1111',
+                'renewal_mode': '2',
+                'visibility': '3',
+            },
+            auth_as=donor,
+        )
+
+        assert r.code == 400
+        checked_amounts = [
+            i.attrib['value'] for i in r.html_tree.findall(".//{*}input[@name='selected_amount']")
+            if 'checked' in i.attrib
+        ]
+        assert checked_amounts == ['custom']
+        checked_renewal_modes = [
+            i.attrib['value'] for i in r.html_tree.findall(".//{*}input[@name='renewal_mode']")
+            if 'checked' in i.attrib
+        ]
+        assert checked_renewal_modes == ['2']
+        checked_visibilities = [
+            i.attrib['value'] for i in r.html_tree.findall(".//{*}input[@name='visibility']")
+            if 'checked' in i.attrib
+        ]
+        assert checked_visibilities == ['3']
